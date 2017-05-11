a37fc8c2b477bc0d83379357e9975b83e2612301
Bryan Harter <harter@lasp.colorado.edu>
2017-05-11 12:28:06 -0600

    Fixing a bug in tplot_names

--- pytplot/tplot_names.py
+++ pytplot/tplot_names.py
@@ -1,16 +1,18 @@
 from . import tplot_common
 
 def tplot_names():
+    index = 0
     for key, _ in tplot_common.data_quants.items():
         if isinstance(tplot_common.data_quants[key].data, list):
-            if isinstance(key, int):
+            if isinstance(key, str):
                 names_to_print = tplot_common.data_quants[key].name + "  data from: "
                 for name in tplot_common.data_quants[key].data:
                     names_to_print = names_to_print + " " + name
-                print(key, ":", names_to_print)
+                print(index, ":", names_to_print)
+                index+=1
         else:
-            if isinstance(key, int):
+            if isinstance(key, str):
                 names_to_print = tplot_common.data_quants[key].name
-                print(key, ":", names_to_print)
-        
+                print(index, ":", names_to_print)
+                index+=1
     return
