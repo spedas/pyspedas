eefd0f0a6cbcbb710d85243b6ab6f77856017578
esalvolucas <elysia.lucas@lasp.colorado.edu>
2017-09-06 15:40:09 -0600

    Test message

--- pytplot/timebar.py
+++ pytplot/timebar.py
@@ -8,6 +8,7 @@ from .timestamp import TimeStamp
 from . import tplot_utilities
 from bokeh.models import Span
 
+#HELLO IGNORE THIS
 
 def timebar(t, varname = None, databar = False, delete = False, color = 'black', thick = 1, dash = False):    
     """
