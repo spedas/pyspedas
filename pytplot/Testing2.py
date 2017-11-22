733cc25667e44603abbfa0ae01408731a10fe287
esalvolucas <elysia.lucas@lasp.colorado.edu>
2017-11-22 11:28:46 -0700

    testing2 changed

--- pytplot/Testing2.py
+++ pytplot/Testing2.py
@@ -1,18 +1,4 @@
 import pytplot
-pytplot.store_data('orbit', data={'x':[1497830400, 1466380800], 'y':[3354.4, 3355.45]})
-pytplot.store_data('orbit2', data={'x':[1497830400, 1466380800], 'y':[3354.4, 3355.45]})
-pytplot.store_data('orbit3', data={'x':[1497830400, 1466380800], 'y':[3354.4, 3355.45]})
- 
-pytplot.tplot_names()
- 
-pytplot.tplot_rename("orbit2", "orbitb")
- 
-pytplot.tplot_names()
-
-
-
-
-
-#for instrument in insitu
-#	check if instrument == true
-#	for parameter in instrument?
+#pytplot.options(3,"ylog",1)
+import pydivide
+insitu2 = pydivide.insitu_search(insitu, 'inbound')
