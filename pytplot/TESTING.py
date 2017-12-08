ce077d95f89f436ea51f5de0c3aebb42f52077aa
esalvolucas <elysia.lucas@lasp.colorado.edu>
2017-12-08 16:09:35 -0700

    Readded Testing to .gitignore

--- pytplot/TESTING.py
+++ pytplot/TESTING.py
@@ -1,6 +1,13 @@
 import pydivide
-pydivide.download_files(start_date='2015-12-25',end_date='2015-12-27')
-pydivide.download_files(start_date='2015-12-25',end_date='2015-12-27',iuvs=True)
-insitu,iuvs = pydivide.read('2015-12-26')
+import pytplot
+from pytplot import tplot_common
+#pydivide.download_files(start_date='2015-12-25',end_date='2015-12-27')
+#pydivide.download_files(start_date='2015-12-25',end_date='2015-12-27',iuvs=True)
+#insitu,iuvs = pydivide.read('2015-12-26')
 
-insitu2 = pydivide.insitu_search(insitu, 'inbound')
+#pytplot.options(3,"ylog",1)
+#
+pytplot.options('swia_counts', 'ylog', 1)
+
+name = tplot_common.data_quants.keys()[3]
+#insitu2 = pydivide.insitu_search(insitu, 'inbound')
