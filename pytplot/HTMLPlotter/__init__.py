24dce8d92136775f08d29b335b09032c927f0619
Bryan Harter <harter@lasp.colorado.edu>
2018-03-15 12:49:38 -0600

    Bug fixing last commit

--- pytplot/HTMLPlotter/__init__.py
+++ pytplot/HTMLPlotter/__init__.py
@@ -1,5 +1,5 @@
 from .TVarFigure1D import TVarFigure1D
-from .TVarFigure2D import TVarFigure2D
+from .TVarFigureMap import TVarFigureMap
 from .TVarFigureAlt import TVarFigureAlt
 from .TVarFigureSpec import TVarFigureSpec
 from .generate import generate_stack
