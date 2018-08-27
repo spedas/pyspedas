955d15a51c9f4d92cbb96784bcbdc38c23f9e98f
esalvolucas <elysia.lucas@lasp.colorado.edu>
2018-08-27 15:52:50 -0600

    Optional user input enabled

--- pytplot/tplot_math/split_vec.py
+++ pytplot/tplot_math/split_vec.py
@@ -7,12 +7,15 @@ import pandas as pd
 
 #SPLIT TVAR
 #store columns of TVar into new TVars
-def split_vec(tvar,newtvars,columns):
+def split_vec(tvar,newtvars=None,columns='all'):
     #separate and add data
     time = pytplot.data_quants[tvar].data.index
     data = pytplot.data_quants[tvar].data
     df = pytplot.data_quants[tvar]
+    defaultlist = []
     #grab column data
+    if columns == 'all':
+        columns = pytplot.data_quants[tvar].data.columns.values
     for i,val in enumerate(columns):
         #if not a list
         if isinstance(val,int):
@@ -23,5 +26,13 @@ def split_vec(tvar,newtvars,columns):
             range_end = val[1]
         split_col = list(range(range_start,range_end+1))
         #store split data
-        pytplot.store_data(newtvars[i],data={'x':time, 'y':data[split_col]})
-    return newtvars
+        defaultname = 'data_' + str(i)
+        defaultlist = defaultlist + [defaultname]
+        if newtvars is None:
+            pytplot.store_data(defaultname,data={'x':time, 'y':data[split_col]})
+        else:
+            pytplot.store_data(newtvars[i],data={'x':time, 'y':data[split_col]})
+    if newtvars is None:
+        return defaultlist
+    else:
+        return newtvars
