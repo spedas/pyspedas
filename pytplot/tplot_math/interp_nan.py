ee1adf23c664558d3f7304b173550d29d2dbbc88
esalvolucas <elysia.lucas@lasp.colorado.edu>
2018-08-27 15:53:18 -0600

    Used copy of dataframe

--- pytplot/tplot_math/interp_nan.py
+++ pytplot/tplot_math/interp_nan.py
@@ -13,7 +13,7 @@ import pandas as pd
 #LINEAR INTERPOLATION
 #interpolate over NaN data
 def interp_nan(tvar1,newtvar,s_limit=0):
-    tv1 = pytplot.data_quants[tvar1].data
+    tv1 = pytplot.data_quants[tvar1].data.copy()
     tv1 = tv1.astype(float)
     cadence = tv1.index[1] - tv1.index[0]
     n_nans = int(round(s_limit/cadence))
