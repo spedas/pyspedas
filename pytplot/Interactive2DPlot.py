ec2cc4a8bf6c96ee16e3af71c3514b7716513236
Julie Barnum <juba8233@macl3810.lasp.colorado.edu>
2018-09-27 14:23:52 -0600

    made necessary changes to Interactive2D plot after deciding to redo default values

--- pytplot/Interactive2DPlot.py
+++ pytplot/Interactive2DPlot.py
@@ -18,9 +18,16 @@ def get_plot_labels(names):
     plot_labels = {}
     for n in names:
         if pytplot.data_quants[n].spec_bins is not None:
+
             zlabel = pytplot.data_quants[n].zaxis_opt['axis_label']
-            ztype = pytplot.data_quants[n].zaxis_opt['z_axis_type']
-            ytype = pytplot.data_quants[n].yaxis_opt['y_axis_type']
+            try:
+                ztype = pytplot.data_quants[n].zaxis_opt['z_axis_type']
+            except:
+                ztype = pytplot.default_values['z_axis_type']
+            try:
+                ytype = pytplot.data_quants[n].yaxis_opt['y_axis_type']
+            except:
+                ytype = pytplot.default_values['y_axis_type']
             plot_labels[n] = [zlabel, ytype, ztype]
     return plot_labels
 
