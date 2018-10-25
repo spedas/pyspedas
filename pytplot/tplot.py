e19264aa3537113732f305173cbbedd7ec6a4aab
Julie Barnum <juba8233@macl3810.lasp.colorado.edu>
2018-10-25 09:31:39 -0600

    Created option to save png from window (for main plots, not interactive plots)

--- pytplot/tplot.py
+++ pytplot/tplot.py
@@ -15,65 +15,66 @@ import tempfile
 
 if pytplot.using_graphics:
     from .QtPlotter import PyTPlot_Exporter
-    from pyqtgraph.Qt import QtCore, QtGui
+    from pyqtgraph.Qt import QtCore, QtGui, QtCore
     import pyqtgraph as pg
     from . import QtPlotter, interactive2dPlot
+
     try:
         from PyQt5.QtWebKitWidgets import QWebView as WebView
     except:
         from PyQt5.QtWebEngineWidgets import QWebEngineView as WebView
 
-def tplot(name, 
+
+def tplot(name,
           var_label=None,
-          auto_color=True, 
-          interactive=False, 
-          combine_axes=True, 
-          nb=False, 
+          auto_color=True,
+          interactive=False,
+          combine_axes=True,
+          nb=False,
           save_file=None,
-          gui=False, 
+          gui=False,
           qt=False,
           bokeh=False,
           crosshair=True,
           save_png=None,
           display=True):
-    
     """
     This is the function used to display the tplot variables stored in memory.
-    The default output is to show the plots stacked on top of one another inside a GUI window.  
+    The default output is to show the plots stacked on top of one another inside a GUI window.
     The GUI window has the option to export the plots in either PNG or HTML formats.
-    
+
     .. note::
-        This plotting routine uses the python Bokeh library, which creates plots using HTML and Javascript.  
-        Bokeh is technically still in beta, so future patches to Bokeh may require updates to this function.  
-    
+        This plotting routine uses the python Bokeh library, which creates plots using HTML and Javascript.
+        Bokeh is technically still in beta, so future patches to Bokeh may require updates to this function.
+
     Parameters:
         name : str / list
             List of tplot variables that will be plotted
         var_label : str, optional
             The name of the tplot variable you would like as
-            a second x axis. 
+            a second x axis.
         auto_color : bool, optional
             Automatically color the plot lines.
         interactive : bool, optional
-            If True, a secondary interactive plot will be generated next to spectrogram plots.  
-            Mousing over the spectrogram will display a slice of data from that time on the 
+            If True, a secondary interactive plot will be generated next to spectrogram plots.
+            Mousing over the spectrogram will display a slice of data from that time on the
             interactive chart.
         combine_axes : bool, optional
             If True, the axes are combined so that they all display the same x range.  This also enables
-            scrolling/zooming/panning on one plot to affect all of the other plots simultaneously.  
+            scrolling/zooming/panning on one plot to affect all of the other plots simultaneously.
         nb : bool, optional
-            If True, the plot will be displayed inside of a current Jupyter notebook session.  
+            If True, the plot will be displayed inside of a current Jupyter notebook session.
         save_file : str, optional
-            A full file name and path.  
+            A full file name and path.
             If this option is set, the plot will be automatically saved to the file name provided in an HTML format.
-            The plots can then be opened and viewed on any browser without any requirements. 
+            The plots can then be opened and viewed on any browser without any requirements.
         bokeh : bool, optional
             If True, plots data using bokeh
             Else (bokeh=False or omitted), plots data using PyQtGraph
         gui : bool, optional
             If True, then this function will output the 2 HTML components of the generated plots as string variables.
-            This is useful if you are embedded the plots in your own GUI.  For more information, see 
-            http://bokeh.pydata.org/en/latest/docs/user_guide/embed.html  
+            This is useful if you are embedded the plots in your own GUI.  For more information, see
+            http://bokeh.pydata.org/en/latest/docs/user_guide/embed.html
         qt : bool, optional
             If True, then this function will display the plot inside of the Qt window.  From this window, you
             can choose to export the plots as either an HTML file, or as a PNG.
@@ -85,10 +86,10 @@ def tplot(name,
         display: bool, optional
             If True, then this function will display the plotted tplot variables. Necessary to make this optional
             so we can avoid it in a headless server environment.
-        
+
     Returns:
         None
-        
+
     Examples:
         >>> #Plot a single line in bokeh
         >>> import pytplot
@@ -96,13 +97,13 @@ def tplot(name,
         >>> y_data = [1,2,3,4,5]
         >>> pytplot.store_data("Variable1", data={'x':x_data, 'y':y_data})
         >>> pytplot.tplot("Variable1",bokeh=True)
-        
+
         >>> #Display two plots
         >>> x_data = [1,2,3,4,5]
         >>> y_data = [[1,5],[2,4],[3,3],[4,2],[5,1]]
         >>> pytplot.store_data("Variable2", data={'x':x_data, 'y':y_data})
         >>> pytplot.tplot(["Variable1", "Variable2"])
-        
+
         >>> #Display 2 plots, using Variable1 as another x axis
         >>> x_data = [1,2,3]
         >>> y_data = [ [1,2,3] , [4,5,6], [7,8,9] ]
@@ -110,14 +111,14 @@ def tplot(name,
         >>> pytplot.store_data("Variable3", data={'x':x_data, 'y':y_data, 'v':v_data})
         >>> pytplot.options("Variable3", 'spec', 1)
         >>> pytplot.tplot(["Variable2", "Variable3"], var_label='Variable1')
-        
+
         >>> #Plot all 3 tplot variables, sending the output to an HTML file
         >>> pytplot.tplot(["Variable1", "Variable2", "Variable3"], save_file='C:/temp/pytplot_example.html')
-        
+
         >>> #Plot all 3 tplot variables, sending the HTML output to a pair of strings
         >>> div, component = pytplot.tplot(["Variable1", "Variable2", "Variable3"], gui=True)
     """
-    
+
     if not pytplot.using_graphics and save_file is None:
         print("Qt was not successfully imported.  Specify save_file to save the file as a .html file.")
         return
@@ -127,7 +128,7 @@ def tplot(name,
         num_plots = 1
     else:
         num_plots = len(name)
-    
+
     for i in range(num_plots):
         if isinstance(name[i], int):
             name[i] = list(pytplot.data_quants.keys())[name[i]]
@@ -151,16 +152,16 @@ def tplot(name,
             return
         elif save_file is not None:
             output_file(save_file, mode='inline')
-            save(layout)    
+            save(layout)
             return
-        elif qt:        
+        elif qt:
             available_qt_window = tplot_utilities.get_available_qt_window()
             dir_path = tempfile.gettempdir()  # send to user's temp directory
             output_file(os.path.join(dir_path, "temp.html"), mode='inline')
             save(layout)
             new_layout = WebView()
-            available_qt_window.resize(pytplot.tplot_opt_glob['window_size'][0]+100,
-                                       pytplot.tplot_opt_glob['window_size'][1]+100)
+            available_qt_window.resize(pytplot.tplot_opt_glob['window_size'][0] + 100,
+                                       pytplot.tplot_opt_glob['window_size'][1] + 100)
             new_layout.resize(pytplot.tplot_opt_glob['window_size'][0], pytplot.tplot_opt_glob['window_size'][1])
             dir_path = tempfile.gettempdir()  # send to user's temp directory
             new_layout.setUrl(QtCore.QUrl.fromLocalFile(os.path.join(dir_path, "temp.html")))
@@ -170,13 +171,13 @@ def tplot(name,
             if not (hasattr(sys, 'ps1')) or not hasattr(QtCore, 'PYQT_VERSION'):
                 QtGui.QApplication.instance().exec_()
             return
-        else:      
+        else:
             dir_path = tempfile.gettempdir()  # send to user's temp directory
             output_file(os.path.join(dir_path, "temp.html"), mode='inline')
             show(layout)
             return
     else:
-        if save_png != '':
+        if save_png is not None:
             layout = QtPlotter.generate_stack(name, var_label=var_label, auto_color=auto_color,
                                               combine_axes=combine_axes,
                                               mouse_moved_event=pytplot.hover_time.change_hover_time,
@@ -192,6 +193,19 @@ def tplot(name,
             exporter.export(save_png)
 
         if display:
+            # Set up all things needed for when a user asks to save plot from window
+            layout_orig = QtPlotter.generate_stack(name, var_label=var_label, auto_color=auto_color,
+                                                   combine_axes=combine_axes,
+                                                   mouse_moved_event=pytplot.hover_time.change_hover_time,
+                                                   crosshair=crosshair)
+            layout_orig.resize(pytplot.tplot_opt_glob['window_size'][0], pytplot.tplot_opt_glob['window_size'][1])
+            for i, item in enumerate(layout_orig.items()):
+                if type(item) == pg.graphicsItems.GraphicsLayout.GraphicsLayout:
+                    layout_orig.items()[i].resize(pytplot.tplot_opt_glob['window_size'][0],
+                                                  pytplot.tplot_opt_glob['window_size'][1])
+            exporter = QtPlotter.PytplotExporter(layout_orig)
+
+            # Set up displayed plot window and grab plots to plot on it
             available_qt_window = tplot_utilities.get_available_qt_window()
             layout = QtPlotter.generate_stack(name, var_label=var_label, auto_color=auto_color,
                                               combine_axes=combine_axes,
@@ -201,8 +215,13 @@ def tplot(name,
             available_qt_window.newlayout(layout)
             available_qt_window.resize(pytplot.tplot_opt_glob['window_size'][0],
                                        pytplot.tplot_opt_glob['window_size'][1])
+
+            # Implement button that lets you save the PNG
+            available_qt_window.init_savepng(exporter)
+            # Show the plot window and plot
             available_qt_window.show()
             available_qt_window.activateWindow()
+
             if interactive:
                 # Call 2D interactive window; This will only plot something when spectrograms are involved.
                 interactive2dPlot.interactive2dplot()
