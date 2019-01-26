077d465a2a7119c709146a5a4536d8de5a05613b
eric <egrimes@igpp.ucla.edu>
2019-01-26 10:06:11 -0800

    Update load_routines_ut.py

--- pyspedas/mms/tests/load_routines_ut.py
+++ pyspedas/mms/tests/load_routines_ut.py
@@ -6,6 +6,22 @@ import unittest
 from ...mms import mms_load_fgm, mms_load_scm, mms_load_fpi, mms_load_hpca, mms_load_eis, mms_load_feeps, mms_load_edp, mms_load_aspoc
 from ...utilities.data_exists import data_exists
 
+class EDPLoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_edp()
+        self.assertTrue(data_exists('mms1_edp_dce_gse_fast_l2'))
+
+    def test_load_multiple_sc(self):
+        data = mms_load_fgm(probe=['1', '2', '3', '4'])
+        self.assertTrue(data_exists('mms1_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms2_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms3_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms4_edp_dce_gse_fast_l2'))
+
+    def test_load_brst_data(self):
+        data = mms_load_fgm(data_rate='brst', trange=['2015-10-16/13:00', '2015-10-16/13:10'])
+        self.assertTrue(data_exists('mms1_edp_dce_gse_brst_l2'))
+
 class FGMLoadTestCases(unittest.TestCase):
     def test_load_default_data(self):
         data = mms_load_fgm()
