914ada6e919f24c6f68e3b18b9a09ab067df6ae6
eric <egrimes@igpp.ucla.edu>
2019-01-31 12:08:33 -0800

    test updates

--- pyspedas/mms/tests/load_routine_tests.py
+++ pyspedas/mms/tests/load_routine_tests.py
@@ -12,15 +12,15 @@ class EDPLoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('mms1_edp_dce_gse_fast_l2'))
 
     def test_load_multiple_sc(self):
-        data = mms_load_fgm(probe=['1', '2', '3', '4'])
+        data = mms_load_edp(probe=['1', '2', '3', '4'])
         self.assertTrue(data_exists('mms1_edp_dce_gse_fast_l2'))
-        # self.assertTrue(data_exists('mms2_edp_dce_gse_fast_l2'))
-        # self.assertTrue(data_exists('mms3_edp_dce_gse_fast_l2'))
-        # self.assertTrue(data_exists('mms4_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms2_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms3_edp_dce_gse_fast_l2'))
+        self.assertTrue(data_exists('mms4_edp_dce_gse_fast_l2'))
 
-    # def test_load_brst_data(self):
-    #     data = mms_load_fgm(data_rate='brst', trange=['2015-10-16/13:00', '2015-10-16/13:10'])
-    #     self.assertTrue(data_exists('mms1_edp_dce_gse_brst_l2'))
+    def test_load_brst_data(self):
+        data = mms_load_edp(data_rate='brst', trange=['2015-10-16/13:00', '2015-10-16/13:10'])
+        self.assertTrue(data_exists('mms1_edp_dce_gse_brst_l2'))
 
 class FGMLoadTestCases(unittest.TestCase):
     def test_load_default_data(self):
@@ -44,15 +44,15 @@ class SCMLoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('mms1_scm_acb_gse_scsrvy_srvy_l2'))
 
     # def test_load_multiple_sc(self):
-    #     data = mms_load_scm(probe=['1', '2', '3', '4'])
+    #     data = mms_load_scm(probe=['1', '2', '3', '4'], trange=['2017-12-15', '2017-12-16'])
     #     self.assertTrue(data_exists('mms1_scm_acb_gse_scsrvy_srvy_l2'))
     #     self.assertTrue(data_exists('mms2_scm_acb_gse_scsrvy_srvy_l2'))
     #     self.assertTrue(data_exists('mms3_scm_acb_gse_scsrvy_srvy_l2'))
     #     self.assertTrue(data_exists('mms4_scm_acb_gse_scsrvy_srvy_l2'))
 
-    # def test_load_brst_data(self):
-    #     data = mms_load_scm(data_rate='brst', trange=['2015-10-16/13:00', '2015-10-16/13:10'])
-    #     self.assertTrue(data_exists('mms1_scm_acb_gse_scb_brst_l2'))
+    def test_load_brst_data(self):
+        data = mms_load_scm(data_rate='brst', trange=['2015-10-16/13:00', '2015-10-16/13:10'])
+        self.assertTrue(data_exists('mms1_scm_acb_gse_scb_brst_l2'))
 
 if __name__ == '__main__':
     unittest.main()
