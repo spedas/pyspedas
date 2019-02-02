6097a3c91c7730d281a14d1607564028932b4034
eric <egrimes@igpp.ucla.edu>
2019-02-01 18:36:42 -0800

    Update load_routine_tests.py

--- pyspedas/mms/tests/load_routine_tests.py
+++ pyspedas/mms/tests/load_routine_tests.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-from ...mms import mms_load_fgm, mms_load_scm, mms_load_fpi, mms_load_hpca, mms_load_eis, mms_load_feeps, mms_load_edp, mms_load_aspoc
+from ...mms import mms_load_fgm, mms_load_scm, mms_load_fpi, mms_load_hpca, mms_load_eis, mms_load_feeps, mms_load_edp, mms_load_edi, mms_load_aspoc
 from ...utilities.data_exists import data_exists
 
 class FEEPSLoadTestCases(unittest.TestCase):
