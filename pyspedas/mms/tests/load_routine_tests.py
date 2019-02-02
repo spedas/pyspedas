eae0cd8bf26930d86412432a5878d17ed8bfcc05
eric <egrimes@igpp.ucla.edu>
2019-02-01 18:11:58 -0800

    Update load_routine_tests.py

--- pyspedas/mms/tests/load_routine_tests.py
+++ pyspedas/mms/tests/load_routine_tests.py
@@ -6,6 +6,36 @@ import unittest
 from ...mms import mms_load_fgm, mms_load_scm, mms_load_fpi, mms_load_hpca, mms_load_eis, mms_load_feeps, mms_load_edp, mms_load_aspoc
 from ...utilities.data_exists import data_exists
 
+class FEEPSLoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_feeps()
+        self.assertTrue(data_exists('mms1_epd_feeps_srvy_l2_electron_bottom_intensity_sensorid_12'))
+
+class EISLoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_eis()
+        self.assertTrue(data_exists('mms1_epd_eis_phxtof_proton_flux_omni'))
+
+class FPILoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_fpi()
+        self.assertTrue(data_exists('mms1_dis_energyspectr_omni_fast'))
+
+class HPCALoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_hpca()
+        self.assertTrue(data_exists('mms1_hpca_hplus_number_density'))
+
+class EDILoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_edi()
+        self.assertTrue(data_exists('mms1_edi_e_gse_srvy_l2'))
+
+class ASPOCLoadTestCases(unittest.TestCase):
+    def test_load_default_data(self):
+        data = mms_load_aspoc()
+        self.assertTrue(data_exists('mms1_aspoc_ionc'))
+
 class EDPLoadTestCases(unittest.TestCase):
     def test_load_default_data(self):
         data = mms_load_edp()
