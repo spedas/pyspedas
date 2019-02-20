42a36dde89086a6f99a08d1ae752e10b6b7f89ff
eric <egrimes@igpp.ucla.edu>
2019-02-20 12:12:34 -0800

    Now searching for and loading local data if remote data not available

--- pyspedas/mms/mms_load_data.py
+++ pyspedas/mms/mms_load_data.py
@@ -12,6 +12,7 @@ from urllib.request import urlopen
 from shutil import copyfileobj, copy
 from tempfile import NamedTemporaryFile
 from .mms_config import CONFIG
+from .mms_get_local_files import mms_get_local_files
 
 logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
@@ -39,39 +40,57 @@ def mms_load_data(trange=['2015-10-16', '2015-10-17'], probe='1', data_rate='srv
                     if dtype != '':
                         url = url + '&descriptor=' + dtype
 
-                    # query list of available files
-                    http_json = requests.get(url).json()
+                    if CONFIG['no_download'] == False:
+                        # query list of available files
+                        try:
+                            http_json = requests.get(url).json()
 
-                    for file in http_json['files']:
-                        file_date = parse(file['timetag'])
-                        out_dir = CONFIG['local_data_dir'] + os.sep.join(['mms', 'mms'+prb, instrument, drate, lvl, file_date.strftime('%Y'), file_date.strftime('%m')])
-                        out_file = os.sep.join([out_dir, file['file_name']])
+                            for file in http_json['files']:
+                                file_date = parse(file['timetag'])
+                                out_dir = CONFIG['local_data_dir'] + os.sep.join(['mms', 'mms'+prb, instrument, drate, lvl, file_date.strftime('%Y'), file_date.strftime('%m')])
+                                out_file = os.sep.join([out_dir, file['file_name']])
 
-                        if os.path.exists(out_file):
-                            logging.info('Loading ' + out_file)
-                            out_files.append(out_file)
-                            continue
+                                if os.path.exists(out_file):
+                                    logging.info('Loading ' + out_file)
+                                    out_files.append(out_file)
+                                    continue
 
-                        download_url = 'https://lasp.colorado.edu/mms/sdc/public/files/api/v1/download/science?file=' + file['file_name']
-                        logging.info('Downloading ' + file['file_name'] + ' to ' + out_dir)
-                        fsrc = urlopen(download_url)
-                        ftmp = NamedTemporaryFile(delete=False)
-                        copyfileobj(fsrc, ftmp)
+                                download_url = 'https://lasp.colorado.edu/mms/sdc/public/files/api/v1/download/science?file=' + file['file_name']
+                                logging.info('Downloading ' + file['file_name'] + ' to ' + out_dir)
+                                fsrc = urlopen(download_url)
+                                ftmp = NamedTemporaryFile(delete=False)
+                                copyfileobj(fsrc, ftmp)
 
-                        if not os.path.exists(out_dir):
-                            os.makedirs(out_dir)
+                                if not os.path.exists(out_dir):
+                                    os.makedirs(out_dir)
 
-                        # if the download was successful, copy to data directory
-                        copy(ftmp.name, out_file)
-                        out_files.append(out_file)
+                                # if the download was successful, copy to data directory
+                                copy(ftmp.name, out_file)
+                                out_files.append(out_file)
+                        except requests.exceptions.ConnectionError:
+                            # No/bad internet connection; try loading the files locally
+                            logging.error('No internet connection!')
+
+                    
+                    if out_files == []:
+                        logging.info('Searching for local files...')
+                        out_files = mms_get_local_files(prb, instrument, drate, lvl, dtype, trange)
 
     out_files = sorted(out_files)
 
     new_variables = cdf_to_tplot(out_files, merge=True, get_support_data=get_support_data, prefix=prefix, suffix=suffix)
+
+    if new_variables == []:
+        logging.warning('No data loaded.')
+        return
 
     logging.info('Loaded variables:')
     for new_var in new_variables:
         print(new_var)
 
     return new_variables
+
+
+
+
 
