28aa0bb29783003ee091bf1ac8a4692c839f0ceb
eric <egrimes@igpp.ucla.edu>
2019-02-20 12:56:03 -0800

    Update mms_get_local_files.py

--- pyspedas/mms/mms_get_local_files.py
+++ pyspedas/mms/mms_get_local_files.py
@@ -19,15 +19,20 @@ def mms_get_local_files(probe, instrument, data_rate, level, datatype, trange):
 
     files_out = []
 
-    file_name = 'mms'+probe+'_'+instrument+'_'+data_rate+'_'+level+'(_)?_([0-9]{8,14})_v(\d+).(\d+).(\d+).cdf'
+    file_name = 'mms'+probe+'_'+instrument+'_'+data_rate+'_'+level+'(_)?.*_([0-9]{8,14})_v(\d+).(\d+).(\d+).cdf'
 
     days = rrule(DAILY, dtstart=parse(trange[0]), until=parse(trange[1])-timedelta(seconds=1))
 
+    if datatype == '' or datatype == None:
+        level_and_dtype = level
+    else:
+        level_and_dtype = os.sep.join([level, datatype])
+
     for date in days:
         if data_rate == 'brst':
-            local_dir = os.sep.join([CONFIG['local_data_dir'], 'mms', 'mms'+probe, instrument, data_rate, level, date.strftime('%Y'), date.strftime('%m'), date.strftime('%d')])
+            local_dir = os.sep.join([CONFIG['local_data_dir'], 'mms', 'mms'+probe, instrument, data_rate, level_and_dtype, date.strftime('%Y'), date.strftime('%m'), date.strftime('%d')])
         else:
-            local_dir = os.sep.join([CONFIG['local_data_dir'], 'mms', 'mms'+probe, instrument, data_rate, level, date.strftime('%Y'), date.strftime('%m')])
+            local_dir = os.sep.join([CONFIG['local_data_dir'], 'mms', 'mms'+probe, instrument, data_rate, level_and_dtype, date.strftime('%Y'), date.strftime('%m')])
 
         full_path = os.sep.join([local_dir, file_name])
         # /Users/eric/data/mms/mms1/fgm/srvy/l2/2015/12/mms1_fgm_srvy_l2_20151215_v4.18.0.cdf
@@ -36,6 +41,7 @@ def mms_get_local_files(probe, instrument, data_rate, level, datatype, trange):
         for root, dirs, files in os.walk(os.sep.join([CONFIG['local_data_dir'], 'mms'])):
             for file in files:
                 this_file = os.sep.join([root, file])
+                #print('--- ' + this_file)
                 matches = regex.match(this_file)
                 if matches:
                     this_time = parse(matches.groups()[1])
