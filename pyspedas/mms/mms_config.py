9c2ce5d937e79d74736c22ff2c84e08828784d92
eric <egrimes@igpp.ucla.edu>
2019-02-24 14:01:11 -0800

    Update mms_config.py

--- pyspedas/mms/mms_config.py
+++ pyspedas/mms/mms_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-CONFIG = {'local_data_dir': '/pydata',
+CONFIG = {'local_data_dir': 'pydata',
           'debug_mode': False,
           'no_download': False}
