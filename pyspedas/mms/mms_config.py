d80a1c2d1f3143cf8b73c5366ffd8388d53b2930
supervised <egrimes@igpp.ucla.edu>
2019-02-26 17:50:48 -0800

    Now allows environment variables for the local data directory

--- pyspedas/mms/mms_config.py
+++ pyspedas/mms/mms_config.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+
 CONFIG = {'local_data_dir': 'pydata',
           'debug_mode': False,
           'no_download': False}
+
+# override local data directory with environment variables
+if os.environ.get('ROOT_DATA_DIR'):
+	CONFIG['local_data_dir'] = os.sep.join([os.environ['ROOT_DATA_DIR'], 'mms'])
+
+if os.environ.get('MMS_DATA_DIR'):
+	CONFIG['local_data_dir'] = os.environ['MMS_DATA_DIR']
