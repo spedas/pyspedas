d544cdefd6e16a7c65cf85ec620ed37f20ecf323
eric <egrimes@igpp.ucla.edu>
2019-06-25 11:53:10 -0700

    Adding initial EIS crib sheet

---
+++ pyspedas/mms/examples/basic/mms_load_eis_crib.py
@@ -0,0 +1,20 @@
+'''
+
+This crib sheet shows how to load and plot EIS data
+
+'''
+
+from pyspedas import mms_load_hpca, tnames
+from pytplot import tplot
+
+# load ExTOF data
+mms_load_eis(trange=['2015-10-16', '2015-10-17'], datatype='extof')
+
+# plot the H+ flux
+tplot(tnames('*_extof_proton_flux_omni'))
+
+# load PHxTOF data
+mms_load_eis(trange=['2015-10-16', '2015-10-17'], datatype='phxtof')
+
+# plot the H+ flux
+tplot(tnames('*_phxtof_proton_flux_omni'))
