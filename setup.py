ba20d9c0729bba18e743580b6362909b0921572e
Nikos H <nikos@berkeley.edu>
2019-06-25 15:21:52 -0700

    Minor simplification

--- setup.py
+++ setup.py
@@ -8,21 +8,13 @@ https://github.com/pypa/sampleproject
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 
 setup(
     name='pyspedas',
     version='0.8.4',
     description='Python Space Physics Environment Data Analysis \
                     Software (SPEDAS)',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/spedas/pyspedas',
     author='Nick Hatzigeorgiu, Eric Grimes',
