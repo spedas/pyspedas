4d0fbad8a57eced7d12746edc3355122b153414f
eric <egrimes@igpp.ucla.edu>
2019-09-10 12:11:03 -0700

    Updated to use latest channels (May 2019)

--- pyspedas/mms/feeps/mms_feeps_remove_bad_data.py
+++ pyspedas/mms/feeps/mms_feeps_remove_bad_data.py
@@ -98,21 +98,43 @@ def mms_feeps_remove_bad_data(probe = '1', data_rate = 'srvy', datatype = 'elect
 
     bad_vars = []
     bad_vars_both_chans = []
+    bad_vars_3_chans = []
 
+    # -- the following are the pre-May 2019 masks
+    # bad_ch0 = {}
+    # bad_ch0['mms1'] = {'top': [2, 5, 6, 7], 'bottom': [2, 3, 4, 5, 6, 7, 8, 9, 11, 12]}
+    # bad_ch0['mms2'] = {'top': [1, 2, 3, 4, 5, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12]}
+    # bad_ch0['mms3'] = {'top': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12]}
+    # bad_ch0['mms4'] = {'top': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+
+    # bad_ch1 = {}
+    # bad_ch1['mms1'] = {'top': [], 'bottom': [11]}
+    # bad_ch1['mms2'] = {'top': [8], 'bottom': [12]}
+    # bad_ch1['mms3'] = {'top': [1], 'bottom': []}
+    # bad_ch1['mms4'] = {'top': [1], 'bottom': [6, 9]}
+
+    # -- the following are the post-May 2019 masks
     bad_ch0 = {}
-    bad_ch0['mms1'] = {'top': [2, 5, 6, 7], 'bottom': [2, 3, 4, 5, 6, 7, 8, 9, 11, 12]}
-    bad_ch0['mms2'] = {'top': [1, 2, 3, 4, 5, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12]}
-    bad_ch0['mms3'] = {'top': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12]}
-    bad_ch0['mms4'] = {'top': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+    bad_ch0['mms1'] = {'top': [2, 5, 6], 'bottom': [2, 3, 4, 5, 6, 8, 9, 11, 12]}
+    bad_ch0['mms2'] = {'top': [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]}
+    bad_ch0['mms3'] = {'top': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12]}
+    bad_ch0['mms4'] = {'top': [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12], 'bottom': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
 
     bad_ch1 = {}
-    bad_ch1['mms1'] = {'top': [], 'bottom': [11]}
-    bad_ch1['mms2'] = {'top': [8], 'bottom': [12]}
-    bad_ch1['mms3'] = {'top': [1], 'bottom': []}
-    bad_ch1['mms4'] = {'top': [1], 'bottom': [6, 9]}
+    bad_ch1['mms1'] = {'top': [6], 'bottom': [6, 11]}
+    bad_ch1['mms2'] = {'top': [8], 'bottom': [7, 12]}
+    bad_ch1['mms3'] = {'top': [1, 6, 7], 'bottom': [6, 7]}
+    bad_ch1['mms4'] = {'top': [1, 6], 'bottom': [6, 7, 8, 9]}
+
+    bad_ch2 = {}
+    bad_ch2['mms1'] = {'top': [], 'bottom': []}
+    bad_ch2['mms2'] = {'top': [], 'bottom': []}
+    bad_ch2['mms3'] = {'top': [], 'bottom': []}
+    bad_ch2['mms4'] = {'top': [], 'bottom': [6, 7]}
 
     bad_ch0 = bad_ch0['mms'+str(probe)]
     bad_ch1 = bad_ch1['mms'+str(probe)]
+    bad_ch2 = bad_ch2['mms'+str(probe)]
 
     #### bottom channel
 
@@ -175,6 +197,37 @@ def mms_feeps_remove_bad_data(probe = '1', data_rate = 'srvy', datatype = 'elect
         bad_vars_both_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_bottom_intensity_sensorid_'+str(bad_var)+suffix))
         bad_vars_both_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_bottom_counts_sensorid_'+str(bad_var)+suffix))
 
+
+    #### bottom 3 channels
+
+    # top electrons
+    for bad_var in bad_ch2['top']:
+        if bad_var in [6, 7, 8]: continue # ion eyes
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_top_count_rate_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_top_intensity_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_top_counts_sensorid_'+str(bad_var)+suffix))
+
+    # bottom electrons
+    for bad_var in bad_ch2['bottom']:
+        if bad_var in [6, 7, 8]: continue # ion eyes
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_bottom_count_rate_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_bottom_intensity_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_electron_bottom_counts_sensorid_'+str(bad_var)+suffix))
+
+    # top ions
+    for bad_var in bad_ch2['top']:
+        if bad_var not in [6, 7, 8]: continue # ion eyes
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_top_count_rate_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_top_intensity_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_top_counts_sensorid_'+str(bad_var)+suffix))
+
+    # bottom ions
+    for bad_var in bad_ch2['bottom']:
+        if bad_var not in [6, 7, 8]: continue # ion eyes
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_bottom_count_rate_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_bottom_intensity_sensorid_'+str(bad_var)+suffix))
+        bad_vars_3_chans.append(tnames('mms'+str(probe)+'_epd_feeps_'+data_rate_level+'_ion_bottom_counts_sensorid_'+str(bad_var)+suffix))
+
     # set the first energy channel to NaN
     for bad_var in bad_vars:
         if bad_var == []: continue
@@ -200,4 +253,19 @@ def mms_feeps_remove_bad_data(probe = '1', data_rate = 'srvy', datatype = 'elect
 
             data[:, 0] = np.nan
             data[:, 1] = np.nan
+            pytplot.store_data(bad_var[0], data={'x': times, 'y': data, 'v': energies})
+
+    # set the bottom 3 energy channels to NaN
+    for bad_var in bad_vars_3_chans:
+        if bad_var == []: continue
+        bad_var_data = pytplot.get_data(bad_var[0])
+        if bad_var_data is not None:
+            times, data, energies = bad_var_data
+
+            # check if the energy table contains all names
+            if np.isnan(np.sum(energies)): continue
+
+            data[:, 0] = np.nan
+            data[:, 1] = np.nan
+            data[:, 2] = np.nan
             pytplot.store_data(bad_var[0], data={'x': times, 'y': data, 'v': energies})
