fd108095655743dc892c9847ff7615aba7eb9ec4
eric <egrimes@igpp.ucla.edu>
2019-09-25 10:54:12 -0700

    adding PAD example

--- pyspedas/mms/examples/basic/mms_load_feeps_crib.py
+++ pyspedas/mms/examples/basic/mms_load_feeps_crib.py
@@ -4,7 +4,7 @@ This crib sheet shows how to load and plot FEEPS data
 
 '''
 
-from pyspedas import mms_load_feeps
+from pyspedas import mms_load_feeps, mms_feeps_pad
 from pytplot import tplot
 
 # load electron data
@@ -12,3 +12,9 @@ mms_load_feeps(trange=['2015-10-16', '2015-10-17'], datatype='electron')
 
 # plot the omni-directional electron intensity
 tplot(['mms1_epd_feeps_srvy_l2_electron_intensity_omni', 'mms1_epd_feeps_srvy_l2_electron_intensity_omni_spin'])
+
+# calculate the pitch angle distribution
+mms_feeps_pad()
+
+# plot the PAD
+tplot('mms1_epd_feeps_srvy_l2_electron_intensity_70-600keV_pad')
