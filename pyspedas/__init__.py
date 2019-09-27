23fdd07474b217e3557e26b79da6c61a4b112b03
eric <egrimes@igpp.ucla.edu>
2019-09-27 14:08:55 -0700

    Adding EIS PAD routine

--- pyspedas/__init__.py
+++ pyspedas/__init__.py
@@ -33,4 +33,5 @@ from .spdtplot.tplot_names import tplot_names
 from .mms import mms_load_mec, mms_load_fgm, mms_load_scm, mms_load_edi, mms_load_edp, mms_load_eis, mms_load_feeps, \
     mms_load_hpca, mms_load_fpi, mms_load_aspoc, mms_load_dsp, mms_load_fsm
 from .mms.feeps.mms_feeps_pad import mms_feeps_pad
+from .mms.eis.mms_eis_pad import mms_eis_pad
 from .maven import maven_load
