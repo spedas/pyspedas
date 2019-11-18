818408dabd20da57cf1076dec681e660289f1938
eric <egrimes@igpp.ucla.edu>
2019-11-18 11:32:32 -0800

    Now setting HPCA spectra metadata

--- pyspedas/mms/hpca/mms_hpca_spin_sum.py
+++ pyspedas/mms/hpca/mms_hpca_spin_sum.py
@@ -5,6 +5,7 @@ from pyspedas import tnames
 def mms_hpca_spin_sum(probe = '1', datatypes=None, species=['hplus', 'oplus', 'oplusplus', 'heplus', 'heplusplus'], fov=['0', '360'], avg=False):
     """
     This function will sum (or average, when the avg keyword is set to True) the HPCA data over each spin
+    
     Parameters:
         fov : list of str
             field of view, in angles, from 0-360
@@ -28,10 +29,12 @@ def mms_hpca_spin_sum(probe = '1', datatypes=None, species=['hplus', 'oplus', 'o
 
     spin_starts = np.argwhere(start_az == 0)
     output_vars = []
+    species_map = {'hplus': 'H+', 'oplus': 'O+', 'heplus': 'He+', 'heplusplus': 'He++'}
 
     for datatype in datatypes:
         vars_to_sum = tnames(datatype+'_elev_'+fov[0]+'-'+fov[1])
         for var in vars_to_sum:
+            var_species = var.split('_')[2]
             data = get_data(var, xarray=True)
             out_data = []
             for i, spin_start in enumerate(spin_starts[:-1]):
@@ -45,5 +48,7 @@ def mms_hpca_spin_sum(probe = '1', datatypes=None, species=['hplus', 'oplus', 'o
             options(var+'_spin', 'ylog', True)
             options(var+'_spin', 'zlog', True)
             options(var+'_spin', 'Colormap', 'jet')
+            options(var+fov_str, 'ztitle', species_map[var_species] + ' ' + var.split('_')[3] + ' (cm^2-s-sr-eV)^-1')
+            options(var+fov_str, 'ytitle', species_map[var_species] + ' Energy (eV)')
             output_vars.append(var+'_spin')
     return output_vars
