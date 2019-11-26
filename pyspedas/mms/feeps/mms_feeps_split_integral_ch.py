76c189052e1cc876b9527ea8103255a814814feb
eric <egrimes@igpp.ucla.edu>
2019-11-26 12:40:19 -0800

    adding docstring

--- pyspedas/mms/feeps/mms_feeps_split_integral_ch.py
+++ pyspedas/mms/feeps/mms_feeps_split_integral_ch.py
@@ -1,6 +1,39 @@
 import pytplot
 
 def mms_feeps_split_integral_ch(units_type, species, probe, suffix='', data_rate='srvy', level='l2', sensor_eyes=None):
+    """
+    This function splits the last integral channel from the FEEPS spectra, 
+    creating 2 new tplot variables:
+
+       [original variable]_clean - spectra with the integral channel removed
+       [original variable]_500keV_int - the integral channel that was removed
+    
+    Parameters:
+        units_type: str
+            instrument datatype, e.g., 'intensity'
+
+        species: str
+            'electron' or 'ion'
+
+        probe: str
+            probe #, e.g., '4' for MMS4
+
+        suffix: str
+            suffix of the loaded data
+
+        data_rate: str
+            instrument data rate, e.g., 'srvy' or 'brst'
+
+        level: str
+            data level
+
+        sensor_eyes: dict
+            Hash table containing the active sensor eyes
+
+    Returns:
+        List of tplot variables created.
+    """
+
     if sensor_eyes is None:
         print('Error: sensor_eyes not defined')
         return
