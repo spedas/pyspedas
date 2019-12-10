094e3797f3da0f7ccdf9606e4794fe04f622b6ee
Bryan Harter <bryan.harter@lasp.colorado.edu>
2019-12-09 17:34:48 -0800

    Updating the MAVEN load functionality

--- pyspedas/maven/maven_load.py
+++ pyspedas/maven/maven_load.py
@@ -20,12 +20,14 @@ def maven_filenames(filenames=None,
                     update_prefs=False,
                     only_update_prefs=False,
                     local_dir=None):
-    """
-    This function identifies which MAVEN data to download.
-    """
+    '''
+    This function queries the MAVEN SDC API, and will return a list of files that match the inputs above.
+    '''
 
     # Check for orbit num rather than time string
     if isinstance(start_date, int) and isinstance(end_date, int):
+        print("Orbit numbers specified, checking for updated orbit # file from naif.jpl.nasa.gov")
+        get_orbit_files()
         start_date, end_date = orbit_time(start_date, end_date)
         start_date = parse(start_date)
         end_date = parse(end_date)
@@ -33,24 +35,19 @@ def maven_filenames(filenames=None,
         end_date = end_date.replace(day=end_date.day+1, hour=0, minute=0, second=0)
         start_date = start_date.strftime('%Y-%m-%d')
         end_date = end_date.strftime('%Y-%m-%d')
-        
+
+    # Update the preference directory
     if update_prefs or only_update_prefs:
         set_new_data_root_dir()
         if only_update_prefs:
             return
-    
+
+    # Check for public vs private access
     public = get_access()
     if not public:
         get_uname_and_password()
 
-    if filenames is None:
-        if insitu and iuvs:
-            print("Can't request both INSITU and IUVS in one query.")
-            return
-        if not insitu and not iuvs:
-            print("If not specifying filename(s) to download, Must specify either insitu=True or iuvs=True.")
-            return
-        
+    # If no instruments are specified, default to the KP data set
     if instruments is None:
         instruments = ['kp']
         if insitu:
@@ -138,22 +135,26 @@ def load_data(filenames=None,
     This function downloads MAVEN data loads it into tplot variables, if applicable.
     """
 
-    # 1. Download files
-
+    # 1. Get a list of MAVEN files queries from the above seach parameters
     maven_files = maven_filenames(filenames, instruments, level, insitu, iuvs, start_date, end_date, update_prefs,
                                   only_update_prefs, local_dir)
+
+    # If we are not asking for KP data, this flag ensures only ancillary data is loaded in from the KP files
     if instruments != 'kp':
-        ancillary_only=True
+        ancillary_only = True
     else:
         ancillary_only = False
 
+    # Convert to list
     if not isinstance(type, list):
         type = [type]
 
     # Keep track of what files are downloaded
     files_to_load = []
 
+    # Loop through all instruments, download files locally if needed
     for instr in maven_files.keys():
+        bn_files_to_load = []
         if maven_files[instr]:
             s = maven_files[instr][0]
             data_dir = maven_files[instr][1]
@@ -176,7 +177,7 @@ def load_data(filenames=None,
                     full_path = create_dir_if_needed(f, data_dir, 'insitu')
                 else:
                     full_path = create_dir_if_needed(f, data_dir, level)
-
+                bn_files_to_load.append(f)
                 files_to_load.append(os.path.join(full_path, f))
 
             if list_files:
@@ -186,9 +187,9 @@ def load_data(filenames=None,
 
             if new_files:
                 if instr == 'kp':
-                    s = get_new_files(s, data_dir, instr, 'insitu')
+                    s = get_new_files(bn_files_to_load, data_dir, instr, 'insitu')
                 else:
-                    s = get_new_files(s, data_dir, instr, level)
+                    s = get_new_files(bn_files_to_load, data_dir, instr, level)
             if len(s) == 0:
                 continue
             print("Your request will download a total of: "+str(len(s))+" files for instrument "+str(instr))
@@ -209,11 +210,6 @@ def load_data(filenames=None,
 
             if cancel:
                 continue
-
-            if not exclude_orbit_file:
-                print("Before downloading data files, checking for updated orbit # file from naif.jpl.nasa.gov")
-                print("")
-                get_orbit_files()
 
             i = 0
             display_progress(i, len(s))
@@ -242,18 +238,29 @@ def load_data(filenames=None,
 
         loaded_tplot_vars = []
         if not download_only:
-            # Create tplot variables
+
             for f in cdf_files:
+                # Loop through CDF files
                 desc = l2_regex.match(os.path.basename(f)).group("description")
                 if desc is not '' and suffix == '':
-                    loaded_tplot_vars.append(pytplot.cdf_to_tplot(f, varformat=varformat,
+                    created_vars = pytplot.cdf_to_tplot(f, varformat=varformat,
                                                                  get_support_data=get_support_data, prefix=prefix,
-                                                                 suffix=desc, merge=True))
+                                                                 suffix=desc, merge=True)
                 else:
-                    loaded_tplot_vars.append(pytplot.cdf_to_tplot(f, varformat=varformat,
+                    created_vars = pytplot.cdf_to_tplot(f, varformat=varformat,
                                                                   get_support_data=get_support_data, prefix=prefix,
-                                                                  suffix=suffix, merge=True))
+                                                                  suffix=suffix, merge=True)
+
+                # Specifically for SWIA and SWEA data, make sure the plots have log axes and are spectrograms
+                instr = l2_regex.match(os.path.basename(f)).group("instrument")
+                if instr in ["swi", "swe"]:
+                    pytplot.options(created_vars, 'ylog', 1)
+                    pytplot.options(created_vars, 'zlog', 1)
+                    pytplot.options(created_vars, 'spec', 1)
+                loaded_tplot_vars.append(created_vars)
+
             for f in sts_files:
+                # Loop through STS (Mag) files
                 desc = l2_regex.match(os.path.basename(f)).group("description")
                 if desc is not '' and suffix == '':
                     loaded_tplot_vars.append(pytplot.sts_to_tplot(f, prefix=prefix,
@@ -262,10 +269,22 @@ def load_data(filenames=None,
                     loaded_tplot_vars.append(pytplot.sts_to_tplot(f, prefix=prefix,
                                                                   suffix=suffix, merge=True))
 
-            loaded_tplot_vars.append(maven_kp_to_tplot(filename=kp_files, ancillary_only=True))
-            flat_list = [item for sublist in loaded_tplot_vars for item in sublist]
+                # Remove the Decimal Day column, not really useful
+                for tvar in loaded_tplot_vars:
+                    if "DDAY_" in tvar:
+                        pytplot.del_data(tvar)
+                        del tvar
 
+            # Flatten out the list and only grab the unique tplot variables
+            flat_list = list(set([item for sublist in loaded_tplot_vars for item in sublist]))
+
+            # Load in KP data specifically for all of the Ancillary data (position, attitude, Ls, etc)
+            kp_data_loaded = maven_kp_to_tplot(filename=kp_files, ancillary_only=True)
+
+            # Link all created tplot variables to the corresponding KP data
             for tvar in flat_list:
+                if tvar=='data_lpnt':
+                    x=2
                 pytplot.link(tvar, "mvn_kp::spacecraft::altitude", link_type='alt')
                 pytplot.link(tvar, "mvn_kp::spacecraft::mso_x", link_type='x')
                 pytplot.link(tvar, "mvn_kp::spacecraft::mso_y", link_type='y')
@@ -276,4 +295,17 @@ def load_data(filenames=None,
                 pytplot.link(tvar, "mvn_kp::spacecraft::sub_sc_longitude", link_type='lon')
                 pytplot.link(tvar, "mvn_kp::spacecraft::sub_sc_latitude", link_type='lat')
 
-            return list(set(flat_list))
+            # Link all created KP data to the ancillary KP data
+            for tvar in kp_data_loaded:
+                pytplot.link(tvar, "mvn_kp::spacecraft::altitude", link_type='alt')
+                pytplot.link(tvar, "mvn_kp::spacecraft::mso_x", link_type='x')
+                pytplot.link(tvar, "mvn_kp::spacecraft::mso_y", link_type='y')
+                pytplot.link(tvar, "mvn_kp::spacecraft::mso_z", link_type='z')
+                pytplot.link(tvar, "mvn_kp::spacecraft::geo_x", link_type='geo_x')
+                pytplot.link(tvar, "mvn_kp::spacecraft::geo_y", link_type='geo_y')
+                pytplot.link(tvar, "mvn_kp::spacecraft::geo_z", link_type='geo_z')
+                pytplot.link(tvar, "mvn_kp::spacecraft::sub_sc_longitude", link_type='lon')
+                pytplot.link(tvar, "mvn_kp::spacecraft::sub_sc_latitude", link_type='lat')
+
+            # Return list of unique KP data
+            return flat_list
