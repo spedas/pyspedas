7785ff8577b758b35796ddcc67425d7a96489168
Nikos H <nikos@berkeley.edu>
2020-02-06 15:14:56 -0800

    Added comments.

--- pyspedas/examples/basic/ex_dsl2gse.py
+++ pyspedas/examples/basic/ex_dsl2gse.py
@@ -4,7 +4,29 @@ File:
     ex_dsl2gse.py
 
 Description:
-    Read tha_fgl_dsl and perform a dsl to gse transformation.
+    Load tha_fgl_dsl and perform a dsl to gse transformation.
+
+Notes:
+    To duplicate this example on IDL SPEDAS, run the following code:
+
+    pro thm_crib_fgm4
+
+        timespan, '2017-03-23', 1
+        thm_load_state, probe='a', /get_support_data
+        thm_load_fgm, lev=2, probe=['a']
+
+        dsl2gse, 'tha_fgl_dsl', 'tha_state_spinras', 'tha_state_spindec',$
+            'tha_fgl_gse'
+
+        get_data, 'tha_fgl_dsl', data=d1
+        get_data, 'tha_fgl_gse', data=d2
+        z1 = d1.y[*,2]
+        z2 = d2.y[*,2]
+        store_data, 'z_dsl', d1.x, z1
+        store_data, 'z_gse', d2.x, z2
+
+        tplot, ['tha_fgl_dsl', 'tha_fgl_gse', 'z_dsl', 'z_gse']
+    end
 
 """
 import pyspedas
