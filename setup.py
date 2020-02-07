bab67174fb823533bfa3b258cff6842c029813b1
Nikos H <nikos@berkeley.edu>
2020-02-06 21:31:39 -0800

    Version 0.9.12

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='0.9.11',
+    version='0.9.12',
     description='Python Space Physics Environment Data Analysis \
                     Software (SPEDAS)',
     long_description=open('README.md').read(),
