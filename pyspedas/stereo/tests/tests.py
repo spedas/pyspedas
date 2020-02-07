45e17a06ddb8ebd41892789b70dcea4081c20f6e
eric <egrimes@igpp.ucla.edu>
2020-02-07 14:01:26 -0800

    Update tests.py

--- pyspedas/stereo/tests/tests.py
+++ pyspedas/stereo/tests/tests.py
@@ -9,5 +9,11 @@ class LoadTestCases(unittest.TestCase):
         mag_vars = pyspedas.stereo.mag(trange=['2013-11-5', '2013-11-6'])
         self.assertTrue(data_exists('BFIELD'))
 
+    def test_load_plastic_data(self):
+        p_vars = pyspedas.stereo.plastic(trange=['2013-11-5', '2013-11-6'])
+        self.assertTrue(data_exists('proton_number_density'))
+        self.assertTrue(data_exists('proton_bulk_speed'))
+        self.assertTrue(data_exists('proton_temperature'))
+
 if __name__ == '__main__':
     unittest.main()
