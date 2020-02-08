19095ee47834e81ea560e3d9919e6a5a2268267c
eric <egrimes@igpp.ucla.edu>
2020-02-08 10:04:14 -0800

    Adding initial FAST tests

---
+++ pyspedas/fast/tests/tests.py
@@ -0,0 +1,26 @@
+
+import unittest
+from pyspedas.utilities.data_exists import data_exists
+
+import pyspedas
+
+class LoadTestCases(unittest.TestCase):
+    def test_load_dcb_data(self):
+        dcb_vars = pyspedas.fast.dcb(trange=['1999-09-22', '1999-09-23'], time_clip=True, level='k0')
+        self.assertTrue(data_exists('BX'))
+        self.assertTrue(data_exists('BY'))
+        self.assertTrue(data_exists('BZ'))
+
+    # as of 8 Feb 2020, these data fail to load in IDL and Python
+    # def test_load_esa_data(self):
+    #     esa_vars = pyspedas.fast.esa()
+    #     self.assertTrue(data_exists(''))
+
+    def test_load_teams_data(self):
+        teams_vars = pyspedas.fast.teams()
+        self.assertTrue(data_exists('H+'))
+        self.assertTrue(data_exists('O+'))
+        self.assertTrue(data_exists('He+'))
+
+if __name__ == '__main__':
+    unittest.main()
