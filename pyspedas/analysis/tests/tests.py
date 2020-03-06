f8503a4fb8feb3d8071b2360f2f3854fd81efde1
nickssl <nikos@berkeley.edu>
2020-03-06 09:19:29 -0800

    Update tests,

--- pyspedas/analysis/tests/tests.py
+++ pyspedas/analysis/tests/tests.py
@@ -10,7 +10,7 @@ Description:
 
 import unittest
 from pyspedas.analysis.tsmooth import smooth
-from pyspedas import subtract_average, subtract_median
+from pyspedas import subtract_average, subtract_median, tsmooth
 from pytplot import get_data, store_data
 
 import numpy as np
@@ -26,13 +26,13 @@ class AnalysisTestCases(BaseTestCase):
 
     def test_subtract_median(self):
         subtract_median('test')
-        t, d = get_data('test-m')
-        self.assertTrue(d.tolist() == [-3.5, -1.5,  1.5,  8.5, 13.5, -5.5])
+        d = get_data('test-m')
+        self.assertTrue(d[1].tolist() == [-3.5, -1.5,  1.5,  8.5, 13.5, -5.5])
 
     def test_subtract_average(self):
         subtract_average('test')
-        t, d = get_data('test-d')
-        self.assertTrue((np.round(d.tolist()) == [-6., -4., -1.,
+        d = get_data('test-d')
+        self.assertTrue((np.round(d[1].tolist()) == [-6., -4., -1.,
                          6., 11., -8.]).all())
 
     def test_tsmooth(self):
@@ -46,6 +46,9 @@ class AnalysisTestCases(BaseTestCase):
         ry = [1.0, 1.3333333333333333, 2.0, 1.6666666666666665, 1.0,
               np.nan, np.nan, 0.6666666666666666, 1.0, 1.3333333333333333, 1.0]
         self.assertTrue(y == ry)
+        tsmooth('test')
+        d = get_data('test-s')
+        self.assertTrue(d[1].tolist() == [3.,  5.,  8., 15., 20.,  1.])
 
 
 if __name__ == '__main__':
