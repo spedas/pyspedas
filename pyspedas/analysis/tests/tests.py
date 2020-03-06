aacb3224f5bb7e25bd4ecb962656381c88b93412
nickssl <nikos@berkeley.edu>
2020-03-05 18:25:35 -0800

    Updated tests.

--- pyspedas/analysis/tests/tests.py
+++ pyspedas/analysis/tests/tests.py
@@ -1,17 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+File:
+    test.py
+
+Description:
+    Automated tests for the analysis functions.
+
+"""
 
 import unittest
-from pyspedas.utilities.data_exists import data_exists
-
-from pyspedas import subtract_average, subtract_median, yclip, tdeflag, time_clip, tinterpol, tdpwrspc, time_double, time_string
+from pyspedas.analysis.tsmooth import smooth
+from pyspedas import subtract_average, subtract_median
 from pytplot import get_data, store_data
 
 import numpy as np
 
+
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        store_data('test', data={'x': [1., 2., 3., 4., 5., 6.], 'y': [3., 5., 8., 15., 20., 1.]})
+        store_data('test', data={'x': [1., 2., 3., 4., 5., 6.],
+                                 'y': [3., 5., 8., 15., 20., 1.]})
+
 
 class AnalysisTestCases(BaseTestCase):
+
     def test_subtract_median(self):
         subtract_median('test')
         t, d = get_data('test-m')
@@ -20,7 +32,21 @@ class AnalysisTestCases(BaseTestCase):
     def test_subtract_average(self):
         subtract_average('test')
         t, d = get_data('test-d')
-        self.assertTrue((np.round(d.tolist()) == [-6., -4., -1.,  6., 11., -8.]).all())
+        self.assertTrue((np.round(d.tolist()) == [-6., -4., -1.,
+                         6., 11., -8.]).all())
+
+    def test_tsmooth(self):
+        a = [1.0, 1.0, 2.0, 3.0, 4.0, 1.0, 4.0, 3.0, 2.0, 1.0, 1.0]
+        x = smooth(a, 3)
+        r = [1.0, 1.3333333333333333, 2.0, 3.0, 2.6666666666666665,
+             3.0, 2.6666666666666665, 3.0, 2.0, 1.3333333333333333, 1.0]
+        self.assertTrue(x == r)
+        b = [1.0, 1.0, 2.0, 3.0, np.NaN, np.NaN, np.NaN, np.NaN, 2.0, 1.0, 1.0]
+        y = smooth(b, width=3)
+        ry = [1.0, 1.3333333333333333, 2.0, 1.6666666666666665, 1.0,
+              np.nan, np.nan, 0.6666666666666666, 1.0, 1.3333333333333333, 1.0]
+        self.assertTrue(y == ry)
+
 
 if __name__ == '__main__':
     unittest.main()
