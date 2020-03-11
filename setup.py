c4381591116d603170bcb845c5c7f74c6bb47612
nickssl <nikos@berkeley.edu>
2020-03-11 12:52:24 -0700

    Update to latest libraries.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='0.9.17',
+    version='0.9.18',
     description='Python Space Physics Environment Data Analysis \
                     Software (SPEDAS)',
     long_description=open('README.md').read(),
@@ -30,8 +30,8 @@ setup(
     project_urls={'Information': 'http://spedas.org/wiki/',
                   },
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['requests', 'pytplot>=1.5.4', 'cdflib>=0.3.17',
-                      'cdasws', 'netCDF4'],
+    install_requires=['requests', 'pytplot>=1.5.4', 'cdflib>=0.3.18',
+                      'cdasws>=1.7.17', 'netCDF4'],
     python_requires='>=3.5',
     include_package_data=True,
 )
