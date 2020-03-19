96c91865066105b2adbfdeeb92739d6a51b76017
eric <egrimes@igpp.ucla.edu>
2020-03-19 12:45:44 -0700

    trying a different version of pyqt5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
                   },
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['requests', 'pytplot>=1.5.4', 'cdflib>=0.3.18',
-                      'cdasws>=1.7.17', 'netCDF4', 'pyqt5==5.12', 
+                      'cdasws>=1.7.17', 'netCDF4', 'pyqt5==5.11.2', 
                       'pyqtwebengine==5.12'],
     python_requires='>=3.5',
     include_package_data=True,
