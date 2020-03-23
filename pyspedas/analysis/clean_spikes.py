747e0bd8d65ff85a1d2559e4336bd0943cb97a35
nickssl <nikos@berkeley.edu>
2020-03-22 21:59:27 -0700

    Added clean_spikes and an example.

---
+++ pyspedas/analysis/clean_spikes.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+File:
+    clean_spikes.py
+
+Description:
+    Creates a new tplot variable that has spikes removed.
+
+Parameters:
+    names: str/list of str
+        List of pytplot names.
+    new_names: str/list of str
+        List of new_names for pytplot variables.
+        If not given, then a suffix is applied.
+    suffix: str
+        A suffix to apply. Default is '-avg'.
+    overwrite: bool
+        Replace the existing tplot name.
+    nsmooth: int
+        the number of data points for smoothing
+    thresh: float
+        threshold value
+    sub_avg: bool
+        if set, subtract the average value of the data
+        prior to checking for spikes
+
+Notes:
+    Similar to clean_spikes.pro in IDL SPEDAS.
+"""
+
+import numpy as np
+import pyspedas
+import pytplot
+from pyspedas.analysis.subtract_average import subtract_average
+from pyspedas.analysis.tsmooth import tsmooth
+
+
+def clean_spikes(names, nsmooth=10, thresh=0.3, sub_avg=False,
+                 new_names=None, suffix=None, overwrite=None):
+
+    old_names = pyspedas.tnames(names)
+
+    if len(old_names) < 1:
+        print('clean_spikes error: No pytplot names were provided.')
+        return
+
+    if suffix is None:
+        suffix = '-despike'
+
+    if overwrite is not None:
+        n_names = old_names
+    elif new_names is None:
+        n_names = [s + suffix for s in old_names]
+    else:
+        n_names = new_names
+
+    if isinstance(n_names, str):
+        n_names = [n_names]
+
+    if len(n_names) != len(old_names):
+        n_names = [s + suffix for s in old_names]
+
+    for i, old in enumerate(old_names):
+        new = n_names[i]
+        tmp = new + '_tmp_data'
+
+        # Create new
+        if old != new:
+            pyspedas.tcopy(old, new)
+
+        # Perform subtract_average or just copy the values
+        if sub_avg:
+            subtract_average(new, new_names=tmp)
+        else:
+            pyspedas.tcopy(new, tmp)
+
+        # Find spikes
+        tmps = tmp + '-s'
+        tsmooth(tmp, new_names=tmps, width=nsmooth)
+        ds = pytplot.data_quants[tmps].values  # smoothed out values
+        d0 = pytplot.data_quants[tmp].values  # original values
+        d = pytplot.data_quants[new]  # final values
+
+        dim = d.shape
+        for j in range(dim[1]):
+            # print("j = ", j)
+            for i in range(dim[0]):
+                # compare smoothed out values to original values
+                diff = abs((d0[i, j] - ds[i, j]) / ds[i, j])
+                if diff > thresh:
+                    d[i, j] = np.NaN  # for spikes, set to NaN
+
+        pytplot.data_quants[new] = d
+
+        # remove temp data
+        del pytplot.data_quants[tmp]
+        del pytplot.data_quants[tmps]
+
+        print('clean_spikes was applied to: ' + new)
---
+++ pyspedas/examples/basic/ex_spikes.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Example of clean_spikes.
+
+This module demonstrates how to use the function clean_spikes.
+
+"""
+import random
+import pytplot
+import pyspedas
+from pyspedas.analysis.clean_spikes import clean_spikes
+
+
+def ex_spikes():
+    """Load GMAG data and average over 5 min intervals."""
+    # Delete any existing pytplot variables
+    pytplot.del_data()
+
+    # Define a time rage as a list
+    trange = ['2007-03-23', '2007-03-23']
+
+    # Download gmag files and load data into pytplot variables
+    sites = ['ccnv']
+    var = 'thg_mag_ccnv'
+    pyspedas.themis.gmag(sites=sites, trange=trange, varnames=[var])
+    pytplot.tplot_options('title', 'GMAG data, thg_mag_ccnv 2007-03-23')
+
+    # Add spikes
+    data = pytplot.data_quants[var].values
+    dlen = len(data)
+    for i in range(1, 16):
+        s = (1 if random.random() < 0.5 else -1)
+        p1 = int(i*dlen/16)
+        data[p1, 0] = s * i * 40000
+        data[p1+2000, 1] = s * i * 30000
+        data[p1+4000, 2] = s * i * 20000
+
+    pytplot.data_quants[var].values = data
+
+    # Clean spikes
+    clean_spikes(var)
+
+    pytplot.tplot(pytplot.tplot_names())
+
+    # Return 1 as indication that the example finished without problems.
+    return 1
+
+
+# Run the example code
+# ex_spikes()
