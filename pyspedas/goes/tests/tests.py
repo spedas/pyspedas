6278a8bfb4698309ca1e67343972253331bb2dce
eric <egrimes@igpp.ucla.edu>
2020-04-04 22:43:16 -0700

    adding test for 1min data

--- pyspedas/goes/tests/tests.py
+++ pyspedas/goes/tests/tests.py
@@ -6,6 +6,12 @@ from pyspedas.utilities.data_exists import data_exists
 import pyspedas
 
 class LoadTestCases(unittest.TestCase):
+    def test_load_1min_mag_data(self):
+        mag_vars = pyspedas.goes.fgm(datatype='1min')
+        self.assertTrue(data_exists('BX_1'))
+        self.assertTrue(data_exists('BY_1'))
+        self.assertTrue(data_exists('BZ_1'))
+
     def test_load_mag_data(self):
         mag_vars = pyspedas.goes.fgm(datatype='512ms')
         self.assertTrue(data_exists('BX_1'))
