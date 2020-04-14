b5ff8beca10937c8a17002c05ffa4e6fb8f259cb
eric <egrimes@igpp.ucla.edu>
2020-04-14 14:02:34 -0700

    removing reference to preferences file

--- pyspedas/version.py
+++ pyspedas/version.py
@@ -4,20 +4,14 @@ File:
     version.py
 
 Description:
-    Pyspedas version number.
+    pySPEDAS version number.
 
 Returns:
     The version number for the current installation.
-    Also prints the location of the preferences file.
 """
 
 
 def version():
-    import os.path
-    dir_path = os.path.abspath(os.path.dirname(__file__))
-    fname = os.path.join(dir_path, 'spd_prefs_txt.py')
-    print("Preferences file: " + fname)
-
     import pkg_resources
     ver = pkg_resources.get_distribution("pyspedas").version
     print("pyspedas version: " + ver)
