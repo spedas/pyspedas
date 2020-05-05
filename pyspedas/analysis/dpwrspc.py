80bca33e59d2d8292ef9484b8f97a702e34bd166
nickssl <nikos@berkeley.edu>
2020-05-05 16:37:52 -0700

    Revert changes.

--- pyspedas/analysis/dpwrspc.py
+++ pyspedas/analysis/dpwrspc.py
@@ -1,74 +1,19 @@
-"""
-Compute power spectra for data.
 
-Notes
------
-Similar to dpwrspc.pro in IDL SPEDAS.
-
-"""
 import numpy as np
 
+def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3, nohanning=False, noline=False, notperhz=False, notmvariance=False, tm_sensitivity=None):
 
-def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3,
-            nohanning=False, noline=False, notperhz=False, notmvariance=False,
-            tm_sensitivity=None):
-    """
-    Compute power spectra.
-
-    Parameters
-    ----------
-    time: list of float
-        Time array.
-    quantity: list of float
-        Data array.
-    nboxpoints: int, optional
-        The number of points to use for the hanning window.
-        The default is 256.
-    nshiftpoints: int, optional
-        The number of points to shift for each spectrum.
-        The default is 128.
-    binsize: int, optional
-        Size for binning of the data along the frequency domain.
-        The default is 3.
-    nohanning: bool, optional
-        If True, no hanning window is applied to the input.
-        The default is False.
-    noline: bool, optional
-        If True, no straight line is subtracted.
-        The default is False.
-    notperhz: bool, optional
-        If True, the output units are the square of the input units.
-        The default is False.
-    notmvariance: bool, optional
-        If True, replace output spectrum for any windows that have variable
-        cadence with NaNs.
-        The default is False.
-    tm_sensitivity: float, optional
-        If noTmVariance is set, this number controls how much of a dt anomaly
-        is accepted.
-        The default is None.
-
-    Returns
-    -------
-    tdps: array of float
-        The time array for the dynamic power spectrum, the center time of the
-        interval used for the spectrum.
-    fdps: array of float
-        The frequency array (units =1/time units).
-    dps: array of float
-        The power spectrum, (units of quantity)^2/frequency_units.
-
-    """
     if nohanning is False:
         window = np.hanning(nboxpoints)
 
     # remove NaNs from the data
-    where_finite = np.where(np.isnan(quantity) is False)
+    where_finite = np.where(np.isnan(quantity)==False)
 
     quantity2process = quantity[where_finite[0]]
     times2process = time[where_finite[0]]
     nboxpnts = nboxpoints
     nshiftpnts = nshiftpoints
+
 
     totalpoints = len(times2process)
     nspectra = int((totalpoints-nboxpnts/2.)/nshiftpnts)
@@ -114,6 +59,7 @@ def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3,
                 line = poly1d_fn(t)
                 x = x - line
 
+
             if nohanning is False:
                 x = x*window
 
@@ -127,10 +73,9 @@ def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3,
 
             n_tm = len(t)
 
-            # time variance can break power spectrum,
-            # this keyword skips over those gaps
+            # time variance can break power spectrum, this keyword skips over those gaps
             if notmvariance and n_tm > 1:
-                if tm_sensitivity is not None:
+                if tm_sensitivity != None:
                     tmsn = tm_sensitivity
                 else:
                     tmsn = 100.0
@@ -146,8 +91,7 @@ def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3,
 
                     continue
 
-            # following Numerical recipes in Fortran, p. 421, sort of...
-            # (actually following the IDL implementation)
+            # following Numerical recipes in Fortran, p. 421, sort of... (actually following the IDL implementation)
             k = np.array(range(int(bign/2)+1))
             tres = np.median(t[1:len(t)] - t[0:len(t)-1])
             fk = k/(bign*tres)
@@ -177,9 +121,10 @@ def dpwrspc(time, quantity, nboxpoints=256, nshiftpoints=128, binsize=3,
                 power = power+pwr[iarray*binsize+i+1]
 
             if notperhz is False:
-                power = power/dfreq
+                power=power/dfreq
 
             dps[nthspectrum, :] = power
             fdps[nthspectrum, :] = freqcenter
 
     return (tdps, fdps, dps)
+
--- pyspedas/analysis/tdpwrspc.py
+++ pyspedas/analysis/tdpwrspc.py
@@ -1,58 +1,8 @@
-"""
-Compute power spectra for a tplot variable.
 
-Calls dpwrspc for the actual computation.
-
-Notes
------
-Similar to tdpwrspc.pro in IDL SPEDAS.
-
-"""
-from pyspedas.analysis.dpwrspc import dpwrspc
+from .dpwrspc import dpwrspc
 from pytplot import get_data, store_data, options, split_vec
 
-
-def tdpwrspc(varname, newname=None, nboxpoints=256, nshiftpoints=128,
-             binsize=3, nohanning=False, noline=False, notperhz=False,
-             notmvariance=False):
-    """
-    Compute power spectra for a tplot variable.
-
-    Parameters
-    ----------
-    varname: str
-        Name of pytplot variable.
-    newname: str, optional
-        Name of new pytplot variable to save data to.
-    nboxpoints: int, optional
-        The number of points to use for the hanning window.
-        The default is 256.
-    nshiftpoints: int, optional
-        The number of points to shift for each spectrum.
-        The default is 128.
-    binsize: int, optional
-        Size for binning of the data along the frequency domain.
-        The default is 3.
-    nohanning: bool, optional
-        If True, no hanning window is applied to the input.
-        The default is False.
-    noline: bool, optional
-        If True, no straight line is subtracted.
-        The default is False.
-    notperhz: bool, optional
-        If True, the output units are the square of the input units.
-        The default is False.
-    notmvariance: bool, optional
-        If True, replace output spectrum for any windows that have variable.
-        cadence with NaNs.
-        The default is False.
-
-    Returns
-    -------
-    str
-        Name of new pytplot variable.
-
-    """
+def tdpwrspc(varname, newname=None, nboxpoints=256, nshiftpoints=128, binsize=3, nohanning=False, noline=False, notperhz=False, notmvariance=False):
     if newname is None:
         newname = varname + '_dpwrspc'
 
@@ -63,23 +13,16 @@ def tdpwrspc(varname, newname=None, nboxpoints=256, nshiftpoints=128,
             split_vars = split_vec(varname)
             out_vars = []
             for var in split_vars:
-                out_vars.append(tdpwrspc(var, newname=var + '_dpwrspc',
-                                         nboxpoints=nboxpoints,
-                                         nshiftpoints=nshiftpoints))
+                out_vars.append(tdpwrspc(var, newname=var + '_dpwrspc', nboxpoints=nboxpoints, nshiftpoints=nshiftpoints))
             return out_vars
         else:
-            pwrspc = dpwrspc(data_tuple[0], data_tuple[1],
-                             nboxpoints=nboxpoints, nshiftpoints=nshiftpoints,
-                             binsize=binsize, nohanning=nohanning,
-                             noline=noline, notperhz=notperhz,
-                             notmvariance=notmvariance)
+            pwrspc = dpwrspc(data_tuple[0], data_tuple[1], nboxpoints=nboxpoints, nshiftpoints=nshiftpoints, binsize=binsize, nohanning=nohanning, noline=noline, notperhz=notperhz, notmvariance=notmvariance)
 
-            if pwrspc is not None:
+            if pwrspc != None:
                 store_data(newname, data={'x': pwrspc[0], 'y': pwrspc[2], 'v': pwrspc[1]})
                 options(newname, 'spec', True)
                 options(newname, 'ylog', True)
                 options(newname, 'zlog', True)
                 options(newname, 'Colormap', 'jet')
-                # options(newname, 'yrange', [0.01, 16])
-
+               # options(newname, 'yrange', [0.01, 16])
         return newname
