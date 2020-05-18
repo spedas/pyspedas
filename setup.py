8afad0c29d65d1a0925c12bb9df2079bcbe1619a
nickssl <nikos@berkeley.edu>
2020-05-17 17:01:50 -0700

    Version 0.9.30

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='0.9.29',
+    version='0.9.30',
     description='Python Space Physics Environment Data Analysis\
         Software (SPEDAS)',
     long_description=open('README.md').read(),
@@ -26,7 +26,7 @@ setup(
     project_urls={'Information': 'http://spedas.org/wiki/',
                   },
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['requests', 'pytplot>=1.6.4', 'cdflib>=0.3.18',
+    install_requires=['requests', 'pytplot>=1.6.8', 'cdflib>=0.3.18',
                       'cdasws>=1.7.17', 'netCDF4', 'pywavelets'],
     python_requires='>=3.5',
     include_package_data=True,
