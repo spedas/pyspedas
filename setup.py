83d964edb5053bebf6340e1cadb244a817a5805e
supervised <egrimes@igpp.ucla.edu>
2020-10-06 21:04:41 -0700

    version bump for pypi
    to get MMS ASPOC changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='1.0.11',
+    version='1.0.12',
     description='Python Space Physics Environment Data Analysis\
         Software (SPEDAS)',
     long_description=open('README.md').read(),
