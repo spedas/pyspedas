4efd821dade9854d265557653253840dbb39489b
nickssl <nikos@berkeley.edu>
2020-10-22 21:59:42 -0700

    Simplified test, so that it runs faster.
    Specified varnames so that pytplot loads only specific data. This should considerably decrease the time this test needs to run.
    Also, uncommented the gmom test.

--- pyspedas/themis/tests/tests.py
+++ pyspedas/themis/tests/tests.py
@@ -26,103 +26,71 @@ class GmagTestCases(unittest.TestCase):
 
 class LoadTestCases(unittest.TestCase):
     def test_load_state_data(self):
-        data = pyspedas.themis.state()
+        pyspedas.themis.state(varnames=['thc_pos'])
         self.assertTrue(data_exists('thc_pos'))
-        self.assertTrue(data_exists('thc_vel'))
-        self.assertTrue(data_exists('thc_pos_gsm'))
-        self.assertTrue(data_exists('thc_vel_gse'))
 
     def test_load_gmag_data(self):
-        data = pyspedas.themis.gmag()
+        data = pyspedas.themis.gmag(varnames=['thg_mag_amer'])
+        print(data)
         self.assertTrue(data_exists('thg_mag_amer'))
-        self.assertTrue(data_exists('thg_mag_kil'))
-        self.assertTrue(data_exists('thg_mag_eagl'))
-        self.assertTrue(data_exists('thg_mag_leth'))
-        self.assertTrue(data_exists('thg_mag_inuv'))
-        self.assertTrue(data_exists('thg_mag_wrth'))
 
     def test_load_sst_data(self):
-        data = pyspedas.themis.sst()
+        pyspedas.themis.sst(varnames=['thc_psif_en_eflux'])
         self.assertTrue(data_exists('thc_psif_en_eflux'))
-        self.assertTrue(data_exists('thc_psif_density'))
-        self.assertTrue(data_exists('thc_psif_avgtemp'))
-        self.assertTrue(data_exists('thc_psif_vthermal'))
 
     def test_load_fgm_data(self):
-        data = pyspedas.themis.fgm()
+        pyspedas.themis.fgm(varnames=['thc_fgs_btotal'])
         self.assertTrue(data_exists('thc_fgs_btotal'))
-        self.assertTrue(data_exists('thc_fgs_gse'))
-        self.assertTrue(data_exists('thc_fgs_gsm'))
 
     def test_load_fit_data(self):
-        data = pyspedas.themis.fit()
+        pyspedas.themis.fit(varnames=['thc_fgs_gse'])
         self.assertTrue(data_exists('thc_fgs_gse'))
-        self.assertTrue(data_exists('thc_fgs_gsm'))
 
     def test_load_esa_data(self):
-        data = pyspedas.themis.esa()
+        pyspedas.themis.esa(varnames=['thc_peif_density'])
         self.assertTrue(data_exists('thc_peif_density'))
-        self.assertTrue(data_exists('thc_peif_avgtemp'))
-        self.assertTrue(data_exists('thc_peif_en_eflux'))
-        self.assertTrue(data_exists('thc_peif_flux'))
 
     def test_load_fft_data(self):
-        data = pyspedas.themis.fft()
+        pyspedas.themis.fft(varnames=['thc_ffp_16_edc34'])
         self.assertTrue(data_exists('thc_ffp_16_edc34'))
-        self.assertTrue(data_exists('thc_ffp_16_edc56'))
-        self.assertTrue(data_exists('thc_ffp_16_scm2'))
 
     def test_load_fft_l1_data(self):
-        data = pyspedas.themis.fft(level='l1')
+        pyspedas.themis.fft(level='l1', varnames=['thc_ffp_16'])
         self.assertTrue(data_exists('thc_ffp_16'))
-        self.assertTrue(data_exists('thc_ffw_16'))
 
     def test_load_fbk_data(self):
-        data = pyspedas.themis.fbk()
+        pyspedas.themis.fbk(varnames=['thc_fb_hff'])
         self.assertTrue(data_exists('thc_fb_hff'))
-        self.assertTrue(data_exists('thc_fb_edc12'))
-        self.assertTrue(data_exists('thc_fb_scm1'))
 
     def test_load_mom_data(self):
-        data = pyspedas.themis.mom()
+        pyspedas.themis.mom(varnames=['thc_peim_density'])
         self.assertTrue(data_exists('thc_peim_density'))
-        self.assertTrue(data_exists('thc_peim_ptot'))
-        self.assertTrue(data_exists('thc_peim_flux'))
-        self.assertTrue(data_exists('thc_peim_eflux'))
-        self.assertTrue(data_exists('thc_peim_velocity_gse'))
 
-    # the following isn't working for some reason
-    # def test_load_gmom_data(self):
-    #     data = pyspedas.themis.gmom()
-    #     self.assertTrue(data_exists(''))
+    def test_load_gmom_data(self):
+        pyspedas.themis.gmom(trange=['2020-01-01', '2020-01-01'],
+                                    varnames=['thc_ptiff_density'])
+        self.assertTrue(data_exists('thc_ptiff_density'))
 
     def test_load_scm_data(self):
-        data = pyspedas.themis.scm()
+        pyspedas.themis.scm(varnames=['thc_scf_btotal'])
         self.assertTrue(data_exists('thc_scf_btotal'))
-        self.assertTrue(data_exists('thc_scp_gsm'))
-        self.assertTrue(data_exists('thc_scf_gsm'))
 
     def test_load_scm_l1_data(self):
-        data = pyspedas.themis.scm(level='l1')
+        pyspedas.themis.scm(level='l1', varnames=['thc_scf'])
         self.assertTrue(data_exists('thc_scf'))
-        self.assertTrue(data_exists('thc_scp'))
-        self.assertTrue(data_exists('thc_scw'))
 
     def test_load_efi_l1_data(self):
-        data = pyspedas.themis.efi(level='l1')
+        pyspedas.themis.efi(level='l1', varnames=['thc_eff'])
         self.assertTrue(data_exists('thc_eff'))
-        self.assertTrue(data_exists('thc_efp'))
-        self.assertTrue(data_exists('thc_efw'))
-        self.assertTrue(data_exists('thc_vaf'))
 
     def test_load_efi_data(self):
-        data = pyspedas.themis.efi(time_clip=True)
+        pyspedas.themis.efi(time_clip=True, varnames=['thc_eff_e12_efs'])
         self.assertTrue(data_exists('thc_eff_e12_efs'))
-        self.assertTrue(data_exists('thc_eff_e34_efs'))
 
     def test_downloadonly(self):
         files = pyspedas.themis.efi(downloadonly=True,
-                                    trange=['2014-2-15', '2014-2-16'])
+                                    trange=['2014-2-15', '2014-2-16'],
+                                    varnames=['thc_eff_e12_efs'])
         self.assertTrue(os.path.exists(files[0]))
 
 
