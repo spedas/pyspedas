d0eb384de1d93092c0c216dbceeda3510e26d451
supervised <egrimes@igpp.ucla.edu>
2020-10-27 11:56:51 -0700

    bumping the version to get the latest tnames changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='1.0.13',
+    version='1.0.14',
     description='Python Space Physics Environment Data Analysis\
         Software (SPEDAS)',
     long_description=open('README.md').read(),
@@ -26,7 +26,7 @@ setup(
     project_urls={'Information': 'http://spedas.org/wiki/',
                   },
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['requests', 'pytplot>=1.7.7', 'cdflib>=0.3.18',
+    install_requires=['requests', 'pytplot>=1.7.10', 'cdflib>=0.3.18',
                       'cdasws>=1.7.21', 'netCDF4', 'pywavelets', 'pyqtgraph',
                       'astropy'],
     python_requires='>=3.5',
