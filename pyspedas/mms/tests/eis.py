a31e4975cbbc9541ec12fc957800a7d33b7d5cf0
eric <egrimes@igpp.ucla.edu>
2021-01-19 15:37:04 -0800

    Updating EIS test suite for recent changes to the PAD routines

--- pyspedas/mms/tests/eis.py
+++ pyspedas/mms/tests/eis.py
@@ -7,14 +7,21 @@ class EISTestCases(unittest.TestCase):
     def test_pad_extof_srvy(self):
         mms_load_eis(datatype='extof')
         mms_eis_pad(datatype='extof')
-        self.assertTrue(data_exists('mms1_epd_eis_extof_56-535keV_proton_flux_omni_pad_spin'))
-        self.assertTrue(data_exists('mms1_epd_eis_extof_56-535keV_proton_flux_omni_pad'))
+        self.assertTrue(data_exists('mms1_epd_eis_extof_46-10489keV_proton_flux_omni_pad_spin'))
+        self.assertTrue(data_exists('mms1_epd_eis_extof_46-10489keV_proton_flux_omni_pad'))
 
     def test_pad_extof_srvy_probe(self):
         mms_load_eis(probe=4)
         mms_eis_pad(probe=4)
-        self.assertTrue(data_exists('mms4_epd_eis_extof_80-524keV_proton_flux_omni_pad_spin'))
-        self.assertTrue(data_exists('mms4_epd_eis_extof_80-524keV_proton_flux_omni_pad'))
+        self.assertTrue(data_exists('mms4_epd_eis_extof_44-1315keV_proton_flux_omni_pad_spin'))
+        self.assertTrue(data_exists('mms4_epd_eis_extof_44-1315keV_proton_flux_omni_pad'))
+
+    def test_pad_extof_brst(self):
+        mms_load_eis(probe=4, datatype='extof', data_rate='brst', trange=['2015-10-16/13:06', '2015-10-16/13:07'])
+        mms_eis_pad(probe=4, datatype='extof', data_rate='brst')
+        self.assertTrue(data_exists('mms4_epd_eis_brst_extof_52-878keV_proton_flux_omni_pad_spin'))
+        self.assertTrue(data_exists('mms4_epd_eis_brst_extof_52-878keV_proton_flux_omni_pad_'))
+
 
 if __name__ == '__main__':
     unittest.main()
