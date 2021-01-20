91373ded372956064356066612006067b748d9b6
eric <egrimes@igpp.ucla.edu>
2021-01-19 17:05:11 -0800

    fixing typo in test

--- pyspedas/mms/tests/eis.py
+++ pyspedas/mms/tests/eis.py
@@ -20,7 +20,7 @@ class EISTestCases(unittest.TestCase):
         mms_load_eis(probe=4, datatype='extof', data_rate='brst', trange=['2015-10-16/13:06', '2015-10-16/13:07'])
         mms_eis_pad(probe=4, datatype='extof', data_rate='brst')
         self.assertTrue(data_exists('mms4_epd_eis_brst_extof_52-878keV_proton_flux_omni_pad_spin'))
-        self.assertTrue(data_exists('mms4_epd_eis_brst_extof_52-878keV_proton_flux_omni_pad_'))
+        self.assertTrue(data_exists('mms4_epd_eis_brst_extof_52-878keV_proton_flux_omni_pad'))
 
 
 if __name__ == '__main__':
