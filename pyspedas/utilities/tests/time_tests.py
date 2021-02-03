92aa4977aff6a4921bf01b49bd916431144e63d3
eric <egrimes@igpp.ucla.edu>
2021-02-03 14:12:54 -0800

    Adding more date/time tests

--- pyspedas/utilities/tests/time_tests.py
+++ pyspedas/utilities/tests/time_tests.py
@@ -9,14 +9,20 @@ class TimeTestCases(unittest.TestCase):
     def test_time_datetime(self):
         """Test time_datetime function."""
         self.assertTrue(time_datetime(1450137600.0000000) == datetime(2015, 12, 15, 0, 0, tzinfo=timezone.utc))
+        self.assertTrue([time_datetime(1450137600.0000000), time_datetime(1444953600.0000000)] 
+            == [datetime(2015, 12, 15, 0, 0, tzinfo=timezone.utc), datetime(2015, 10, 16, 0, 0, tzinfo=timezone.utc)])
 
     def test_time_string(self):
         """Test time_string function."""
+        self.assertTrue(time_string(fmt='%Y-%m-%d') == datetime.now().strftime('%Y-%m-%d'))
+        self.assertTrue(time_string(1450181243.767, fmt='%Y-%m-%d') == '2015-12-15')
+        self.assertTrue(time_string(1450181243.767, fmt='%Y-%m-%d/%H:%M:%S') == '2015-12-15/12:07:23')
         self.assertTrue(time_string(1450181243.767) == '2015-12-15 12:07:23.767000')
         self.assertTrue(time_string([1450181243.767, 1450181263.767]) == ['2015-12-15 12:07:23.767000', '2015-12-15 12:07:43.767000'])
 
     def test_time_double(self):
         """Test time_double function."""
+        self.assertTrue(time_string(time_double(), fmt='%Y-%m-%d') == datetime.now().strftime('%Y-%m-%d'))
         self.assertTrue(time_double('2015-12-15/12:00') == 1450180800.0000000)
         self.assertTrue(time_double('2015-12-15/12') == 1450180800.0000000)
         self.assertTrue(time_double('2015-12-15') == 1450137600.0000000)
