78f8e9b031839318ca3adff20d88a10d24dae003
eric <egrimes@igpp.ucla.edu>
2021-02-23 11:20:46 -0800

    Updated to work with NRV variables

--- pyspedas/utilities/data_exists.py
+++ pyspedas/utilities/data_exists.py
@@ -1,9 +1,10 @@
 
+import numpy as np
 from pytplot import get_data
-
 
 def data_exists(tvar):
     data = get_data(tvar)
-    if data:
+    # multi-dimensional data returns a tuple, NRV variables return an ndarray
+    if isinstance(data, tuple) or isinstance(data, np.ndarray):
         return True
     return False
