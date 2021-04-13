e6f7de7ec8cd353263de9f7f828730096731cf30
eric <egrimes@igpp.ucla.edu>
2021-04-13 10:30:47 -0700

    Adding routines for getting/setting coordinate systems

---
+++ pyspedas/cotrans/cotrans_get_coord.py
@@ -0,0 +1,33 @@
+
+from pytplot import get_data
+
+def cotrans_get_coord(name):
+    '''
+    This function returns the coordinate system of a tplot variable
+
+    Parameters:
+        name: str
+            name of the tplot variable
+
+    Notes:
+        The coordinate system is stored in the variable's metadata at:
+            metadata['data_att']['coord_sys']
+
+        See cotrans_set_coord to update the coordinate system
+
+    Returns:
+        Coordinate system of the tplot variable 
+        or 
+        None if the coordinate system isn't set
+    '''
+
+    metadata = get_data(name, metadata=True)
+    if metadata == None:
+        return None
+
+    if metadata.get('data_att'):
+        if metadata['data_att'].get('coord_sys'):
+            return metadata['data_att']['coord_sys']
+
+    print('Coordinate system not found: ' + name)
+    return None
---
+++ pyspedas/cotrans/cotrans_set_coord.py
@@ -0,0 +1,36 @@
+
+from pytplot import get_data, store_data
+
+def cotrans_set_coord(name, coord):
+    '''
+    This function sets the coordinate system of a tplot variable
+
+    Parameters:
+        name: str
+            name of the tplot variable
+
+    Notes:
+        The coordinate system is stored in the variable's metadata at:
+            metadata['data_att']['coord_sys']
+
+        See cotrans_get_coord to return the coordinate system
+
+    Returns:
+        bool: True/False depending on if the operation was successful
+    '''
+
+    # check that the variable exists
+    data = get_data(name)
+    if data == None:
+        return False
+
+    metadata = get_data(name, metadata=True)
+
+    if metadata.get('data_att') == None:
+        metadata['data_att'] = {}
+
+    # note: updating the metadata dict directly updates
+    # the variable's metadata in memory, so there's 
+    # no need to update the variable with store_data
+    metadata['data_att'] = {'coord_sys': coord}
+    return True
