db72fad618aca78a4991675948ca696796611a77
eric <egrimes@igpp.ucla.edu>
2021-06-01 16:11:59 -0700

    several updates

--- pyspedas/mms/particles/mms_part_getspec.py
+++ pyspedas/mms/particles/mms_part_getspec.py
@@ -1,12 +1,26 @@
 
+import logging
+from time import time
+
 import pyspedas
 from pyspedas.mms.particles.mms_part_products import mms_part_products
 
+logging.captureWarnings(True)
+logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 def mms_part_getspec(instrument='fpi', probe='1', species='e', data_rate='fast', 
-    trange=None, output=['energy', 'theta', 'phi'], units='eflux'):
+    trange=None, output=['energy', 'theta', 'phi'], units='eflux', energy=None,
+    phi=None, theta=None, pitch=None, gyro=None):
     """
 
     """
+
+    start_time = time()
+
+    if trange == None:
+        # test data for development
+        trange = ['2015-10-16/13:06', '2015-10-16/13:07']
+        data_rate = 'brst'
     
     fpi_vars = pyspedas.mms.fpi(datatype='d'+species+'s-dist', probe=probe, data_rate=data_rate, trange=trange, time_clip=True)
 
@@ -21,7 +35,9 @@ def mms_part_getspec(instrument='fpi', probe='1', species='e', data_rate='fast',
             tname = 'mms'+str(prb)+'_d'+species+'s_dist_'+data_rate
 
         new_vars = mms_part_products(tname, species=species, instrument=instrument, probe=probe, data_rate=data_rate,
-                          units=units)
+                          units=units, energy=energy, phi=phi, theta=theta, pitch=pitch, gyro=gyro)
         out_vars = out_vars + new_vars
 
+    logging.info('Finished; time to run: ' + str(round(time()-start_time, 1)) + ' seconds.')
+    
     return out_vars
--- pyspedas/mms/particles/mms_part_products.py
+++ pyspedas/mms/particles/mms_part_products.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+from time import time
 
 from pytplot import get_data, store_data, options
 
@@ -7,6 +8,8 @@ from pyspedas.particles.spd_part_products.spd_pgs_make_theta_spec import spd_pgs
 from pyspedas.particles.spd_part_products.spd_pgs_make_phi_spec import spd_pgs_make_phi_spec
 from pyspedas.particles.spd_part_products.spd_pgs_make_e_spec import spd_pgs_make_e_spec
 from pyspedas.particles.spd_part_products.spd_pgs_make_tplot import spd_pgs_make_tplot
+from pyspedas.particles.spd_part_products.spd_pgs_limit_range import spd_pgs_limit_range
+from pyspedas.particles.spd_part_products.spd_pgs_progress_update import spd_pgs_progress_update
 
 from pyspedas.mms.fpi.mms_get_fpi_dist import mms_get_fpi_dist
 from pyspedas.mms.particles.mms_convert_flux_units import mms_convert_flux_units
@@ -16,7 +19,7 @@ logging.captureWarnings(True)
 logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 def mms_part_products(in_tvarname, units='eflux', species='e', data_rate='fast', instrument='fpi', probe='1',
-    output=['energy', 'theta', 'phi']):
+    output=['energy', 'theta', 'phi'], energy=None, phi=None, theta=None, pitch=None, gyro=None):
     """
 
     """
@@ -27,7 +30,7 @@ def mms_part_products(in_tvarname, units='eflux', species='e', data_rate='fast',
 
     data_in = get_data(in_tvarname)
 
-    if data_in == None:
+    if data_in is None:
         logging.error('Error, could not find the variable: ' + in_tvarname)
         return None
 
@@ -43,20 +46,31 @@ def mms_part_products(in_tvarname, units='eflux', species='e', data_rate='fast',
     out_phi_y = np.zeros((dist_in['n_times'], dist_in['n_phi']))
 
     out_vars = []
+    last_update_time = None
+    ntimes = len(data_in.times)
 
-    for i in range(0, len(data_in.times)):
+    for i in range(0, ntimes):
+        last_update_time = spd_pgs_progress_update(last_update_time=last_update_time, current_sample=i, total_samples=ntimes, type_string=in_tvarname)
+
         dist_in = mms_get_fpi_dist(in_tvarname, index=i, species=species, probe=probe, data_rate=data_rate)
 
         data = mms_convert_flux_units(dist_in, units=units)
 
         clean_data = mms_pgs_clean_data(data)
 
+        # Apply phi, theta, & energy limits
+        if energy is not None or theta is not None or phi is not None:
+            clean_data = spd_pgs_limit_range(clean_data, energy=energy, theta=theta, phi=phi)
+
+        # Build energy spectrogram
         if 'energy' in output:
             out_energy_y[i, :], out_energy[i, :] = spd_pgs_make_e_spec(clean_data)
 
+        # Build theta spectrogram
         if 'theta' in output:
             out_theta_y[i, :], out_theta[i, :] = spd_pgs_make_theta_spec(clean_data)
 
+        # Build phi spectrogram
         if 'phi' in output:
             out_phi_y[i, :], out_phi[i, :] = spd_pgs_make_phi_spec(clean_data)
 
