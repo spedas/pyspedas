668370c910a96e52f2fdfe648fa4a7f36fc47b1d
supervised <egrimes@igpp.ucla.edu>
2021-07-21 13:47:48 -0700

    Fixing test regression

--- pyspedas/mms/tests/curlometer.py
+++ pyspedas/mms/tests/curlometer.py
@@ -5,7 +5,7 @@ from pyspedas.utilities.data_exists import data_exists
 
 class CurlTestCases(unittest.TestCase):
     def test_default(self):
-        data = pyspedas.mms.fgm(probe=[1, 2, 3, 4], trange=['2015-10-30/05:15:45', '2015-10-30/05:15:48'], data_rate='brst', time_clip=True, varformat='*_gse_*')
+        data = pyspedas.mms.fgm(probe=[1, 2, 3, 4], trange=['2015-10-30/05:15:45', '2015-10-30/05:15:48'], data_rate='brst', time_clip=True, varformat='*_gse_*', get_fgm_ephemeris=True)
         curl = pyspedas.mms.curlometer(positions=['mms1_fgm_r_gse_brst_l2', 'mms2_fgm_r_gse_brst_l2', 'mms3_fgm_r_gse_brst_l2', 'mms4_fgm_r_gse_brst_l2'], fields=['mms1_fgm_b_gse_brst_l2', 'mms2_fgm_b_gse_brst_l2', 'mms3_fgm_b_gse_brst_l2', 'mms4_fgm_b_gse_brst_l2'])
         self.assertTrue(data_exists('jtotal'))
         self.assertTrue(data_exists('curlB'))
