01269c51b4481992774c447d2d17da2e973c389d
supervised <egrimes@igpp.ucla.edu>
2021-09-01 16:49:37 -0700

    Adding datatypes for Solar Orbiter RPW data

--- pyspedas/solo/__init__.py
+++ pyspedas/solo/__init__.py
@@ -111,6 +111,42 @@ def rpw(trange=['2020-06-15', '2020-06-16'],
 
         datatype: str
             Data type; Valid options:
+            Level 1:
+                'hfr-surv', 'lfr-surv-asm', 'lfr-surv-bp1', 'lfr-surv-bp2',
+                'lfr-surv-cwf', 'lfr-surv-swf', 'tds-surv-hist1d', 'tds-surv-hist2d',
+                'tds-surv-mamp', 'tds-surv-rswf', 'tds-surv-stat', 'tds-surv-tswf',
+                'tnr-surv'
+                (see below for definitions)
+
+            Level 2:
+              High Frequency Receiver (HFR):
+                'hfr-surv': High Frequency Receiver (HFR) Data in Survey Mode 
+
+              Low Frequency Receiver (LFR):
+                'lfr-surv-asm': Averaged Spectral Matrix (ASM) Data in Survey Mode
+                'lfr-surv-bp1': Basic Parameters Set 1 (BP1) Data in Survey Mode
+                'lfr-surv-bp2': Basic Parameter Set 2 (BP2) Data in Survey Mode 
+                'lfr-surv-cwf-b': Continuous Magnetic Waveform (CWF-B) in Survey Mode
+                'lfr-surv-cwf-e': Continuous Electric Waveform (CWF-E) in Survey Mode
+                'lfr-surv-swf-b': Snapshot Magnetic Waveform (SWF-B) in Survey Mode
+                'lfr-surv-swf-e': Snapshot Electric Waveform (SWF-E) in Survey Mode
+
+              Time Domain Sampler (TDS):
+                'tds-surv-hist1d': Histogram Set 1 (HIST1D) Data in Survey Mode
+                'tds-surv-hist2d': Histogram Set 2 (HIST2D) Data in Survey Mode
+                'tds-surv-mamp': Maximum Amplitude (MAMP) Data in Survey Mode
+                'tds-surv-rswf-b': Regular Snapshot Waveform (RSWF) Magnetic Field Data in Survey Mode
+                'tds-surv-rswf-e': Regular Snapshot Waveform (RSWF) Electric Field Data in Survey Mode
+                'tds-surv-stat': Statistical (STAT) Data in Survey Mode 
+                'tds-surv-tswf-b': Triggered Snapshot Magnetic Waveform (TSWF-B) in Survey Mode
+                'tds-surv-tswf-e': Triggered Snapshot Electric Waveform (TSWF-E) in Survey Mode
+
+            Level 3:                
+                'bia-density': Plasma density derived from probe-to-spacecraft potential and electron plasma frequency
+                'bia-density-10-seconds': same as above, but median value over 10 s interval
+                'bia-efield-10-seconds': Electric field vector in SRF. Median value over 10 s interval
+                'bia-scpot-10-seconds': Spacecraft potential with respect to plasma. Median value over 10 s interval
+                'tnr-fp': Plasma frequency value derived by the plasma peak tracking (Thermal Noise Receiver (TNR))
 
         level: str
             Data level (default: l2)
