bdb8437695e4dcbd0c1463ce4230f660f358342c
supervised <egrimes@igpp.ucla.edu>
2021-09-14 15:08:13 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,3 +69,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ['sip', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets']
