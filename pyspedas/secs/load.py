47310dcf5231ee25e558f94c01dc9d4e4f053380
Tiger <supercxman@gmail.com>
2021-10-15 20:15:27 -0600

    modified the load function for adding *.gz files.

--- pyspedas/secs/load.py
+++ pyspedas/secs/load.py
@@ -11,6 +11,8 @@ from .config import CONFIG
 import zipfile
 import pyspedas
 import logging
+import shutil
+import gzip
 
 def load(trange = None, resolution=10, dtype = 'EICS', no_download = False, downloadonly = False, out_type = 'np'):
     """
@@ -23,6 +25,7 @@ def load(trange = None, resolution=10, dtype = 'EICS', no_download = False, down
     if dtype == 'EICS' or dtype == 'SECS':
         pathformat_prefix = dtype + '/%Y/%m/'
         pathformat_zip = pathformat_prefix + dtype + '%Y%m%d.zip'
+        pathformat_gz = pathformat_prefix + dtype + '%Y%m%d.zip.gz'
         pathformat_unzipped = pathformat_prefix + '%d/' + dtype + '%Y%m%d_%H%M%S.dat'
 
     else:
@@ -30,20 +33,47 @@ def load(trange = None, resolution=10, dtype = 'EICS', no_download = False, down
 
     # find the full remote path names using the trange
     remote_names = dailynames(file_format=pathformat_zip, trange=trange)
+    remote_names_gz = dailynames(file_format=pathformat_gz, trange=trange)
+    #remote_names = remote_names + remote_names_gz
+    #print('remote_names: ', remote_names)
 
     out_files = []
 
     files_zip = download(remote_file=remote_names, remote_path=CONFIG['remote_data_dir'],
                      local_path=CONFIG['local_data_dir'], no_download=no_download)
+    files_gz = download(remote_file=remote_names_gz, remote_path=CONFIG['remote_data_dir'],
+                         local_path=CONFIG['local_data_dir'], no_download=no_download)
+    files_zip = files_zip + files_gz
 
-
+    print('files_zip: ', files_zip)
     if files_zip is not None:
-        for rf_zip in files_zip:
+        for rf_zip_zero in files_zip:
+            if rf_zip_zero.endswith('.gz'):
+                rf_zip = rf_zip_zero[0:-3]
+                # unzip .gz file to .zip file
+                with gzip.open(rf_zip_zero, 'rb') as f_in:
+                    with open(rf_zip, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+            elif rf_zip_zero.endswith('.zip'):
+                rf_zip = rf_zip_zero
+
             foldername_unzipped = rf_zip[0:-16] + rf_zip[-6:-4]
             if not os.path.isdir(foldername_unzipped):
                 logging.info('Start unzipping: '+ rf_zip + '  ------')
                 with zipfile.ZipFile(rf_zip, 'r') as zip_ref:
                     zip_ref.extractall(rf_zip[0:-16])
+                if not os.path.isdir(foldername_unzipped):
+                    # for the case of unzipping directly without the %d folder made.
+                    # make %d folder
+                    os.makedirs(foldername_unzipped)
+                    # move .dat files
+                    sourcepath = rf_zip[0:-16]
+                    sourcefiles = os.listdir(sourcepath)
+                    destinationpath = foldername_unzipped
+                    for file in sourcefiles:
+                        if file.endswith('.dat'):
+                            shutil.move(os.path.join(sourcepath, file), os.path.join(destinationpath, file))
+
             else:
                 logging.info('Unzipped folder: ' + foldername_unzipped + ' existed, skip unzipping  ------')
 
