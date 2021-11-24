ed3465c9147f09e9ec00f99ac2eee56b1c6607e9
supervised <egrimes@igpp.ucla.edu>
2021-11-24 11:46:50 -0800

    lowering the required numpy version to 1.19.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     project_urls={'Information': 'http://spedas.org/wiki/',
                   },
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['numpy>=1.20.0', 'requests', 'pytplot>=1.7.27',
+    install_requires=['numpy>=1.19.5', 'requests', 'pytplot>=1.7.27',
                       'cdflib>=0.3.20', 'cdasws>=1.7.24', 'netCDF4',
                       'pywavelets', 'pyqtgraph==0.12.2', 'astropy'],
     python_requires='>=3.7',
