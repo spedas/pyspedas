7e12eb1b3565f7749e713434485710495aff60b3
Eric Grimes <egrimes@igpp.ucla.edu>
2022-01-21 09:33:48 -0800

    Adding initial HAPI load routine, and some more GOES instruments

--- pyspedas/goes/__init__.py
+++ pyspedas/goes/__init__.py
@@ -44,3 +44,267 @@ def fgm(trange=['2013-11-5', '2013-11-6'],
 
     """
     return load(instrument='fgm', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def eps(trange=['2013-11-5', '2013-11-6'], 
+        probe='12',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES energetic particle sensor
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='eps', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def epead(trange=['2013-11-5', '2013-11-6'], 
+        probe='15',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES Electron, Proton, Alpha Detector
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='epead', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def maged(trange=['2013-11-5', '2013-11-6'], 
+        probe='15',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES Magnetospheric Electron Detector
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='maged', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def magpd(trange=['2013-11-5', '2013-11-6'], 
+        probe='15',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES Magnetospheric Proton Detector
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='magpd', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def hepad(trange=['2013-11-5', '2013-11-6'], 
+        probe='15',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES High energy Proton and Alpha Detector
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='hepad', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
+
+def xrs(trange=['2013-11-5', '2013-11-6'], 
+        probe='15',
+        datatype='1min', 
+        suffix='',  
+        downloadonly=False,
+        no_update=False,
+        time_clip=False):
+    """
+    This function loads data from the GOES X-ray Sensor
+    
+    Parameters
+    ----------
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        probe: str/int or list of strs/ints
+            GOES spacecraft #, e.g., probe=15
+
+        datatype: str
+            Data type; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns
+    ----------
+        List of tplot variables created.
+
+    """
+    return load(instrument='xrs', trange=trange, probe=probe, datatype=datatype, suffix=suffix, downloadonly=downloadonly, time_clip=time_clip, no_update=no_update)
--- pyspedas/goes/load.py
+++ pyspedas/goes/load.py
@@ -17,6 +17,12 @@ def load(trange=['2013-11-5', '2013-11-6'],
     This function loads data from the GOES mission; this function is not meant 
     to be called directly; instead, see the wrappers:
         pyspedas.goes.fgm
+        pyspedas.goes.eps
+        pyspedas.goes.epead
+        pyspedas.goes.maged
+        pyspedas.goes.magpd
+        pyspedas.goes.hepad
+        pyspedas.goes.xrs
 
     """
 
@@ -36,9 +42,82 @@ def load(trange=['2013-11-5', '2013-11-6'],
                 pathformat = remote_path + 'g' + str(prb) + '_magneto_1m_%Y%m01_%Y%m??.nc'
             elif datatype == '5min': # 5 min averages
                 pathformat = remote_path + 'g' + str(prb) + '_magneto_5m_%Y%m01_%Y%m??.nc'
+        elif instrument == 'eps':
+            # energetic particle sensor -- only valid for GOES-08 through GOES-12, only averaged data available
+            if datatype == '1min':
+                pathformat = remote_path + 'g' + str(prb) + '_eps_1m_%Y%m01_%Y%m??.nc'
+            else:
+                pathformat = remote_path + 'g' + str(prb) + '_eps_5m_%Y%m01_%Y%m??.nc'
+        elif instrument == 'epead':
+            # electron, proton, alpha detector -- only valid on GOES-13, 14, 15
+            if datatype == '1min':
+                pathformat = [remote_path + 'g' + str(prb) + '_epead_e13ew_1m_%Y%m01_%Y%m??.nc',
+                                                             '_epead_p17ew_1m_%Y%m01_%Y%m??.c',
+                                                             '_epead_a16ew_1m_%Y%m01_%Y%m??.nc']
+            elif datatype == '5min':
+                pathformat = [remote_path + 'g' + str(prb) + '_epead_e13ew_5m_%Y%m01_%Y%m??.nc',
+                                                             '_epead_p17ew_5m_%Y%m01_%Y%m??.c',
+                                                             '_epead_a16ew_5m_%Y%m01_%Y%m??.nc']
+            else:
+                pathformat = [remote_path + 'g' + str(prb) + '_epead_e1ew_4s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_e2ew_16s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_e3ew_16s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_p1ew_8s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_p27e_32s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_p27w_32s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_a16e_32s_%Y%m%d_%Y%m%d.nc',
+                                                             '_epead_a16w_32s_%Y%m%d_%Y%m%d.nc']
+        elif instrument == 'maged':
+            # magnetospheric electron detector -- only valid on GOES 13, 14, 15
+            if datatype == '1min':
+                pathformat = remote_path + 'g' + str(prb) + '_maged_19me15_1m_%Y%m01_%Y%m??.nc'
+            elif datatype == '5min':
+                pathformat = remote_path + 'g' + str(prb) + '_maged_19me15_5m_%Y%m01_%Y%m??.nc'
+            else:
+                channels = ['me1','me2','me3','me4','me5']
+                resolution = ['2','2','4','16','32']
+                pathformat = []
+                for idx, channel in enumerate(channels):
+                    pathformat.append(remote_path + 'g' + str(prb) + '_maged_19'+channel+'_'+resolution[idx]+'s_%Y%m%d_%Y%m%d.nc')
+        elif instrument == 'magpd':
+            # magnetospheric proton detector -- only valid on GOES 13, 14, 15
+            if datatype == '1min':
+                pathformat = remote_path + 'g' + str(prb) + '_magpd_19mp15_1m_%Y%m01_%Y%m??.nc'
+            elif datatype == '5min':
+                pathformat = remote_path + 'g' + str(prb) + '_magpd_19mp15_5m_%Y%m01_%Y%m??.nc'
+            else:
+                channels = ['mp1','mp2','mp3','mp4','mp5']
+                resolution = ['16','16','16','32','32']
+                pathformat = []
+                for idx, channel in enumerate(channels):
+                    pathformat.append(remote_path + 'g' + str(prb) + '_magpd_19'+channel+'_'+resolution[idx]+'s_%Y%m%d_%Y%m%d.nc')
+        elif instrument == 'hepad':
+            # high energy proton and alpha detector -- valid for GOES 08-15
+            if datatype == '1min':
+                pathformat = [remote_path + 'g' + str(prb) + '_hepad_ap_1m_%Y%m01_%Y%m??.nc',
+                                                             '_hepad_s15_1m_%Y%m01_%Y%m??.nc']
+            elif datatype == '5min':
+                pathformat = [remote_path + 'g' + str(prb) + '_hepad_ap_5m_%Y%m01_%Y%m??.nc',
+                                                             '_hepad_s15_5m_%Y%m01_%Y%m??.nc']
+            else:
+                pathformat = [remote_path + 'g' + str(prb) + '_hepad_ap_32s_%Y%m%d_%Y%m%d.nc',
+                                                             '_hepad_s15_4s_%Y%m%d_%Y%m%d.nc']
+        elif instrument == 'xrs':
+            # x-ray sensor -- valid for GOES 08-15
+            if datatype == '1min':
+                pathformat = remote_path + 'g' + str(prb) + '_xrs_1m_%Y%m01_%Y%m??.nc'
+            elif datatype == '5min':
+                pathformat = remote_path + 'g' + str(prb) + '_xrs_5m_%Y%m01_%Y%m??.nc'
+            else:
+                pathformat = remote_path + 'g' + str(prb) + '_xrs_2s_%Y%m%d_%Y%m%d.nc'
 
         # find the full remote path names using the trange
-        remote_names = dailynames(file_format=pathformat, trange=trange)
+        if isinstance(pathformat, list):
+            remote_names = []
+            for path in pathformat:
+                remote_names.extend(dailynames(file_format=path, trange=trange))
+        else:
+            remote_names = dailynames(file_format=pathformat, trange=trange)
 
         out_files = []
 
