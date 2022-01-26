417de8fec3325d24a311565050928881b3cb1a55
Alexander Drozdov <adrozdov@ucla.edu>
2022-01-25 16:51:12 -0800

    Simplified tplot for the metadata test

--- pyspedas/themis/tests/test_cal_fit_tplot_metadata.py
+++ pyspedas/themis/tests/test_cal_fit_tplot_metadata.py
@@ -8,5 +8,6 @@ pyspedas.themis.fit(trange=t, probe='a', level='l1', get_support_data=True,
 cal_fit(probe='a')
 
 tplot_names()
-tplot(['tha_fgs', 'tha_fgs_sigma', 'tha_fit_bfit', 'tha_fit_efit',
-       'tha_efs', 'tha_efs_sigma', 'tha_efs_0', 'tha_efs_dot0'])
+tplot(['tha_fit_bfit', 'tha_fit_efit'])
+#tplot(['tha_fgs', 'tha_fgs_sigma', 'tha_fit_bfit', 'tha_fit_efit',
+#       'tha_efs', 'tha_efs_sigma', 'tha_efs_0', 'tha_efs_dot0'])
