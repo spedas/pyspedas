9c1a956e93c96fea92bb7c2613cbf4676d630b63
Eric Grimes <egrimes@igpp.ucla.edu>
2022-03-20 11:03:39 -0700

    Bumping the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='1.3.0',
+    version='1.3.1',
     description='Python Space Physics Environment Data Analysis Software (SPEDAS)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
