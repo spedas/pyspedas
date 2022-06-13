3f0fffc8d2ddce96c91862232391f8d3702da3be
supervised <egrimes@igpp.ucla.edu>
2022-06-13 16:01:22 -0700

    adding initial routine for plotting slices

---
+++ pyspedas/particles/spd_slice2d/slice2d_getinfo.py
@@ -0,0 +1,49 @@
+from pyspedas import time_string
+from pyspedas.particles.spd_units_string import spd_units_string
+
+
+def slice2d_getinfo(the_slice, title=None, xtitle=None, ytitle=None, ztitle=None):
+    """
+    """
+    if title is None:
+        sc = the_slice.get('spacecraft')
+        if sc is None:
+            sc = ''
+
+        if not isinstance(the_slice['trange'][0], str):
+            trange_str = ' -> '.join(time_string(the_slice['trange']))
+        else:
+            trange_str = ' -> '.join(the_slice['trange'])
+
+        title = the_slice['project_name'] + ' ' + sc + ' ' + the_slice['data_name'] + ' (' + the_slice[
+            'rotation'] + ') ' + trange_str
+
+    if xtitle is None or ytitle is None:
+        xt = 'x'
+        yt = 'y'
+
+        rot = the_slice['rotation']
+
+        if rot == 'yz':
+            xt = 'y'
+            yt = 'z'
+        elif rot == 'xz':
+            xt = 'x'
+            yt = 'z'
+
+        # add prefix
+        if the_slice['energy']:
+            xyprefix = 'E'
+        else:
+            xyprefix = 'V'
+
+        if xtitle is None:
+            xtitle = xyprefix + xt + ' (' + the_slice['xyunits'] + ')'
+
+        if ytitle is None:
+            ytitle = xyprefix + yt + ' (' + the_slice['xyunits'] + ')'
+
+        if ztitle is None:
+            ztitle = spd_units_string(the_slice['units_name'])
+
+    return {'title': title, 'xtitle': xtitle, 'ytitle': ytitle, 'ztitle': ztitle}
---
+++ pyspedas/particles/spd_slice2d/slice2d_plot.py
@@ -0,0 +1,56 @@
+import pytplot
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+from .slice2d_getinfo import slice2d_getinfo
+
+
+def plot(the_slice,
+         xrange=None,
+         yrange=None,
+         zrange=None,
+         plotsize=10):
+    """
+
+    """
+    spec_options = {}
+
+    if zrange is None:
+        spec_options['norm'] = mpl.colors.LogNorm()
+    else:
+        spec_options['norm'] = mpl.colors.LogNorm(vmin=zrange[0], vmax=zrange[1])
+
+    _colors = pytplot.spedas_colorbar
+    spd_map = [(np.array([r, g, b])).astype(np.float64) / 256 for r, g, b in zip(_colors.r, _colors.g, _colors.b)]
+    cmap = LinearSegmentedColormap.from_list('spedas', spd_map)
+    spec_options['cmap'] = cmap
+
+    fig, axes = plt.subplots()
+    fig.set_size_inches(plotsize, plotsize)
+
+    if xrange is not None:
+        axes.set_xlim(xrange)
+
+    if yrange is not None:
+        axes.set_ylim(yrange)
+
+    info = slice2d_getinfo(the_slice)
+
+    axes.set_title(info['title'])
+    axes.set_ylabel(info['ytitle'])
+    axes.set_xlabel(info['xtitle'])
+
+    box = axes.get_position()
+    pad, width = 0.02, 0.01
+    cax = fig.add_axes([box.xmax + pad, box.ymin, width, box.height])
+
+    im = axes.pcolormesh(the_slice['xgrid'], the_slice['ygrid'], the_slice['data'].T, **spec_options)
+
+    colorbar = fig.colorbar(im, cax=cax)
+
+    colorbar.set_label(info['ztitle'])
+
+    # draw lines at the origin
+    axes.axvline(x=0, linestyle=(0, (5, 10)), color='black')
+    axes.axhline(y=0, linestyle=(0, (5, 10)), color='black')
