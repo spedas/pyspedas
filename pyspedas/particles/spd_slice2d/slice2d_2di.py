80b9a5512154b126df09ce750f65f7477904e024
supervised <egrimes@igpp.ucla.edu>
2022-06-21 09:22:51 -0700

    2D interpolation bug fixes

--- pyspedas/particles/spd_slice2d/slice2d_2di.py
+++ pyspedas/particles/spd_slice2d/slice2d_2di.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.interpolate
 import scipy.spatial
-
+from matplotlib.tri import Triangulation, LinearTriInterpolator
 
 def slice2d_2di(datapoints, xyz, resolution, thetarange=None, zdirrange=None):
     """
@@ -40,7 +40,7 @@ def slice2d_2di(datapoints, xyz, resolution, thetarange=None, zdirrange=None):
     # note: the following algorithm was translated directly
     # from IDL, and apparently came from thm_esa_slice2d
     # originally
-    uni2 = np.unique(x, return_index=True)[1]
+    uni2 = np.unique(x, return_index=True)[1]+1
     uni1 = np.insert(uni2[0:len(uni2)-1] + 1, 0, 0)
 
     kk = 0
@@ -53,7 +53,7 @@ def slice2d_2di(datapoints, xyz, resolution, thetarange=None, zdirrange=None):
         datapointsi = datapointsi[np.argsort(yi)]
         yi = yi[np.argsort(yi)]
 
-        index2 = np.unique(yi, return_index=True)[1]
+        index2 = np.unique(yi, return_index=True)[1]+1
         if len(index2) == 1:
             index1 = 0
         else:
