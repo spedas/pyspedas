d0e05115a83d5f9d60e0fd9fd4c3e4bc0e35d551
huangzesen <hzsbazinga@outlook.com>
2022-06-22 10:37:12 -0400

    Update load.py

--- pyspedas/psp/load.py
+++ pyspedas/psp/load.py
@@ -63,6 +63,9 @@ def load(trange=['2018-11-5', '2018-11-6'],
                 dtype_tmp = datatype[:11]
                 stype_tmp = datatype[12:]
             pathformat = instrument + '/' + level + '/' + dtype_tmp + '/' + stype_tmp + '/%Y/psp_fld_' + level + '_' + datatype + '_%Y%m%d_v??.cdf'
+        elif datatype['sqtn_rfs_v1v2']:
+            # unfortunately the naming format of quasi-thermal-noise cdf file is different from others
+            pathformat = instrument + '/' + level + '/' + datatype + '/%Y/psp_fld_' + level + '_' + datatype + '_%Y%m%d%H_v?.?.cdf'
 
         else:
             pathformat = instrument + '/' + level + '/' + datatype + '/%Y/psp_fld_' + level + '_' + datatype + '_%Y%m%d%H_v??.cdf'
