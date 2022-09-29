9332bece8c98c14a2431f2af1bbc3dfbbee8b4f5
supervised <egrimes@igpp.ucla.edu>
2022-09-28 19:05:43 -0700

    adding more tests

--- pyspedas/mms/tests/slice2d.py
+++ pyspedas/mms/tests/slice2d.py
@@ -1,5 +1,6 @@
 import unittest
 from pyspedas.mms.particles.mms_part_slice2d import mms_part_slice2d
+from pyspedas.particles.spd_units_string import spd_units_string
 
 
 class SliceTests(unittest.TestCase):
@@ -10,6 +11,32 @@ class SliceTests(unittest.TestCase):
         for spc in species:
             for rotation in rotations:
                 mms_part_slice2d(time=time, probe='1', species=spc, data_rate='brst', rotation=rotation, save_png='test_fpi_brst_' + spc + '_' + rotation, display=False)
+
+    def test_fpi_2d_interp(self):
+        time = '2015-10-16/13:06:30'
+        mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
+                         interpolation='2d', save_png='test_fpi_brst_2d_interp', display=False)
+
+    def test_fpi_limits(self):
+        time = '2015-10-16/13:06:30'
+        mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy', erange=[0, 10000],
+                         save_png='test_fpi_brst_erange', display=False)
+
+    def test_hpca(self):
+        time = '2015-10-16/13:06:30'
+        mms_part_slice2d(time=time, probe='1', species='hplus', instrument='hpca', data_rate='brst', rotation='xy',
+                         save_png='test_hpca_brst', display=False)
+
+    def test_units_string(self):
+        self.assertTrue(spd_units_string('counts') == 'Counts')
+        self.assertTrue(spd_units_string('rate') == 'Rate (#/sec)')
+        self.assertTrue(spd_units_string('eflux') == 'Energy Flux (eV / sec / $cm^2$ / ster / eV)')
+        self.assertTrue(spd_units_string('flux') == 'Flux (# / sec / $cm^2$ / ster / eV)')
+        self.assertTrue(spd_units_string('df') == 'f ($s^3$ / $cm^3$ / $km^3$)')
+        self.assertTrue(spd_units_string('df_cm') == 'f ($s^3$ / $cm^6$)')
+        self.assertTrue(spd_units_string('df_km') == 'f ($s^3$ / $km^6$)')
+        self.assertTrue(spd_units_string('e2flux') == '$Energy^2$ Flux ($eV^2$ / sec / $cm^2$ / ster /eV)')
+        self.assertTrue(spd_units_string('e3flux') == '$Energy^3$ Flux ($eV^3$ / sec / $cm^2$ / ster /eV)')
 
 
 if __name__ == '__main__':
