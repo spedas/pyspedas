93c25f4351550f4d8ab5b65ee14233a63029ee0b
supervised <egrimes@igpp.ucla.edu>
2022-10-26 14:57:50 -0700

    updated to use spherical linear interpolation using quaternions when the timestamps don't match (like we do in IDL)

--- pyspedas/cotrans/tvector_rotate.py
+++ pyspedas/cotrans/tvector_rotate.py
@@ -2,6 +2,8 @@ import logging
 import numpy as np
 from pyspedas import tnames, tinterpol
 from pytplot import get_data, store_data
+from pyspedas.cotrans.quaternions import qtom, mtoq, qslerp
+from pyspedas.cotrans.matrix_array_lib import ctv_verify_mats, ctv_left_mats, ctv_swap_hands
 
 
 def tvector_rotate(mat_var_in, vec_var_in, newname=None):
@@ -52,25 +54,40 @@ def tvector_rotate(mat_var_in, vec_var_in, newname=None):
 
     out_names = []
 
-    mat_data = get_data(mat_var_in)
-
     # loop over the vectors
     for vec_var, new_var in zip(vec_var_in, newname):
         vec_data = get_data(vec_var)
         vec_metadata = get_data(vec_var, metadata=True)
+        mat_data = get_data(mat_var_in)
+        m_d_y = mat_data.y
 
         if not np.array_equal(vec_data.times, mat_data.times) and len(mat_data.times) != 1:
-            logging.info('Interpolating the matrix timestamps to the vector time stamps')
-            tinterpol(mat_var_in, vec_var)
-            mat_data = get_data(mat_var_in + '-itrp')
+            verify_check = ctv_verify_mats(mat_data.y)
+
+            is_left_mat = ctv_left_mats(mat_data.y)
+
+            # left-handed matrices can mess up qslerping
+            if is_left_mat:
+                q_in = mtoq(ctv_swap_hands(mat_data.y))
+            else:
+                q_in = mtoq(mat_data.y)
+
+            # interpolate quaternions
+            q_out = qslerp(q_in, mat_data.times, vec_data.times)
+
+            # turn quaternions back into matrices
+            m_d_y = qtom(q_out)
+
+            if is_left_mat:
+                m_d_y = ctv_swap_hands(m_d_y)
 
         vec_fac = np.zeros((len(vec_data.times), len(vec_data.y[0, :])))
 
         for i in range(0, len(vec_data.times)):
-            if mat_data.y.shape[0] == 1:  # only a single matrix
-                matrix = mat_data.y[0, :, :]
+            if m_d_y.shape[0] == 1:  # only a single matrix
+                matrix = m_d_y[0, :, :]
             else:
-                matrix = mat_data.y[i, :, :]
+                matrix = m_d_y[i, :, :]
 
             vec_fac[i, :] = matrix @ vec_data.y[i, :]
 
