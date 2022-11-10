b92e693b1c70cd3b75f7c9b4bcb19026ab55f6e5
supervised <egrimes@igpp.ucla.edu>
2022-11-10 05:39:05 -0800

    improving error message when the number of colors doesn't match the number of lines (#166)

--- pytplot/MPLPlotter/lineplot.py
+++ pytplot/MPLPlotter/lineplot.py
@@ -75,12 +75,12 @@ def lineplot(var_data,
         colors = np.array(colors)
         if len(colors.shape) == 1:
             if len(colors) != num_lines:
-                print('Problem with the number of line colors specified')
+                print('Incorrect number of line colors specified; expected: ' + str(num_lines) + '; got: ' + str(len(colors)))
                 return
         else:
             # time varying symbol colors, not supported yet
             if colors.shape[1] != num_lines:
-                print('Problem with the number of line colors specified')
+                print('Incorrect number of line colors specified; expected: ' + str(num_lines) + '; got: ' + str(colors.shape[1]))
                 return
     else:
         if style is None:
