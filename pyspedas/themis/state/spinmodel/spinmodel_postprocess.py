844fc444e9c2cf3f145db2c41ffc11b1cd925ad2
Jim Lewis <jwl@ssl.berkeley.edu>
2023-01-13 20:56:58 -0800

    Avoid crashing if support data requested, but only a subset that doesn't include all spin model variables.

--- pyspedas/themis/state/spinmodel/spinmodel_postprocess.py
+++ pyspedas/themis/state/spinmodel/spinmodel_postprocess.py
@@ -1,4 +1,5 @@
 from .spinmodel import Spinmodel, save_spinmodel
+from pyspedas.utilities.data_exists import data_exists
 
 
 def spinmodel_postprocess(probe: str):
@@ -13,6 +14,24 @@ def spinmodel_postprocess(probe: str):
         probe (str):  A single letter string specifying the probe for the models being built.
 
     """
+
+    # It is possible that /get_support_data was specified, but only a limited set of variables
+    # was requested.  Check that all needed spin model variables are present before attempting
+    # to create the models.
+
+    sm_quantities = ['tend', 'spinper', 'c', 'phaserr', 'nspins', 'npts', 'maxgap', 'initial_delta_phi', 'idpu_spinper',
+                     'segflags']
+    missing_var = False
+    for v in sm_quantities:
+        non_ecl_v = 'th' + probe + '_spin_' + v
+        ecl_v = 'th' + probe + '_spin_ecl_' + v
+        if not (data_exists(non_ecl_v) and data_exists(ecl_v)):
+            missing_var = True
+
+    if missing_var is True:
+        print("Some required spin model variables were not requested, skipping spin model creation")
+        return
+
     print("Creating spin model for probe ", probe, "correction level 0")
     # Expect a warning message here: That name is currently not in tplot.
     # Maybe there's a better idiom in pytplot for checking whether a variable exists?
