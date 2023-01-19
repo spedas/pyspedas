3c3a98ad531bf2c732dac42c927a8481245e3c7f
supervised <egrimes@igpp.ucla.edu>
2023-01-18 16:06:25 -0800

    adding the initial ELFIN plug-in

---
+++ pyspedas/elfin/config.py
@@ -0,0 +1,12 @@
+import os
+
+CONFIG = {'local_data_dir': 'elfin_data/',
+          'remote_data_dir': 'https://data.elfin.ucla.edu/'}
+
+# override local data directory with environment variables
+if os.environ.get('SPEDAS_DATA_DIR'):
+    CONFIG['local_data_dir'] = os.sep.join([os.environ['SPEDAS_DATA_DIR'], 'elfin'])
+
+if os.environ.get('ELFIN_DATA_DIR'):
+    CONFIG['local_data_dir'] = os.environ['ELFIN_DATA_DIR']
+    
