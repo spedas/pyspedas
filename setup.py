73c2674ec6a4574741ca9a5f1f3846c9ccd40f48
supervised <egrimes@igpp.ucla.edu>
2023-01-24 07:11:00 -0800

    Cluster paths bug fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='1.4.21',
+    version='1.4.22',
     description='Python Space Physics Environment Data Analysis Software (pySPEDAS)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
