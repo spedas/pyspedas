8b8cb072862d22605592cf9d2141407137297019
Jim Lewis <jwl@ssl.berkeley.edu>
2023-03-13 17:03:15 -0700

    Removed some unneeded single segment extrapolation routines.

--- pyspedas/themis/state/spinmodel/spinmodel_segment.py
+++ pyspedas/themis/state/spinmodel/spinmodel_segment.py
@@ -10,7 +10,7 @@ class SpinmodelSegment:
         c1 (int): Spin count at start of segment
         c2 (int): Spin count at end of segment
         b (float): Initial spin rate in degrees/sec
-        c (float): Accelleration of spin rate in degrees/sec^2
+        c (float): Acceleration of spin rate in degrees/sec^2
         npts (int): Number of data points used to construct this segment
         maxgap (float): Largest gap, in seconds, in data used to construct this segment
         phaserr (float): The maximum fitting error in this segment (seconds)
@@ -63,59 +63,18 @@ class SpinmodelSegment:
                                                                self.initial_delta_phi,
                                                                self.idpu_spinper, self.segflags))
 
-    def extrap_before_t(self,
-                        t: float) -> (float, float, int, float, float):
-        """Return modeled values for a time before the segment start
-
-        Args:
-            t (float): Input time (seconds since epoch)
-
-        Returns:
-            A tuple containing the modeled spin period, spin count, spin phase, eclipse_delta_phi, and t_last
-                at the input time.
-
-        """
-
-        dt = self.t1 - t
-        spinper = 360.0 / self.b
-        fracspins = dt / spinper
-        intspins = math.ceil(fracspins)
-        spinphase = (intspins - fracspins) * 360.0
-        spincount = self.c1 - intspins
-        t_last = self.t1 - intspins * spinper
-        eclipse_delta_phi = 0.0
-        return spinper, spincount, spinphase, eclipse_delta_phi, t_last
-
-    def extrap_after_t(self,
-                       t: float) -> (float, float, int, float, float):
-        """Return modeled values for a time after the segment end
-
-        Args:
-            t (float): Input time (seconds since epoch)
-
-        Returns:
-            A tuple containing the modeled spin period, spin count, spin phase, eclipse_delta_phi, and t_last
-                at the input time.
-
-        """
-
-        dt = t - self.t2
-        bp = self.b + 2.0 * self.c * (self.t2 - self.t1)
-        spinper = 360.0 / bp
-        fracspins = dt / spinper
-        if ((self.segflags & 1) == 1) and (self.idpu_spinper > 1.0):
-            model_phi = fracspins * 360.0
-            idpu_bp = 360.0 / self.idpu_spinper
-            idpu_phi = dt * idpu_bp
-            eclipse_delta_phi = self.initial_delta_phi + (model_phi - idpu_phi)
-        else:
-            eclipse_delta_phi = 0.0
-
-        intspins = math.floor(fracspins)
-        spinphase = (fracspins - intspins) * 360.0
-        spincount = self.c2 + intspins
-        t_last = self.t2 + intspins * spinper
-        return spinper, spinphase, spincount, eclipse_delta_phi, t_last
+# Previously, this module included a set of interpolation and extrapolation routines for
+# single spinmodel segments:
+#
+# extrap_before_t()
+# extrap_after_t()
+# extrap_before_n()
+# extrap_after_n()
+#
+# These routines have been inlined where needed, and users should be calling the interp_t or interp_n methods on
+# the spinmodel object.   If needed, the removed routines can be restored from the version history, or the IDL code.
+# JWL 2023-03-13
+#
 
     def interp_t(self,
                  t: float) -> (float, float, int, float, float):
@@ -155,65 +114,5 @@ class SpinmodelSegment:
         spincount = spincount + self.c1
         return spinper, spinphase, spincount, eclipse_delta_phi, t_last
 
-    def interp_before_n(self,
-                        n: int) -> (float, int, float):
-        """Return modeled values for a spin count before the start of a segment
 
-        Args:
-            n (int): Input spin number
 
-        Returns:
-            A tuple containing the modeled spin period, spin count offset from segment start, and t_last at the
-            specified spin count value
-
-        """
-
-        spinper = 360.0 / self.b
-        nspins = self.c1 - n
-        t_last = self.t1 - nspins * spinper
-        return spinper, nspins, t_last
-
-    def interp_after_n(self,
-                       n: int) -> (float, int, float):
-        """Return modeled values for a spin count after the end of a segment
-
-        Args:
-            n (int): Input spin number
-
-        Returns:
-            A tuple containing the modeled spin period, spin count offset from segment end, and t_last at the specified
-            spin count value
-
-        """
-
-        bp = self.b + 2.0 * self.c * (self.t2 - self.t1)
-        nspins = n - self.c2
-        spinper = 360.0 / bp
-        t_last = self.t2 + spinper * nspins
-        return spinper, nspins, t_last
-
-    def interp_n(self,
-                 n: int) -> (float, int, float):
-        """Return modeled values for a spin count falling within a segment
-
-         Args:
-             n (int): Input spin number
-
-         Returns:
-             A tuple containing the modeled spin period, spin count offset from segment start, and t_last at the
-             specified spin count value
-
-         """
-
-        nspins = n - self.c1
-        phi = 360.0 * nspins
-        if abs(self.c) < 1.0e-12:
-            dt = phi / self.b
-        else:
-            b = self.b
-            c = self.c
-            dt = (-b + math.sqrt(b * b - 4.0 * c * (-phi))) / (2.0 * c)
-        bp = self.b + 2.0 * self.c * dt
-        spinper = 360.0 / bp
-        t_last = self.t1 + dt
-        return spinper, nspins, t_last
