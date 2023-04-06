6a19f0a6bb23db07df1e410fa9be0771050cdc4b
supervised <egrimes@igpp.ucla.edu>
2023-04-06 00:23:44 -0700

    bumping required version of pytplot to get EIS bug fix also bumping version to get this into PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from codecs import open
 
 setup(
     name='pyspedas',
-    version='1.4.25',
+    version='1.4.26',
     description='Python Space Physics Environment Data Analysis Software (pySPEDAS)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -28,7 +28,7 @@ setup(
     install_requires=['numpy>=1.19.5', 'requests', 'geopack>=1.0.10',
                       'cdflib>=0.4.3', 'cdasws>=1.7.24', 'netCDF4>=1.6.2',
                       'pywavelets', 'astropy', 'hapiclient>=0.2.2',
-                      'pytplot-mpl-temp>=2.1.25', 'viresclient'],
+                      'pytplot-mpl-temp>=2.1.26', 'viresclient'],
     python_requires='>=3.7',
     include_package_data=True,
 )
