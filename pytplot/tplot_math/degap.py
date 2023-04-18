fd074a8ba56a4cc2aa6ce7d6d81027ff3064871b
Jim McTiernan <jmjm@berkeley.edu>
2023-04-18 12:53:42 -0700

    Fixed degap with correction from T.Hori

--- pytplot/tplot_math/degap.py
+++ pytplot/tplot_math/degap.py
@@ -33,15 +33,21 @@ def degap(tvar,dt,margin,func='nan',new_tvar = None):
         >>> # TODO
     '''
 
-    gap_size = np.diff(pytplot.data_quants[tvar].coords['time'])
+#fix from T.Hori, 2023-04-10, jimm02
+    #    gap_size = np.diff(pytplot.data_quants[tvar].coords['time']) This is in Nanoseconds, and causes a type mismatch with dt+margin
+    #    new_tvar_index = pytplot.data_quants[tvar].coords['time']
+    new_tvar_index = pytplot.get_data(tvar)[0] #Unix time float64
+    gap_size = np.diff(new_tvar_index)
     gap_index_locations = np.where(gap_size > dt+margin)
-    new_tvar_index = pytplot.data_quants[tvar].coords['time']
     values_to_add = np.array([])
     for i in gap_index_locations[0]:
         values_to_add = np.append(values_to_add, np.arange(new_tvar_index[i], new_tvar_index[i+1], dt))
 
-    new_index = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
-
+    #new_index = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
+    new_index_float64 = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
+    new_index = np.array([datetime.datetime.utcfromtimestamp(t) if np.isfinite(t) else
+                          datetime.datetime.utcfromtimestamp(0) for t in new_index_float64])
+ 
     if func == 'nan':
         method = None
     if func == 'ffill':
