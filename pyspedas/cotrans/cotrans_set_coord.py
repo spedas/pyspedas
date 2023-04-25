f369891dd969dd7d5fa286cf94248d1a26d29e70
Jim Lewis <jwl@ssl.berkeley.edu>
2023-04-25 16:43:19 -0700

    Do not clobber other entries in data_att structure

--- pyspedas/cotrans/cotrans_set_coord.py
+++ pyspedas/cotrans/cotrans_set_coord.py
@@ -34,13 +34,15 @@ def cotrans_set_coord(name, coord):
     # Get the current value of the coordinate system, if present
     coord_in = cotrans_get_coord(name)
 
-    if metadata.get('data_att') is None:
-        metadata['data_att'] = {}
-
     # note: updating the metadata dict directly updates
-    # the variable's metadata in memory, so there's 
+    # the variable's metadata in memory, so there's
     # no need to update the variable with store_data
-    metadata['data_att'] = {'coord_sys': coord}
+
+    data_att = metadata.get('data_att')
+    if data_att is None:
+        metadata['data_att'] = {'coord_sys':coord}
+    else:
+        data_att['coord_sys'] = coord
 
     # should also update the legend, if it includes the coordinate system
     # for this to work, the coordinate system should be in all upper case
