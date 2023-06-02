bdb2e44c77d37bc6215dec92fa42480408558aa6
Nick H <34072991+nickssl@users.noreply.github.com>
2023-06-02 10:59:22 -0700

    Now using logging instead of print

--- pyspedas/cdagui/cdagui.py
+++ pyspedas/cdagui/cdagui.py
@@ -3,7 +3,13 @@ GUI for CDAWeb.
 
 A GUI that can download data files from CDAWeb
 and load them into pytplot variables.
+
 Requires cdasws, PyQt5.
+
+PyQt5 is no longer included in the general pyspedas requirements,
+and if it is not installed an error message will be printed.
+To install PyQt5, please use:
+pip install pyqt5
 
 To open the GUI window:
     from pyspedas.cdagui.cdagui import cdagui
@@ -13,13 +19,11 @@ For cdasws documentation, see:
     https://pypi.org/project/cdasws/
     https://cdaweb.gsfc.nasa.gov/WebServices/REST/py/cdasws/index.html
 
-Notes:
-    To start the gui from the command line:
-        python pyspedas/cdagui/cdagui.py
-    To start the gui inside the python environment:
-        exec(open('cdagui.py').read())
+To start the gui from the command line, use:
+    python pyspedas/cdagui/cdagui.py
 """
 import sys
+import logging
 import datetime
 from cdaweb import CDAWeb
 from config import CONFIG
@@ -34,9 +38,10 @@ try:
                                  QFileDialog, QCalendarWidget, QDialog)
     appx = QApplication(sys.argv)
 except ImportError:
-    print(
-        "PyQt5 library is not installed. You can install it by using: pip install pyqt5")
-    sys.exit()
+    logging.error("PyQt5 library must be installed to use this module.")
+    logging.error("Please install it using: pip install pyqt5")
+    logging.error("Original python error follows...")
+    # sys.exit()
 
 
 def show_my_message(self, title, msg):
@@ -49,7 +54,7 @@ def show_my_message(self, title, msg):
     return alert
 
 
-class cdagui():
+class cdagui(QMainWindow):
     """Main CDAWeb Window."""
 
     def __init__(self, parent=None):
