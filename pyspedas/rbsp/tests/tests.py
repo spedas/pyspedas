bd5caa1b735483514d1a5a0729c8de0b08321a09
supervised <egrimes@igpp.ucla.edu>
2023-06-06 11:36:49 -0700

    Fixing crash with RBSP ESRHELT data

--- pyspedas/rbsp/tests/tests.py
+++ pyspedas/rbsp/tests/tests.py
@@ -51,6 +51,10 @@ class LoadTestCases(unittest.TestCase):
         files = pyspedas.rbsp.rbspice(downloadonly=True, trange=['2018-11-5', '2018-11-6'], datatype='tofxeh', level='l3')
         self.assertTrue(isinstance(files, list))
 
+    def test_load_rbspice_esrhelt(self):
+        rbspice_vars = pyspedas.rbsp.rbspice(trange=['2013-11-5', '2013-11-6'], datatype='ESRHELT', level='l3')
+        self.assertTrue(data_exists('rbspa_rbspice_l3_ESRHELT_FEDU'))
+
     def test_load_rbspice_data(self):
         data = pyspedas.rbsp.rbspice(trange=['2018-11-5', '2018-11-6'], datatype='TOFxEH', level='l3')
         self.assertTrue(data_exists('rbspa_rbspice_l3_TOFxEH_proton_omni_spin'))
