f1dd17f659043e19e151493f69dbdc047dc1d325
Austin Norris <austinnorris@ucla.edu>
2023-06-18 13:14:13 -0700

    Modifies EPD load routine for postprocessing

--- pyspedas/elfin/epd/epd.py
+++ pyspedas/elfin/epd/epd.py
@@ -4,6 +4,7 @@ def elfin_load_epd(trange=['2020-11-01', '2020-11-02'],
         probe='a',
         datatype='pef',
         level='l1',
+        type_='nflux',
         suffix='',
         get_support_data=False,
         varformat=None,
@@ -11,7 +12,10 @@ def elfin_load_epd(trange=['2020-11-01', '2020-11-02'],
         downloadonly=False,
         notplot=False,
         no_update=False,
-        time_clip=False):
+        time_clip=False,
+        nspinsinsum=None,
+        no_spec=False,
+):
     """
     This function loads data from the Energetic Particle Detector (EPD)
 
@@ -34,6 +38,10 @@ def elfin_load_epd(trange=['2020-11-01', '2020-11-02'],
 
         level: str
             Data level; options: 'l1' (default: l1)
+
+        type_ : str
+            Calibrated data type, one of ('raw', 'cps', 'nflux', 'eflux'). ('eflux' not fully tested)
+            Default: 'nflux'.
 
         suffix: str
             The tplot variable names will be given this suffix.  By default,
@@ -65,6 +73,9 @@ def elfin_load_epd(trange=['2020-11-01', '2020-11-02'],
         time_clip: bool
             Time clip the variables to exactly the range specified in the trange keyword
 
+        nspinsinsum : int, optional
+             Number of spins in sum which is needed by the calibration function.
+
     Returns
     ----------
         List of tplot variables created.
@@ -77,12 +88,58 @@ def elfin_load_epd(trange=['2020-11-01', '2020-11-02'],
     if tvars is None or notplot or downloadonly:
         return tvars
 
-    return epd_postprocessing(tvars)
+    CALIBRATED_TYPE_UNITS = {
+        "raw": "counts/sector",
+        "cps": "counts/s",
+        "nflux": "#/(scm!U2!NstrMeV)",
+        "eflux": "keV/(scm!U2!NstrMeV)",
+    }
+
+    if type_ in ("cal", "calibrated") or type_ not in CALIBRATED_TYPE_UNITS.keys():
+        type_ = "nflux"
+
+    return epd_postprocessing(tvars, trange=trange, type_=type_, nspinsinsum=nspinsinsum,
+                              unit=CALIBRATED_TYPE_UNITS[type_], no_spec=no_spec)
 
 
-def epd_postprocessing(variables):
+def epd_postprocessing(
+    tplotnames,
+    trange=None,
+    type_=None,
+    nspinsinsum=None,
+    unit=None,
+    no_spec=False,
+):
     """
-    Placeholder for EPD post-processing
+    Calibrates data from the Energetic Particle Detector (EPD) and sets dlimits.
+
+    Parameters
+    ----------
+        tplotnames : list of str
+            The tplot names of EPD data to be postprocessed.
+
+        trange : list of str
+            Time range of interest [starttime, endtime] with the format
+            ['YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        type_ : str, optional
+            Desired data type. Options: 'raw', 'cps', 'nflux', 'eflux'.
+
+        nspinsinsum : int, optional
+            Number of spins in sum which is needed by the calibration function.
+
+        unit : str, optional
+            Units of the data.
+
+        no_spec : bool
+            Flag to set tplot options to linear rather than the default of spec.
+            Default is False.
+
+    Returns
+    ----------
+        List of tplot variables created.
     """
-    return variables
+
+    return tplotnames
 
