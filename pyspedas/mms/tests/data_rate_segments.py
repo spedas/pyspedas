94feaa9e69f832ac7711d016e4d00481f84831b3
supervised <egrimes@igpp.ucla.edu>
2023-06-27 14:12:20 -0400

    adding a few more checks to increase MMS code coverage

--- pyspedas/mms/tests/data_rate_segments.py
+++ pyspedas/mms/tests/data_rate_segments.py
@@ -1,8 +1,6 @@
-
 import unittest
-
 from pyspedas.mms.spd_mms_load_bss import spd_mms_load_bss
-from pyspedas.mms.mms_load_sroi_segments import mms_load_sroi_segments
+from pyspedas.mms.mms_load_sroi_segments import mms_load_sroi_segments, get_mms_srois
 from pyspedas.mms.mms_load_fast_segments import mms_load_fast_segments
 from pyspedas.mms.mms_load_brst_segments import mms_load_brst_segments
 from pytplot import data_exists
@@ -15,6 +13,14 @@ class SegmentTestCases(unittest.TestCase):
         self.assertTrue(len(sroi[0]) == 28)
         self.assertTrue(sroi[0][0] == 1569849345.0)
         self.assertTrue(sroi[1][0] == 1569923029.0)
+        # error, no trange specified
+        sroi = mms_load_sroi_segments()
+        # error, start time not specified
+        none = get_mms_srois(end_time=1569849345.0)
+        # error, end time not specified
+        none = get_mms_srois(start_time=1569849345.0)
+        # error, probe not specified
+        none = get_mms_srois(start_time=1569849345.0, end_time=1569849345.0)
 
     def test_brst(self):
         brst = mms_load_brst_segments(trange=['2015-10-16', '2015-10-17'])
@@ -22,6 +28,8 @@ class SegmentTestCases(unittest.TestCase):
         self.assertTrue(brst[0][0] == 1444975174.0)
         self.assertTrue(brst[1][0] == 1444975244.0)
         self.assertTrue(data_exists('mms_bss_burst'))
+        # error, no trange specified
+        brst = mms_load_brst_segments()
 
     def test_fast(self):
         fast = mms_load_fast_segments(trange=['2015-10-01', '2015-11-01'])
@@ -29,6 +37,8 @@ class SegmentTestCases(unittest.TestCase):
         self.assertTrue(len(fast[0]) == 35)
         self.assertTrue(fast[0][0] == 1443504404.0)
         self.assertTrue(fast[1][0] == 1443554774.0)
+        # error, no trange specified
+        none = mms_load_fast_segments()
 
     def test_spd_mms_load_bss(self):
         spd_mms_load_bss(trange=['2015-10-01', '2015-11-01'])
