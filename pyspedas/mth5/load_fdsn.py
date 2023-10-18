2b6ab3999001ba011dfcf3b5ef9ee5b68b5877d0
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-10-18 14:43:22 -0700

    Using MTH5 as a context manager and additional guards are added.

--- pyspedas/mth5/load_fdsn.py
+++ pyspedas/mth5/load_fdsn.py
@@ -40,15 +40,13 @@ def load_fdsn(trange=None, network=None, station=None):
         pyspedas.logging.info("Station not specified")
         return
 
-    fdsn_object = FDSN(mth5_version='0.2.0', client="IRIS")
-
     # Determine where data will be stored
     mth5dir = CONFIG['local_data_dir']
     # if os.environ.get('SPEDAS_DATA_DIR'):
     #     mth5dir = os.environ['SPEDAS_DATA_DIR']
 
     # Get MTH5 file from the server
-    # TODO: channel should not be a "*" - we use "*F*"
+    # we use "*F*" channel instead of "*" to extract magnetic data
     request_df = pd.DataFrame(
         {
             "network": [network],
@@ -60,6 +58,8 @@ def load_fdsn(trange=None, network=None, station=None):
         }
     )
 
+    # Initialize FDSN object
+    fdsn_object = FDSN(mth5_version='0.2.0', client="IRIS")
     try:
         mth5_path = fdsn_object.make_mth5_from_fdsn_client(request_df, interact=False, path=mth5dir)
     except Exception:
@@ -78,95 +78,104 @@ def load_fdsn(trange=None, network=None, station=None):
                     pyspedas.logger.info(f"mth5 file {mth5filename} object is now closed.")
         raise
 
-    # TODO: change to context manager as below:
-    # with mth5_object.open_mth5(mth5_path) as mth5_object_ref
-    # with mth5_object.open_mth5(mth5_path) as m
+    # Using MTH5 as a context manager
+    with MTH5() as mth5_object:
+        mth5_object.open_mth5(mth5_path)
+        surveys = mth5_object.surveys_group.groups_list
 
-    mth5_object = MTH5()
-    mth5_object.open_mth5(mth5_path)
-    surveys = mth5_object.surveys_group.groups_list
-    # print(surveys)
+        # mth5_object = MTH5()
+        # mth5_object.open_mth5(mth5_path)
+        # surveys = mth5_object.surveys_group.groups_list
+        # # print(surveys)
 
-    # Create new data for time, x, y and z
-    # TODO: is there a better way to create a new time array?
-    time = np.array([])
-    x = np.array([])
-    y = np.array([])
-    z = np.array([])
+        # Create new data for time, x, y and z
+        # TODO: is there a better way to create a new time array?
+        time = np.array([])
+        x = np.array([])
+        y = np.array([])
+        z = np.array([])
 
-    attr_dict = {}  # metadata
-    units = set()  # basic units
-    measurements_type = set()  # basic type of measurements
+        attr_dict = {}  # metadata
+        units = set()  # basic units
+        measurements_type = set()  # basic type of measurements
 
-    # loop over surveys and runs
-    for survey in surveys:
-        station_data = mth5_object.get_station(station_name=station, survey=survey)
-        attr_dict["metadata"] = station_data.metadata
+        # loop over surveys and runs
+        for survey in surveys:
+            station_data = mth5_object.get_station(station_name=station, survey=survey)
+            attr_dict["metadata"] = station_data.metadata
 
-        # TODO: Check orientation reference frame
-        # "orientation.reference_frame": "geographic"
+            # TODO: Check orientation reference frame
+            # "orientation.reference_frame": "geographic"
 
-        # Loop over runs
-        for run_name in station_data.metadata.run_list:
-            # Get the data
-            run_data = station_data.get_run(run_name=run_name)
+            # Loop over runs
+            for run_name in station_data.metadata.run_list:
+                # Get the data
+                run_data = station_data.get_run(run_name=run_name)
 
-            # Get the run table summary
-            run_ts = run_data.to_runts()
+                # Get the run table summary
+                run_ts = run_data.to_runts()
 
-            # Determine is we have all components
-            # TODO: What to do with the runs without 3 components - operate only with 3 components for now...
-            # TODO: can channels_recorded_magnetic be missing from the dataset?
-            if len(run_ts.dataset.channels_recorded_magnetic) != 3:
-                continue
+                # Determine is we have all components
+                # can channels_recorded_magnetic be missing from the dataset?
+                # TODO: What to do with the runs without 3 components - operate only with 3 components for now...
+                if 'channels_recorded_magnetic' in run_ts.dataset.attrs:
+                    if len(run_ts.dataset.channels_recorded_magnetic) != 3:
+                        pyspedas.logger.info("Run has less than 3 components")
+                        continue
+                else:
+                    pyspedas.logger.warning("Run dataset has no channels_recorded_magnetic attribute")
+                    continue
 
-            # TODO: add check if the filters are available to calibrate data
+                # Check if the filters are available to calibrate data
+                if len(run_ts.filters)  == 0:
+                    pyspedas.logger.warning("Filters were not added to run_ts")
+                    continue
 
-            # Convert counts to physical units
-            run_ts = run_ts.calibrate()
+                # Convert counts to physical units
+                run_ts = run_ts.calibrate()
 
-            # Copy numpy arrays to ensure that data is in memory after mth5 is closed
-            if time.size == 0:
-                time = run_ts.dataset.time.to_numpy()
-            else:
-                time = np.append(time, run_ts.dataset.time.to_numpy())
+                # Copy numpy arrays to ensure that data is in memory after mth5 is closed
+                if time.size == 0:
+                    time = run_ts.dataset.time.to_numpy()
+                else:
+                    time = np.append(time, run_ts.dataset.time.to_numpy())
 
-            # TODO: can channels_recorded_magnetic not be hx, hy, hz? Just in case add the check
-            # TODO: add support for bx, by, bz
-            x = np.append(x, run_ts.dataset.hx.to_numpy())
-            y = np.append(y, run_ts.dataset.hy.to_numpy())
-            z = np.append(z, run_ts.dataset.hz.to_numpy())
+                # TODO: can channels_recorded_magnetic not be hx, hy, hz? Just in case add the check
+                # TODO: add support for bx, by, bz
+                x = np.append(x, run_ts.dataset.hx.to_numpy())
+                y = np.append(y, run_ts.dataset.hy.to_numpy())
+                z = np.append(z, run_ts.dataset.hz.to_numpy())
 
-            try:
-                units.add(run_ts.dataset.hx.units)
-                units.add(run_ts.dataset.hy.units)
-                units.add(run_ts.dataset.hz.units)
-            except AttributeError:
-                pyspedas.logger.warning("Problem with adding run dataset units")
+                try:
+                    units.add(run_ts.dataset.hx.units)
+                    units.add(run_ts.dataset.hy.units)
+                    units.add(run_ts.dataset.hz.units)
+                except AttributeError:
+                    pyspedas.logger.warning("Problem with adding run dataset units")
 
-            try:
-                measurements_type.add(run_ts.dataset.hx.type)
-                measurements_type.add(run_ts.dataset.hx.type)
-                measurements_type.add(run_ts.dataset.hx.type)
-            except AttributeError:
-                pyspedas.logger.warning("Problem with adding run dataset type")
+                try:
+                    measurements_type.add(run_ts.dataset.hx.type)
+                    measurements_type.add(run_ts.dataset.hx.type)
+                    measurements_type.add(run_ts.dataset.hx.type)
+                except AttributeError:
+                    pyspedas.logger.warning("Problem with adding run dataset type")
 
-    # TODO: Should data be sorted before saving
-    data = {'x': time, 'y': np.vstack((x, y, z)).T}
-    tplot_variable = 'fdsn_' + network + '_' + station
+        # TODO: Should data be sorted before saving
+        data = {'x': time, 'y': np.vstack((x, y, z)).T}
+        tplot_variable = 'fdsn_' + network + '_' + station
 
-    # TODO: Add metadata
+        # TODO: Add metadata
 
-    # TODO: Clip time according to original times, add noclip parameter
+        # TODO: Clip time according to original times, add noclip parameter
 
-    store_data(tplot_variable, data=data, attr_dict=attr_dict)
-    tplot_options = {
-        "name": f"FDSN: {network}, station: {station}",  # network and stations
-        "ytitle": f"{','.join(list(measurements_type))}",  # measurment
-        "ysubtitle": f"{','.join(list(units))}"  # units
-    }
+        store_data(tplot_variable, data=data, attr_dict=attr_dict)
+        tplot_options = {
+            "name": f"FDSN: {network}, station: {station}",  # network and stations
+            "ytitle": f"{','.join(list(measurements_type))}",  # measurment
+            "ysubtitle": f"{','.join(list(units))}"  # units
+        }
 
-    options(tplot_variable, opt_dict=tplot_options)
+        options(tplot_variable, opt_dict=tplot_options)
 
-    # It is crucial to close mth5 file
-    mth5_object.close_mth5()
+        # It is crucial to close mth5 file
+        mth5_object.close_mth5()
