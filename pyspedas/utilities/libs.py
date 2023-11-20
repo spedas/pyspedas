2e29b22a414878f5335edcc920ba12fb60792cb7
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-11-20 15:14:14 -0800

    pytplot search is added

--- pyspedas/utilities/libs.py
+++ pyspedas/utilities/libs.py
@@ -3,11 +3,17 @@ Displays location of source files, one line of documentation and the function na
 """
 
 import importlib
-import pkgutil
 import inspect
+import itertools
+import pkgutil
+import sys
+import io
+
+import pytplot
 import pyspedas
 
-def libs(function_name, package=pyspedas):
+
+def libs(function_name, package=None):
     """
     Searches for a specified function within a given package and its submodules,
     and prints information about the function if found. The search is performed
@@ -20,13 +26,13 @@ def libs(function_name, package=pyspedas):
       Default is the pyspedas package. This should be a Python module object.
 
     Note:
-    - All submodules of pyspedas are imported during the search. The package option is
+    - All submodules of pyspedas and pytplot are imported during the search. The package option is
       simply narrows the search.
     - The function specifically searches for functions, not classes or other objects.
     - If multiple functions with the same name exist in different modules within the package,
       it will list them all.
     - The function handles ImportError exceptions by printing an error message and
-      continuing the search.
+      continuing the search, except 'pytplot.QtPlotter'. pytplot.QtPlotter results in error during import and ignored
 
     Example Usage:
     ```python
@@ -51,12 +57,34 @@ def libs(function_name, package=pyspedas):
                 print(f"Function: {full_name}\nLocation: {source_file}\nDocumentation: {first_line_of_doc}\n")
 
     def traverse_modules(package, function_name, pacakge_obj):
-        for importer, modname, ispkg in pkgutil.walk_packages(path=package.__path__, prefix=package.__name__ + '.'):
-            if ispkg:
+        # Add the module itself
+        walk_packages_iterator = pkgutil.walk_packages(path=package.__path__, prefix=package.__name__ + '.')
+        combined_iterator = itertools.chain(((None, package.__name__, True),), walk_packages_iterator)
+
+        for _, modname, ispkg in combined_iterator:
+            if ispkg and 'qtplotter' not in modname.lower():
+                # Save the current stdout so that we can restore it later
+                original_stdout = sys.stdout
+
+                # Redirect stdout to a dummy stream
+                sys.stdout = io.StringIO()
+
                 try:
                     module = importlib.import_module(modname)
+                    if not pacakge_obj:
+                        pacakge_obj = package
+
+                    # Restore the original stdout
+                    sys.stdout = original_stdout
                     list_functions(module, package, function_name, pacakge_obj)
                 except ImportError as e:
+                    # Restore the original stdout
+                    sys.stdout = original_stdout
                     print(f"Error importing module {modname}: {e}")
+                finally:
+                    # Restore the original stdout
+                    sys.stdout = original_stdout
 
-    traverse_modules(pyspedas, function_name, package)
+    for module in [pyspedas, pytplot]:
+        if not package or module.__name__ in package.__name__:
+            traverse_modules(module, function_name, package)
