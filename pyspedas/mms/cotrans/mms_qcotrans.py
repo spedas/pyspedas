f58acd6d3b840285a23c446830d38c2c6df04a0c
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-01-05 11:42:16 -0800

    Added usage example

--- pyspedas/mms/cotrans/mms_qcotrans.py
+++ pyspedas/mms/cotrans/mms_qcotrans.py
@@ -55,12 +55,11 @@ def mms_qcotrans(in_name=None, out_name=None, in_coord=None, out_coord=None, pro
 
     Examples
     --------
-    >>> from pyspedas import mms, mms_qcotrans # Import MMS namespace and mms_qcotrans routine
+    >>> from pyspedas.mms import mec, mms_qcotrans # Import MMS namespace and mms_qcotrans routine
     >>> from pytplot import tplot # Import tplot routine to plot results
-    >>> mms.mec()   # Load quaternions, positions, and velocity variables from MEC data, using default trange and datatypes
+    >>> mec(trange=['2015-10-16', '2015-10-17'], probe='1')   # Load quaternions, positions, and velocity variables from MEC data, using default datatypes
     >>> mms_qcotrans('mms1_mec_v_sm', 'mms1_mec_v_sm_2gse', out_coord='gse') # Transform SM velocity to GSE
-    >>> mms_qcotrans('mms1_mec_r_sm', 'mms1_mec_r_sm_2gse', out_coord='gse') # Transform SM position to GSE
-    >>> tplot(['mms1_mec_v_sm','mms1_mec_v_sm_2gse','mms1_mec_r_sm','mms1_mec_r_sm_2gse']) # Plot inputs and outputs
+    >>> tplot(['mms1_mec_v_sm','mms1_mec_v_sm_2gse']) # Plot input and output variables
 
     """
     valid_probes = ['1', '2', '3', '4']
