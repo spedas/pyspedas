0039ef2f3a1dda3d83728df68b324fd89548856b
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-01-16 19:36:46 -0800

    Added magnetic null type classifier to pyspedas imports.

--- pyspedas/__init__.py
+++ pyspedas/__init__.py
@@ -11,7 +11,7 @@ from .analysis.tdpwrspc import tdpwrspc
 from .analysis.tinterpol import tinterpol
 from .analysis.yclip import yclip
 from .analysis.twavpol import twavpol
-from .analysis.find_magnetic_nulls import find_magnetic_nulls_fote
+from .analysis.find_magnetic_nulls import find_magnetic_nulls_fote, classify_null_type
 from .cdagui.cdagui import cdagui
 from .cotrans.cotrans import cotrans
 from .cotrans.cotrans_get_coord import cotrans_get_coord
