532f3ff558a24605303894e10270dcd25e5d1569
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-01-18 18:49:20 -0800

    Version bump to pick up extended magnetic null type codes, change plot symbols to better match published papers, suppress spacepy import warnings.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from codecs import open
 
 setup(
     name="pyspedas",
-    version="1.5.3",
+    version="1.5.4",
     description="Python Space Physics Environment Data Analysis Software (pySPEDAS)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
