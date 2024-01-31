9ec43c898fcd0191f2358e02fcda0cc2e34dad2c
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-01-30 19:44:24 -0800

    Updated requirements for building readthedocs documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '1.5.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.intersphinx', 'numpydoc']
 
 # Napoleon settings
 napoleon_numpy_docstring = True
