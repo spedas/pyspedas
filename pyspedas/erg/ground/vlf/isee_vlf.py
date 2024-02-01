8b48b9f7ed4c3e9a1d1c509f4b6f9e7006f42e55
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-01-31 17:07:03 -0800

    Add docstrings to routines to load SuperDARN, riometer and VLF data from the ERG Science Center.

--- pyspedas/erg/ground/vlf/isee_vlf.py
+++ pyspedas/erg/ground/vlf/isee_vlf.py
@@ -23,6 +23,69 @@ def isee_vlf(
     ror=True,
     cal_gain=False
 ):
+    """
+    Load ISEE VLF radar data from ERG Science Center
+
+    Parameters
+    ----------
+    trange: list of str
+            time range of interest [starttime, endtime] with the format
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+            Default: ['2017-03-30/12:00:00', '2017-03-30/15:00:00']
+
+    suffix: str
+            The tplot variable names will be given this suffix.  Default: ''
+
+    site: str or list of str
+            The site or list of sites to load. Valid values: 'ath', 'gak', 'hus', 'ist', 'kap', 'mam', 'nai', 'all'
+            Default: ['all']
+
+    get_support_data: bool
+            If true, data with an attribute "VAR_TYPE" with a value of "support_data"
+            or 'data' will be loaded into tplot. Default: False
+
+    varformat: str
+            The CDF file variable formats to load into tplot.  Wildcard character
+            "*" is accepted.  Default: None (all variables will be loaded).
+
+    varnames: list of str
+            List of variable names to load. Default: [] (all variables will be loadee)
+
+    downloadonly: bool
+            Set this flag to download the CDF files, but not load them into
+            tplot variables. Default: False
+
+    notplot: bool
+            Return the data in hash tables instead of creating tplot variables. Default: False
+
+    no_update: bool
+            If set, only load data from your local cache. Default: False
+
+    uname: str
+            User name.  Default: None
+
+    passwd: str
+            Password. Default: None
+
+    time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword. Default: False
+
+    ror: bool
+            If set, print PI info and rules of the road. Default: True
+
+    cal_gain: bool
+        If True, use calibration parameters in the CDF file to apply a gain calibration.
+        Default: False
+
+    Returns
+    -------
+
+    Examples
+    ________
+
+
+    """
 
     site_code_all = ['ath', 'gak', 'hus', 'ist', 'kap', 'mam', 'nai']
 
