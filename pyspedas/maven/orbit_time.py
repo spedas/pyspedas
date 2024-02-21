868b77c1b637a27c792dafd937d6c940beb75945
Nick H <34072991+nickssl@users.noreply.github.com>
2024-02-21 10:43:16 -0800

    Added docstrings for MAVEN mission.
    Also fixed some other problems:
    1. Removed global variables for username and password.
    2. Changed download code so that orbit files are not created in the source code directory.

--- pyspedas/maven/orbit_time.py
+++ pyspedas/maven/orbit_time.py
@@ -1,29 +1,73 @@
-
-"""
-File:
-    orbit_time.py
-
-Description:
-    MAVEN orbit file handling routines.
-"""
-
+import logging
 import os
 
 
 def month_to_num(month_string):
+    """
+    Converts a three-letter month abbreviation to its corresponding two-digit number.
 
-    month_to_num_dict = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04', 'MAY': '05', 'JUN': '06', 'JUL': '07',
-                         'AUG': '08', 'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
+    Parameters
+    ----------
+    month_string : str
+        The three-letter month abbreviation (e.g., "JAN", "FEB", etc.).
+
+    Returns
+    -------
+    str
+        The two-digit number corresponding to the month abbreviation.
+
+    Raises
+    ------
+    ValueError
+        If the month string is not a valid abbreviation.
+
+    Examples
+    --------
+    >>> month_to_num("JAN")
+    '01'
+    >>> month_to_num("FEB")
+    '02'
+    """
+
+    month_to_num_dict = {
+        "JAN": "01",
+        "FEB": "02",
+        "MAR": "03",
+        "APR": "04",
+        "MAY": "05",
+        "JUN": "06",
+        "JUL": "07",
+        "AUG": "08",
+        "SEP": "09",
+        "OCT": "10",
+        "NOV": "11",
+        "DEC": "12",
+    }
 
     if month_string in month_to_num_dict.keys():
         return month_to_num_dict[month_string]
     else:
-        raise ValueError('Month string is not valid')
+        raise ValueError("Month string is not valid")
 
 
 def orbit_time(begin_orbit, end_orbit=None):
-    orb_file = os.path.join(os.path.dirname(__file__),
-                            'maven_orb_rec.orb')
+    """
+    Calculate the corresponding time range for a given MAVEN orbit number or range of orbit numbers.
+
+    Parameters
+    ----------
+    begin_orbit : int
+        The beginning orbit number.
+    end_orbit : int, optional
+        The ending orbit number. If not provided, the time range will be calculated for a single orbit.
+
+    Returns
+    -------
+    list
+        A list containing the beginning and ending time in the format [begin_time, end_time].
+        If the orbit numbers are not found, [None, None] will be returned.
+    """
+    orb_file = os.path.join(os.path.dirname(__file__), "maven_orb_rec.orb")
 
     with open(orb_file, "r") as f:
         if end_orbit is None:
@@ -34,14 +78,20 @@ def orbit_time(begin_orbit, end_orbit=None):
         f.readline()
         for line in f:
             line = line[0:28]
-            line = line.split(' ')
-            line = [x for x in line if x != '']
+            line = line.split(" ")
+            line = [x for x in line if x != ""]
             orbit_num.append(int(line[0]))
-            time.append(line[1] + "-" + month_to_num(line[2]) + "-" + line[3] + "T" + line[4])
+            time.append(
+                line[1] + "-" + month_to_num(line[2]) + "-" + line[3] + "T" + line[4]
+            )
         try:
-
-            if orbit_num.index(begin_orbit) > len(time) or orbit_num.index(end_orbit) + 1 > len(time):
-                print("Orbit numbers not found.  Please choose a number between 1 and %s.", orbit_num[-1])
+            if orbit_num.index(begin_orbit) > len(time) or orbit_num.index(
+                end_orbit
+            ) + 1 > len(time):
+                logging.warning(
+                    "Orbit numbers not found.  Please choose a number between 1 and %s.",
+                    orbit_num[-1],
+                )
                 return [None, None]
             else:
                 begin_time = time[orbit_num.index(begin_orbit)]
