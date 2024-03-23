e49b847721a16d2e37c95f7bded60c7351d26a0d
Nick H <34072991+nickssl@users.noreply.github.com>
2024-03-23 11:03:18 -0700

    Minor changes to docstring. Added example.

--- pytplot/tplot_math/degap.py
+++ pytplot/tplot_math/degap.py
@@ -1,8 +1,3 @@
-# Copyright 2020 Regents of the University of Colorado. All Rights Reserved.
-# Released under the MIT license.
-# This software was developed at the University of Colorado's Laboratory for Atmospheric and Space Physics.
-# Verify current version before use at: https://github.com/MAVENSDC/Pytplot
-
 import pytplot
 import numpy as np
 import pandas as pd
@@ -10,7 +5,17 @@ import copy
 import datetime
 import logging
 
-def degap(tvar,dt = None, margin = 0.25, func='nan', newname=None, new_tvar = None, onenanpergap = False, twonanpergap = False):
+
+def degap(
+    tvar,
+    dt=None,
+    margin=0.25,
+    func="nan",
+    newname=None,
+    new_tvar=None,
+    onenanpergap=False,
+    twonanpergap=False,
+):
     """
     Fills gaps in the data either with NaNs or the last number.
 
@@ -25,10 +30,10 @@ def degap(tvar,dt = None, margin = 0.25, func='nan', newname=None, new_tvar = No
         func : str, optional
             Either 'nan' or 'ffill', which overrides normal interpolation with NaN
             substitution or forward-filled values.
-        new_tvar : str, optional (Deprecated)
+        newname : str, optional
             The new tplot variable name to store the data into.  If None, then the data is overwritten.
             THIS is not an option for multiple variable input, for multiple or pseudo variables, the data is overwritten.
-        newname : str, optional
+        new_tvar : str, optional (Deprecated)
             The new tplot variable name to store the data into.  If None, then the data is overwritten.
             THIS is not an option for multiple variable input, for multiple or pseudo variables, the data is overwritten.
         onenanpergap : bool
@@ -37,79 +42,114 @@ def degap(tvar,dt = None, margin = 0.25, func='nan', newname=None, new_tvar = No
             if set to True, then only insert one NaN value, rather than adding NaN values at dt resolution
     Returns:
         None
+            Creates a new tplot variable with the degap data
 
     Examples:
-        >>> # TODO
+        >>> import pytplot
+        >>> time = [pytplot.time_float("2020-01-01") + i for i in [1, 2, 3, 4, 5, 6, 9, 10, 11]]
+        >>> y = [1, 2, 3, 4, 5, 6, 9, 10, 11]
+        >>> pytplot.store_data("a", data={"x": time, "y": y})
+        >>> degap("a", newname="b")
+        >>> b = pytplot.get("b")
+        >>> print(b)
     """
 
     # new_tvar is deprecated in favor of newname
     if new_tvar is not None:
-        logging.info("degap: The new_tvar parameter is deprecated. Please use newname instead.")
+        logging.info(
+            "degap: The new_tvar parameter is deprecated. Please use newname instead."
+        )
         newname = new_tvar
 
-    #check for globbed or array input, and call recursively
+    # check for globbed or array input, and call recursively
     tn = pytplot.tnames(tvar)
     if len(tn) > 1:
         for j in range(len(tn)):
-            pytplot.degap(tn[j], dt=dt, margin=margin, func=func, onenanpergap=onenanpergap, twonanpergap=twonanpergap)
+            pytplot.degap(
+                tn[j],
+                dt=dt,
+                margin=margin,
+                func=func,
+                onenanpergap=onenanpergap,
+                twonanpergap=twonanpergap,
+            )
         return
 
-    #here we have 1 variable
+    # here we have 1 variable
 
-    #fix from T.Hori, 2023-04-10, jimm02
+    # fix from T.Hori, 2023-04-10, jimm02
     #    gap_size = np.diff(pytplot.data_quants[tvar].coords['time']) This is in Nanoseconds, and causes a type mismatch with dt+margin
     #    new_tvar_index = pytplot.data_quants[tvar].coords['time']
-    new_tvar_index = pytplot.get_data(tvar)[0] #Unix time float64
+    new_tvar_index = pytplot.get_data(tvar)[0]  # Unix time float64
     gap_size = np.diff(new_tvar_index)
 
-    #Default for dt is the median value of gap_size, the time interval differences
+    # Default for dt is the median value of gap_size, the time interval differences
     if dt == None:
         dt = np.median(gap_size)
 
-    gap_index_locations = np.where(gap_size > dt+margin)
+    gap_index_locations = np.where(gap_size > dt + margin)
     values_to_add = np.array([])
     if onenanpergap == True:
         for i in gap_index_locations[0]:
-            values_to_add = np.append(values_to_add, new_tvar_index[i]+dt)
+            values_to_add = np.append(values_to_add, new_tvar_index[i] + dt)
     elif twonanpergap == True:
-        #add two NaN values between the two values, either at margin if it's nonzero, or at dt/2
-        #since the gap is greater than dt, this will work
+        # add two NaN values between the two values, either at margin if it's nonzero, or at dt/2
+        # since the gap is greater than dt, this will work
         if margin > 0.0:
-            if margin < dt/2.0:
+            if margin < dt / 2.0:
                 dt_nan = margin
             else:
-                dt_nan = dt/2.0
+                dt_nan = dt / 2.0
         else:
-            dt_nan = dt/2.0
+            dt_nan = dt / 2.0
         for i in gap_index_locations[0]:
-            values_to_add = np.append(values_to_add, new_tvar_index[i]+dt_nan)
-            values_to_add = np.append(values_to_add, new_tvar_index[i+1]-dt_nan)
-    else:  
+            values_to_add = np.append(values_to_add, new_tvar_index[i] + dt_nan)
+            values_to_add = np.append(values_to_add, new_tvar_index[i + 1] - dt_nan)
+    else:
         for i in gap_index_locations[0]:
-            values_to_add = np.append(values_to_add, np.arange(new_tvar_index[i], new_tvar_index[i+1], dt))
+            values_to_add = np.append(
+                values_to_add, np.arange(new_tvar_index[i], new_tvar_index[i + 1], dt)
+            )
 
-    #new_index = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
-    new_index_float64 = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
-    new_index = np.array([datetime.datetime.utcfromtimestamp(t) if np.isfinite(t) else
-                          datetime.datetime.utcfromtimestamp(0) for t in new_index_float64])
- 
-    if func == 'nan':
+    # new_index = np.sort(np.unique(np.concatenate((values_to_add, new_tvar_index))))
+    new_index_float64 = np.sort(
+        np.unique(np.concatenate((values_to_add, new_tvar_index)))
+    )
+    new_index = np.array(
+        [
+            (
+                datetime.datetime.utcfromtimestamp(t)
+                if np.isfinite(t)
+                else datetime.datetime.utcfromtimestamp(0)
+            )
+            for t in new_index_float64
+        ]
+    )
+
+    if func == "nan":
         method = None
-    if func == 'ffill':
-        method = 'ffill'
+    if func == "ffill":
+        method = "ffill"
 
-    a = pytplot.data_quants[tvar].reindex({'time': new_index}, method=method)
+    a = pytplot.data_quants[tvar].reindex({"time": new_index}, method=method)
 
     if newname is None:
         a.name = tvar
         a.attrs = copy.deepcopy(pytplot.data_quants[tvar].attrs)
         pytplot.data_quants[tvar] = copy.deepcopy(a)
     else:
-        if 'spec_bins' in a.coords:
-            pytplot.store_data(newname, data={'x': a.coords['time'], 'y': a.values, 'v': a.coords['spec_bins']})
-            pytplot.data_quants[newname].attrs = copy.deepcopy(pytplot.data_quants[tvar].attrs)
+        if "spec_bins" in a.coords:
+            pytplot.store_data(
+                newname,
+                data={"x": a.coords["time"], "y": a.values, "v": a.coords["spec_bins"]},
+            )
+            pytplot.data_quants[newname].attrs = copy.deepcopy(
+                pytplot.data_quants[tvar].attrs
+            )
         else:
-            pytplot.store_data(newname, data={'x': a.coords['time'], 'y': a.values})
-            pytplot.data_quants[newname].attrs = copy.deepcopy(pytplot.data_quants[tvar].attrs)
+            pytplot.store_data(newname, data={"x": a.coords["time"], "y": a.values})
+            pytplot.data_quants[newname].attrs = copy.deepcopy(
+                pytplot.data_quants[tvar].attrs
+            )
 
     return
--- pytplot/tplot_math/makegap.py
+++ pytplot/tplot_math/makegap.py
@@ -1,13 +1,9 @@
-# Copyright 2020 Regents of the University of Colorado. All Rights Reserved.
-# Released under the MIT license.
-# This software was developed at the University of Colorado's Laboratory for Atmospheric and Space Physics.
-# Verify current version before use at: https://github.com/MAVENSDC/Pytplot
-
 import pytplot
 import numpy as np
 
-def makegap(var_data, dt = None, margin = 0.0, func='nan'):
-    '''
+
+def makegap(var_data, dt=None, margin=0.0, func="nan"):
+    """
     Fills gaps in the data either with NaNs or the last number.  This
     is identical to degap, except operates directly on the data and
     time arrays, rather than the tplot variable. This is intended for
@@ -16,12 +12,13 @@ def makegap(var_data, dt = None, margin = 0.0, func='nan'):
     that data into the proper form.
 
     Parameters:
-        var_data : number, the data for the tplot variable, contains at least, tags for 'y' and 'times'
+        var_data : float
+            The data for the tplot variable, a structure that contains at least, tags for 'y' and 'times'
         dt : int/float
             Step size of the data in seconds, default is to use the median time interval
         margin : int/float, optional, default is 0.0 seconds (there is no margin in the IDL tplot makegap)
-            The maximum deviation from the step size allowed before degapping occurs.  In other words, 
-            if you'd like to fill in data every 4 seconds but occasionally the data is 4.1 seconds apart, 
+            The maximum deviation from the step size allowed before degapping occurs.  In other words,
+            if you'd like to fill in data every 4 seconds but occasionally the data is 4.1 seconds apart,
             set the margin to .1 so that a data point is not inserted there.
         func : str, optional
             Either 'nan' or 'ffill', which overrides normal interpolation with NaN
@@ -31,41 +28,61 @@ def makegap(var_data, dt = None, margin = 0.0, func='nan'):
         None
 
     Examples:
-        >>> # TODO
+        >>> import pytplot
+        >>> time = [pytplot.time_float("2020-01-01") + i for i in [1, 2, 3, 4, 5, 6, 9, 10, 11]]
+        >>> y = [1, 2, 3, 4, 5, 6, 9, 10, 11]
+        >>> pytplot.store_data("a", data={"x": time, "y": y})
+        >>> a = pytplot.get_data("a")
+        >>> print(a)
+        >>> b = makegap(a)
+        >>> print(b)
 
-    '''
-    #vt = var_data.times
-    #gap_size = np.diff(vt) #vt is in nanoseconds, and np.diff works over day boundaries
-    #Default for dt is the median value of gap_size, the time interval differences
-    #if dt == None:
+    """
+    # vt = var_data.times
+    # gap_size = np.diff(vt) #vt is in nanoseconds, and np.diff works over day boundaries
+    # Default for dt is the median value of gap_size, the time interval differences
+    # if dt == None:
     #    dt = np.median(gap_size)
     #    dt0 = dt
-    #else:
-        #change dt to appropriate type
+    # else:
+    # change dt to appropriate type
     #    dt0 = np.timedelta64(int(dt*1e9), 'ns')
 
-    #gap_index_locations = np.where(gap_size > dt0)
+    # gap_index_locations = np.where(gap_size > dt0)
 
-    #First create a temporary tplot variable for the data, times need to be in unix time
-    x = np.int64(var_data.times)/1e9
+    # First create a temporary tplot variable for the data, times need to be in unix time
+    x = pytplot.time_float(var_data.times)
     if var_data.y.ndim == 1:
-        pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y})
-    else: #multiple dimensions
+        pytplot.store_data("makegap_tmp", data={"x": x, "y": var_data.y})
+    else:  # multiple dimensions
         var_data_out = None
-        pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y})
-#Check for values, v, or v1, v2, v3
-        if len(var_data) == 2: #No v's
-            pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y})
-        elif len(var_data) == 3: #v or v1,needs indexing if it's 2d
-            pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y, 'v':var_data[2]})
-        elif len(var_data) == 4: #has both v1 and v2
-            pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y, 'v1':var_data[2], 'v2':var_data[3]})
-        elif len(var_data) == 5: #has v1, v2, v3
-            pytplot.store_data('makegap_tmp', data = {'x':x, 'y':var_data.y, 'v1':var_data[2], 'v2':var_data[3], 'v3':var_data[3]})
-    #Now, degap the variable
-    pytplot.degap('makegap_tmp', dt = dt, margin = margin, func = func, twonanpergap = True)
-    #and return the getdata result
-    var_data_out = pytplot.get_data('makegap_tmp', dt = True)
-    pytplot.del_data('makegap_tmp')
+        pytplot.store_data("makegap_tmp", data={"x": x, "y": var_data.y})
+        # Check for values, v, or v1, v2, v3
+        if len(var_data) == 2:  # No v's
+            pytplot.store_data("makegap_tmp", data={"x": x, "y": var_data.y})
+        elif len(var_data) == 3:  # v or v1,needs indexing if it's 2d
+            pytplot.store_data(
+                "makegap_tmp", data={"x": x, "y": var_data.y, "v": var_data[2]}
+            )
+        elif len(var_data) == 4:  # has both v1 and v2
+            pytplot.store_data(
+                "makegap_tmp",
+                data={"x": x, "y": var_data.y, "v1": var_data[2], "v2": var_data[3]},
+            )
+        elif len(var_data) == 5:  # has v1, v2, v3
+            pytplot.store_data(
+                "makegap_tmp",
+                data={
+                    "x": x,
+                    "y": var_data.y,
+                    "v1": var_data[2],
+                    "v2": var_data[3],
+                    "v3": var_data[3],
+                },
+            )
+    # Now, degap the variable
+    pytplot.degap("makegap_tmp", dt=dt, margin=margin, func=func)
+    # and return the getdata result
+    var_data_out = pytplot.get_data("makegap_tmp", dt=True)
+    pytplot.del_data("makegap_tmp")
     return var_data_out
-#End of makegap.py
