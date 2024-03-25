eef9f0ce7da1372905c465d37f4c42ecd0c4911a
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2024-03-25 16:24:37 -0700

    Documentation end example unittest for all functions in time_double

--- pytplot/time_double.py
+++ pytplot/time_double.py
@@ -1,18 +1,3 @@
-"""
-Transform datetimes from string to decimal.
-
-Examples
---------
-time_float()
-time_float(['2017-06-29 23:59:59.1234', '2017-12-01 00:15:21.1234'])
-time_double()
-
-Notes
------
-Similar to time_double.pro in IDL SPEDAS.
-
-"""
-
 from dateutil import parser
 from datetime import datetime, timezone
 import numpy as np
@@ -26,13 +11,18 @@ def time_float_one(s_time=None):
     Parameters
     ----------
     s_time : str, optional
-        Input string.
-        The default is None, which returns Now.
+        Input string representing a datetime. If not provided, the current time is used.
 
     Returns
     -------
-    float
-        Output time.
+    float_time : float
+        The decimal representation of the input datetime, or the current local datetime if no input is provided.
+
+    Examples
+    --------
+    >>> from pytplot import time_float_one
+    >>> time_float_one('2023-03-25 12:00:00')
+    1679745600.0
 
     """
     if s_time is None:
@@ -57,15 +47,32 @@ def time_float(str_time=None):
 
     Parameters
     ----------
-    str_time : str/list of str, optional
-        Input times. The default is None.
+    str_time : str or list of str, optional
+        The input datetime(s) as a string or a list of strings. If not provided the current time is used.
 
     Returns
     -------
-    list of float
-        Output times as floats.
+    time_list : list of float or float
+        The decimal representation of the input datetime(s) as a list of floats. If the input is a single
+        datetime string, the list will contain only one element. The current local datetime is used if no input is provided.
 
+    Examples
+    --------
+    >>> from pytplot import time_float
+    >>> time_float(['2023-03-25 12:00:00', '2023-03-26 12:00:00'])
+    [1679745600.0, 1679832000.0]
+
+    >>> time_float('2023-03-25 12:00:00')
+    1679745600.0
+
+    >>> time_float()
+    # Returns the current timestamp as a list containing one float.
+
+    Note
+    ----
+    This function is to time_double.pro in IDL SPEDAS.
     """
+
     if str_time is None:
         return time_float_one()
 
@@ -85,17 +92,32 @@ def time_double(str_time=None):
     """
     Transform a list of datetimes from string to decimal.
 
-    Same as time_float.
-
     Parameters
     ----------
-    str_time : str/list of str, optional
-        Input times. The default is None.
+    str_time : str or list of str, optional
+        The input datetime(s) as a string or a list of strings. If not provided the current time is used.
 
     Returns
     -------
-    list of float
-        Output times as floats.
+    time_list : list of float or float
+        The decimal representation of the input datetime(s) as a list of floats. If the input is a single
+        datetime string, the list will contain only one element. The current local datetime is used if no input is provided.
+
+    Examples
+    --------
+    >>> from pytplot import time_double
+    >>> time_double(['2023-03-25 12:00:00', '2023-03-26 12:00:00'])
+    [1679745600.0, 1679832000.0]
+
+    >>> time_double('2023-03-25 12:00:00')
+    1679745600.0
+
+    >>> time_double()
+    # Returns the current timestamp as a list containing one float.
+
+    Note
+    ----
+    This function is an alilas of time_float. This function is to time_double.pro in IDL SPEDAS.
 
     """
     return time_float(str_time)
