b763bab964957bbef93a696df54589e184676211
Nick H <34072991+nickssl@users.noreply.github.com>
2024-03-25 12:27:14 -0700

    Fixed the example.
    Also fixed a bug ("hamming" parameter)

--- pytplot/tplot_math/pwr_spec.py
+++ pytplot/tplot_math/pwr_spec.py
@@ -12,7 +12,8 @@ from scipy import signal
 # Interestingly enough, the output is the same if units of seconds are used in the periodogram instead of Hertz.
 # Perhaps they calculated it differently?
 
-def pwr_spec(tvar, nbp=256, nsp=128, name=None):
+
+def pwr_spec(tvar, nbp=256, nsp=128, newname=None):
     """
     Calculates the power spectrum of a line, and adds a tplot variable for this new spectrogram
 
@@ -23,39 +24,46 @@ def pwr_spec(tvar, nbp=256, nsp=128, name=None):
             The number of points to use when calculating the FFT
         nsp : int, optional
             The number of points to shift over to calculate the next FFT
-        name : str, optional
+        newname : str, optional
             The name of the new tplot variable created,
 
     Returns:
         None
 
     Examples:
-        >>> pytplot.cdf_to_tplot("/path/to/pytplot/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
-        >>> pytplot.tplot_math.split_vec('data')
-        >>> pytplot.pwr_spec('data_0')
-        >>> pytplot.tplot('data_0_pwrspec')
+        >>> import pytplot
+        >>> import math
+        >>> time = [pytplot.time_float("2020-01-01") + i for i in range(10000)]
+        >>> quantity = [math.sin(i) for i in range(10000)]
+        >>> pytplot.store_data("dp", data={"x": time, "y": quantity})
+        >>> pytplot.pwr_spec("dp", name="dp_pwrspec")
+        >>> pytplot.tplot("dp_pwrspec")
     """
 
-    x, y = pytplot.get_data(tvar)
+    d = pytplot.get_data(tvar)
+    x, y = d[0], d[1]
 
     if len(y.shape) > 1:
-        logging.warning("Cannot create pwr_spec for variable %s, must be a single line",tvar)
+        logging.warning(
+            "Cannot create pwr_spec for variable %s, must be a single line", tvar
+        )
 
     l = len(x)
     x_new = []
     f_new = []
     pxx_new = []
-    shift_lsp = np.arange(0, l-1, nsp)
+    shift_lsp = np.arange(0, l - 1, nsp)
     for i in shift_lsp:
-
-        x_n = x[i:i+nbp]
-        y_n = y[i:i+nbp]
+        x_n = x[i : i + nbp]
+        y_n = y[i : i + nbp]
         if len(x_n) < nbp:
             continue
 
         median_diff_between_points = np.median(np.diff(x_n))
-        w = signal.get_window("hanning", nbp)
-        f,pxx = signal.periodogram(y_n, fs=(1/median_diff_between_points), window=w, detrend='linear')
+        w = signal.get_window("hamming", nbp)
+        f, pxx = signal.periodogram(
+            y_n, fs=(1 / median_diff_between_points), window=w, detrend="linear"
+        )
         f = f[1:-1]
         pxx = pxx[1:-1]
         x_new.append((x_n[-1] + x_n[0]) / 2)
@@ -65,9 +73,9 @@ def pwr_spec(tvar, nbp=256, nsp=128, name=None):
     if name is None:
         name = tvar + "_pwrspec"
 
-    pytplot.store_data(name, data={'x': x_new, 'y': pxx_new, 'v': f_new})
-    pytplot.options(name, 'spec', 1)
-    pytplot.options(name, 'zlog', 1)
-    pytplot.options(name, 'ylog', 1)
+    pytplot.store_data(name, data={"x": x_new, "y": pxx_new, "v": f_new})
+    pytplot.options(name, "spec", 1)
+    pytplot.options(name, "zlog", 1)
+    pytplot.options(name, "ylog", 1)
 
     return
