301f4198ba54a32e74272b58df72406c2699efe1
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-03 11:12:22 -0700

    Temporarily adding sleep(10) calls after each test to avoid tripping rate limiting on MAVEN data server

--- pyspedas/maven/tests/tests.py
+++ pyspedas/maven/tests/tests.py
@@ -3,6 +3,7 @@ import unittest
 from pytplot import data_exists
 from pyspedas import maven
 from pyspedas.maven.download_files_utilities import get_orbit_files, merge_orbit_files
+import time
 
 
 class OrbitTestCases(unittest.TestCase):
@@ -20,27 +21,34 @@ class LoadTestCases(unittest.TestCase):
     def test_load_kp_data(self):
         data = maven.kp()
         self.assertTrue(data_exists("mvn_kp::spacecraft::geo_x"))
+        time.sleep(10)
+
 
     def test_load_mag_data(self):
         data = maven.mag(datatype="ss1s")
         self.assertTrue(data_exists("OB_B"))
+        time.sleep(10)
 
     def test_load_sta_data(self):
         data = maven.sta()
         self.assertTrue(data_exists("hkp_raw_2a-hkp"))
         self.assertTrue(data_exists("hkp_2a-hkp"))
+        time.sleep(10)
 
     def test_load_swea_data(self):
         data = maven.swea()
         self.assertTrue(data_exists("diff_en_fluxes_svyspec"))
+        time.sleep(10)
 
     def test_load_swia_data(self):
         data = maven.swia()
         self.assertTrue(data_exists("spectra_diff_en_fluxes_onboardsvyspec"))
+        time.sleep(10)
 
     def test_load_sep_data(self):
         data = maven.sep()
         self.assertTrue(data_exists("f_ion_flux_tot_s2-cal-svy-full"))
+        time.sleep(10)
 
     # def test_load_lpw_data(self):
     #    data = maven.lpw()
@@ -49,6 +57,7 @@ class LoadTestCases(unittest.TestCase):
     def test_load_euv_data(self):
         data = maven.euv()
         self.assertTrue(data_exists("mvn_euv_calib_bands_bands"))
+        time.sleep(10)
 
     # def test_load_rse_data(self):
     #     data = maven.rse()
