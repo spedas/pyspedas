1b689a70a955ccada1fcf4c960ccfe006398e963
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-05 09:30:30 -0700

    Added sleep_time variable to MAVEN test suite, to control time between tests to avoid tripping data server rate limits.

--- pyspedas/maven/tests/tests.py
+++ pyspedas/maven/tests/tests.py
@@ -5,6 +5,7 @@ from pyspedas import maven
 from pyspedas.maven.download_files_utilities import get_orbit_files, merge_orbit_files
 import time
 
+sleep_time=5
 
 class OrbitTestCases(unittest.TestCase):
     def test_get_merge_orbit_files(self):
@@ -21,34 +22,34 @@ class LoadTestCases(unittest.TestCase):
     def test_load_kp_data(self):
         data = maven.kp()
         self.assertTrue(data_exists("mvn_kp::spacecraft::geo_x"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
 
     def test_load_mag_data(self):
         data = maven.mag(datatype="ss1s")
         self.assertTrue(data_exists("OB_B"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     def test_load_sta_data(self):
         data = maven.sta()
         self.assertTrue(data_exists("hkp_raw_2a-hkp"))
         self.assertTrue(data_exists("hkp_2a-hkp"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     def test_load_swea_data(self):
         data = maven.swea()
         self.assertTrue(data_exists("diff_en_fluxes_svyspec"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     def test_load_swia_data(self):
         data = maven.swia()
         self.assertTrue(data_exists("spectra_diff_en_fluxes_onboardsvyspec"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     def test_load_sep_data(self):
         data = maven.sep()
         self.assertTrue(data_exists("f_ion_flux_tot_s2-cal-svy-full"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     # def test_load_lpw_data(self):
     #    data = maven.lpw()
@@ -57,7 +58,7 @@ class LoadTestCases(unittest.TestCase):
     def test_load_euv_data(self):
         data = maven.euv()
         self.assertTrue(data_exists("mvn_euv_calib_bands_bands"))
-        time.sleep(10)
+        time.sleep(sleep_time)
 
     # def test_load_rse_data(self):
     #     data = maven.rse()
