532c0b2753a94281ffe465e767916fd7161c9a05
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-10 18:51:34 -0700

    Added exception handling for use with unreleased bleeding edge code

--- pyspedas/version.py
+++ pyspedas/version.py
@@ -1,6 +1,8 @@
 """Print the version number for the current installation."""
 import logging
-
+# We don't want to shadow the importlib version() with pyspedas(version)
+from importlib.metadata import version as imp_version
+from importlib.metadata import PackageNotFoundError
 
 def version():
     """
@@ -11,6 +13,8 @@ def version():
     None.
 
     """
-    from importlib.metadata import version
-    ver = version("pyspedas")
+    try:
+        ver = imp_version("pyspedas")
+    except PackageNotFoundError:
+        ver = 'bleeding edge'
     logging.info("pyspedas version: " + ver)
