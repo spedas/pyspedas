bf52ec5ce3224110c01d8ffaa9cc64bb5ee42a45
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-14 11:39:51 -0700

    Add test of degap routine with various time representations

--- pyspedas/utilities/tests/time_tests.py
+++ pyspedas/utilities/tests/time_tests.py
@@ -1,8 +1,8 @@
 import unittest
 from datetime import datetime, timezone
-
+import numpy as np
 from pytplot import time_string, time_datetime
-from pytplot import time_float, time_double
+from pytplot import time_float, time_double, degap, store_data, get_data, options, data_exists
 
 
 class TimeTestCases(unittest.TestCase):
@@ -39,6 +39,98 @@ class TimeTestCases(unittest.TestCase):
         self.assertTrue(time_double('2015') == 1420070400.0000000)
         self.assertTrue(time_double('2015-12-15 12:07:23.767000') == 1450181243.767)
         self.assertTrue(time_double(['2015-12-15 12:07:23.767000', '2015-12-15 12:07:43.767000']) == [1450181243.767, 1450181263.767])
+
+    def test_degap(self):
+        float_times=np.array([1.0,2.0,3.0,11.0,12.0,13.0,21.0,22.0,23.0])
+        int_times = np.array(float_times,dtype=np.int64)
+        str_times = time_string(float_times)
+        dtimes=np.array(float_times*1e9,dtype='datetime64[ns]')
+        data = [1.0,2.0,3.0,11.0,12.0,13.0,21.0,22.0,23.0]
+        sp = np.array([1,2,3])
+        spdata = [sp,sp,sp,sp,sp,sp,sp,sp,sp]
+        spv = [100,200,300]
+        # Scalar data with floating point times
+        store_data('degap_test_float',data={'x':float_times,'y':data})
+        # Spectral data with floating point times
+        store_data('degap_test_float_spec',data={'x':float_times,'y':spdata,'v':spv})
+        options('degap_test_float_spec','spec',1)
+        # Scalar data with integer times
+        store_data('degap_test_int',data={'x':int_times,'y':data})
+        # Spectral data with integer times
+        store_data('degap_test_int_spec',data={'x':int_times,'y':spdata,'v':spv})
+        options('degap_test_int_spec','spec',1)
+        # Scalar data with datetime64[ns] times
+        store_data('degap_test_dt64',data={'x':dtimes,'y':data})
+        # Spectral data with datetime64[ns] times
+        store_data('degap_test_dt64_spec',data={'x':dtimes,'y':spdata,'v':spv})
+        options('degap_test_dt64_spec','spec',1)
+        # Scalar data with string times
+        store_data('degap_test_string',data={'x':str_times,'y':data})
+        # Spectral data with string times
+        store_data('degap_test_string_spec',data={'x':str_times,'y':spdata,'v':spv})
+        options('degap_test_string_spec','spec',1)
+
+        degap('degap_test_float',dt=2.0)
+        dg_data=get_data('degap_test_float')
+        # Should insert a data point with timestamp 5.0, value NaN
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3]))
+
+        degap('degap_test_int',dt=2.0)
+        dg_data=get_data('degap_test_int')
+        # Should insert a data point with timestamp 5.0, value NaN
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3]))
+
+        degap('degap_test_float_spec',dt=2.0)
+        dg_data=get_data('degap_test_float_spec')
+        # Should insert a data point with timestamp 5.0, value [NaN, NaN, NaN]
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3][1]))
+
+        degap('degap_test_int_spec',dt=2.0)
+        dg_data=get_data('degap_test_int_spec')
+        # Should insert a data point with timestamp 5.0, value [NaN, NaN, NaN]
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3][1]))
+
+        degap('degap_test_dt64',dt=2.0)
+        dg_data=get_data('degap_test_dt64')
+        # Should insert a data point with timestamp 5.0, value NaN
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3]))
+
+        degap('degap_test_dt64_spec',dt=2.0)
+        dg_data=get_data('degap_test_dt64_spec')
+        # Should insert a data point with timestamp 5.0, value [NaN, NaN, NaN]
+        self.assertEqual(dg_data.times[3], 5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3][1]))
+
+        degap('degap_test_string',dt=2.0)
+        dg_data=get_data('degap_test_string')
+        # Should insert a data point with timestamp 5.0, value NaN
+        self.assertEqual(dg_data.times[3],5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3]))
+
+        degap('degap_test_string_spec',dt=2.0)
+        dg_data=get_data('degap_test_string_spec')
+        # Should insert a data point with timestamp 5.0, value [NaN, NaN, NaN]
+        self.assertEqual(dg_data.times[3], 5.0)
+        self.assertFalse(np.isfinite(dg_data.y[3][1]))
+
+    def test_missing_pseudovar(self):
+        times=np.array([1.0,2.0,3.0,11.0,12.0,13.0,21.0,22.0,23.0])
+        data = [1.0,2.0,3.0,11.0,12.0,13.0,21.0,22.0,23.0]
+
+        store_data('basevar1', data={'x':times,'y':data})
+        store_data('basevar2', data={'x':times,'y':data})
+        store_data('pseudovar_missing', data=['basevar1', 'basevar2', 'missing_var'])
+        self.assertTrue(data_exists('pseudovar_missing'))
+        store_data('pseudovar_empty_list',data=[])
+        self.assertFalse(data_exists('pseudovar_empty_list'))
+        store_data('pseudovar_all_bad',data=['missing_var1','missing_var2','missing_var3'])
+        self.assertFalse(data_exists('pseudovar_all_bad'))
+
 
 if __name__ == '__main__':
     unittest.main()
