41b9d32986f04398be5027910bb92e71c6c9edd0
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-16 16:43:07 -0700

    1.5.10 had a typo in tdpwrspc :(

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2018-2024, UC Regents, unless otherwise indicated'
 author = 'The PySPEDAS Community'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.10'
+release = '1.5.11'
 
 
 # -- General configuration ---------------------------------------------------
