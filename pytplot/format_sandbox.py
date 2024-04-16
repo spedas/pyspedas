3b6e0c462e0cf7c98b3cf82ab649c7ce25ebe5ff
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-16 01:16:00 -0700

    Play with formatting options without wrecking the history of a real tool

--- pytplot/format_sandbox.py
+++ pytplot/format_sandbox.py
@@ -36,6 +36,7 @@ def format_sandbox(option, value):
         y_axis_zoom       bool         Set True if the mouse wheel should zoom in on the y axis as well as the x on plots.
         ================  ==========   =====
 
+    Notes:
         ==============================   ==========   =====
         Obsolete/Unimplemented Options   Value type   Notes
         ==============================   ==========   =====
