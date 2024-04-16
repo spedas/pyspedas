ec8d38a80f027b6bf3d413dd604155679a610db1
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-16 16:16:47 -0700

    New pyspedas release 1.5.10 to pick up latest pytplot patches, changes to imports, bug fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2018-2024, UC Regents, unless otherwise indicated'
 author = 'The PySPEDAS Community'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.9'
+release = '1.5.10'
 
 
 # -- General configuration ---------------------------------------------------
