7f48446e090aae38d1e7cd84f8cb6a73616e0f65
clrussell90404 <clrussell@igpp.ucla.edu>
2024-04-18 09:42:13 -0600

    Added some example code and tested

--- pytplot/tplot_math/subtract_average.py
+++ pytplot/tplot_math/subtract_average.py
@@ -42,9 +42,9 @@ def subtract_average(
         List of new tplot variables created
 
     Examples:
-        >>> pytplot.store_data('a', data={'x':[0,4,8,12,16], 'y':[1,2,3,4,5]})
-        >>> pytplot.store_data('c', data={'x':[0,4,8,12,16,19,21], 'y':[1,4,1,7,1,9,1]})
-        >>> pytplot.subtract_average('a','c','a-c')
+        >>> pytplot.store_data('a', data={'x':[0,4,8,12,16], 'y':[1.,2.,3.,4.,5.]})
+        >>> pytplot.store_data('c', data={'x':[0,4,8,12,16,19,21], 'y':[1.,4.,1.,7.,1.,9.,1.]})
+        >>> pytplot.tplot_math.subtract_average('a')
 
     """
 
--- pytplot/tplot_math/subtract_median.py
+++ pytplot/tplot_math/subtract_median.py
@@ -36,9 +36,11 @@ def subtract_median(
         Returns a list of new pytplot variables created
 
     Examples:
-        >>> pytplot.store_data('a', data={'x':[0,4,8,12,16], 'y':[1,2,3,4,5]})
-        >>> pytplot.store_data('c', data={'x':[0,4,8,12,16,19,21], 'y':[1,4,1,7,1,9,1]})
-        >>> pytplot.subtract_median('a','c','a-c')
+        >>> from pytplot import subtract_median
+
+        >>> pytplot.store_data('a', data={'x':[0,4,8,12,16], 'y':[1.,2.,3.,4.,5.]})
+        >>> pytplot.store_data('c', data={'x':[0,4,8,12,16,19,21], 'y':[1.,4.,1.,7.,1.,9.,1.]})
+        >>> pytplot.tplot_math.subtract_median('a')
 
     """
     # new_names is deprecated in favor of newname
