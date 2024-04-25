08bc846e7be5893038b6c9fc13f327d2b423d732
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-24 22:55:07 -0700

    Version bump to 1.5.12 to pick up cdaweb bug fix and documentation updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from codecs import open
 
 setup(
     name="pyspedas",
-    version="1.5.11",
+    version="1.5.12",
     description="Python Space Physics Environment Data Analysis Software (pySPEDAS)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
