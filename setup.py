cea059a4b4397e83b8d911a26074fedafc8f59ee
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-04-25 15:59:04 -0700

    Bump pytplot-mpl-temp version to pick up documentation updates

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pywavelets",
         "astropy",
         "hapiclient>=0.2.2",
-        "pytplot-mpl-temp>=2.2.20",
+        "pytplot-mpl-temp>=2.2.21",
         "viresclient",
     ],
     python_requires=">=3.9",
