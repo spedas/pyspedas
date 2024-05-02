cbca20cd168f4886d37659852653d27135cdbe29
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2024-05-02 15:31:34 -0700

    Import test is added.

--- pyspedas/mth5/config.py
+++ pyspedas/mth5/config.py
@@ -1,7 +1,7 @@
 import os
 
 CONFIG = {'local_data_dir': 'mth5/',
-          'remote_data_dir': ''}  # TODO: include later
+          'remote_data_dir': ''}  # Reserved for the future use
 
 # override local data directory with environment variables
 if os.environ.get('SPEDAS_DATA_DIR'):
