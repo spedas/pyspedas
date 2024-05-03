c5333b1ee17b3ac8e3082943a1ec711a0c0c5bca
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-05-02 23:10:10 -0700

    Adjusted some mth5 imports

--- pyspedas/mth5/__init__.py
+++ pyspedas/mth5/__init__.py
@@ -1,5 +1,7 @@
 # Make sure that download folder exists
-import os, pyspedas
+import os
+import logging
+import pyspedas
 from .config import CONFIG
 
 if not os.path.exists(CONFIG['local_data_dir']):
@@ -13,8 +15,8 @@ try:
     import mth5
     del mth5, pyspedas
 except ImportError:
-    pyspedas.logging.error(f'MTH5 must be installed to use module {__name__}.')
-    pyspedas.logging.error('Please install it using: pip install mth5')
+    logging.error(f'MTH5 must be installed to use module {__name__}.')
+    logging.error('Please install it using: pip install mth5')
     raise
 
 # # Synchronize mth5 logging output level with pyspedas logging output level
@@ -37,7 +39,7 @@ try:
         handler_id = next(iter(loguru.logger._core.handlers.keys()))
         loguru.logger.remove(handler_id)
         loguru.logger.configure(**mth5_logger_config)
-        pyspedas.logging.debug('loguru synchronized with pyspedas logging')
+        logging.debug('loguru synchronized with pyspedas logging')
 except:
-    pyspedas.logging.error('loguru synchronization error')
+    logging.error('loguru synchronization error')
     pass
