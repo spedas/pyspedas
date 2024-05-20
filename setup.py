306a8c085bab9006571f746ea7d67ae8281a6a15
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-05-20 12:18:18 -0600

    Another version bump for more pytplot doc updates

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pywavelets",
         "astropy",
         "hapiclient>=0.2.2",
-        "pytplot-mpl-temp>=2.2.23",
+        "pytplot-mpl-temp>=2.2.25",
         "viresclient",
     ],
     python_requires=">=3.9",
