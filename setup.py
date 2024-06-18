257499e86f5c8a3e0f0352d375ad6135b75c8ad5
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-06-18 16:44:41 -0700

    Bump pytplot-mpl-temp version to 2.2.28 to pick up cdf_to_tplot patch for Cluster CSA data

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pywavelets",
         "astropy",
         "hapiclient>=0.2.2",
-        "pytplot-mpl-temp>=2.2.27",
+        "pytplot-mpl-temp>=2.2.28",
         "viresclient",
     ],
     python_requires=">=3.9",
