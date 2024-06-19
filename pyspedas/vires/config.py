eb3ef3e4c6f53fb71e9ab2eba5b8d04b9fbc5ba8
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-06-19 13:20:40 -0700

    Make sure VIRES access token is unittest environment; changed wording on error message

--- pyspedas/vires/config.py
+++ pyspedas/vires/config.py
@@ -31,10 +31,10 @@ else:
         logging.warning('Unable to load VIRES access token from VIRES_TOKEN environment variable or token file %s.',
                         token_file)
         logging.warning(
-            'Visit https://viresclient.readthedocs.io/en/latest/access_token.html to set up an account and generate an access token.')
+            'Visit https://viresclient.readthedocs.io/en/latest/access_token.html to learn how to set up an account and generate an access token.')
     except configparser.NoSectionError:
         logging.warning('Unable to load VIRES access token from VIRES_TOKEN environment variable or token file %s.', token_file)
-        logging.warning('Visit https://viresclient.readthedocs.io/en/latest/access_token.html to set up an account and generate an access token.')
+        logging.warning('Visit https://viresclient.readthedocs.io/en/latest/access_token.html to learn how to set up an account and generate an access token.')
 
 # cc = ClientConfig()
 # cc.set_site_config("https://vires.services/ows", token=CONFIG['access_token'])
