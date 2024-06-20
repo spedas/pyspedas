b2bf782efbed827d1e2e9f2f25102a93e8643263
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-06-19 23:39:40 -0700

    Add trailing / if using SPEDAS_DATA_DIR to build local_data_dir

--- pyspedas/cdagui/config.py
+++ pyspedas/cdagui/config.py
@@ -4,4 +4,4 @@ CONFIG = {'local_data_dir': 'cdaweb/'}
 
 # override local data directory with environment variables
 if os.environ.get('SPEDAS_DATA_DIR'):
-    CONFIG['local_data_dir'] = os.sep.join([os.environ['SPEDAS_DATA_DIR'], 'cdaweb'])
+    CONFIG['local_data_dir'] = os.sep.join([os.environ['SPEDAS_DATA_DIR'], 'cdaweb/'])
