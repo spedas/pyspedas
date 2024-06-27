c5f92b3787ea6678bc360daa97728a36c3378e2c
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-06-26 17:36:02 -0700

    Convert specplot data with 2-D (time varying) bin values to 1-D bin values with the union of all the 2-D bin boundaries.
    Temporarily(?) disabling y resampling, which doesn't seem to be needed anymore.

--- pytplot/MPLPlotter/specplot.py
+++ pytplot/MPLPlotter/specplot.py
@@ -7,6 +7,237 @@ import warnings
 import pytplot
 import logging
 
+def get_bin_boundaries(bins):
+    """
+
+    Parameters
+    ----------
+    bins
+
+    Returns
+    -------
+
+    """
+    nbins = len(bins)
+    # Bin boundaries need to be floating point, even if the original bin values are
+    # integers.
+    outbins = np.zeros(nbins+1,dtype=np.float64)
+    idx_finite = np.where(np.isfinite(bins))
+
+    if type(idx_finite) is tuple:
+        idx_finite = idx_finite[0]
+
+    if len(idx_finite) == 0:
+        logging.warning("get_bin_boundaries: No finite Y bin values found!")
+        outbins[:] = np.nan
+        return outbins
+
+
+    # We need to make sure that no NaNs are sandwiched between finite values
+    good_bin_count = len(idx_finite)
+    leading_nan_count = idx_finite[0]
+    trailing_nan_count = nbins - idx_finite[-1] - 1
+
+    if good_bin_count + leading_nan_count + trailing_nan_count != nbins:
+        logging.warning("get_bin_boundaries: may contain nans between finite values. leading nans: %d, trailing_nans: %d, finite vals: %d",
+                        leading_nan_count, trailing_nan_count, good_bin_count)
+
+    finite_bins = np.copy(bins[idx_finite])
+    edge_count = good_bin_count+1
+    goodbins = np.zeros(edge_count, dtype=np.float64)
+    goodbins[0] = finite_bins[0] - (finite_bins[1] - finite_bins[0]) / 2.0
+    goodbins[1:edge_count-1] = (finite_bins[:-1] + finite_bins[1:]) / 2.0
+    goodbins[edge_count-1] = finite_bins[-1] + (finite_bins[-1] - finite_bins[-2]) / 2.0
+    if leading_nan_count > 0:
+        outbins[0:leading_nan_count] = np.nan
+    outbins[leading_nan_count:leading_nan_count+edge_count] = goodbins[:]
+    if trailing_nan_count > 0:
+        outbins[leading_nan_count+edge_count:nbins+2] = np.nan
+
+    return outbins
+
+
+
+def specplot_make_1d_ybins(values, vdata, ylog, min_ratio=0.001):
+    """Specplot energy bin resampling, without the overhead of arbitrary resampling
+
+    We find the union of all the bin boundaries for time-varying bins, and use that
+    instead of an arbitrary high-resolution grid as the resampling target y-values.
+
+    Allows for monotonically increasing or decreasing bin values, and bins that change over time.
+    Performance optimized by ChatGPT.
+    """
+    ntimes = values.shape[0]
+
+    # Dealing with nans (e.g. THEMIS ESA):
+    # We should probably figure out if the nans are at the high or low end of the energies,
+    # then squish those bins down to a little sliver at the top or bottom of that section of the plot.
+    # If the nans are in the middle with good bins on either side....hahaha good luck!
+    # Prepare bin edges
+    if len(vdata.shape) == 1:
+        vdata_bins = get_bin_boundaries(vdata)
+        bin_boundaries_set = vdata_bins
+        nv = len(vdata)
+    else:  # 2-d V
+        nv = vdata.shape[1]
+        vdata_bins = np.zeros((ntimes, nv + 1), dtype=np.float64)
+        # vdata_bins[:, 0] = vdata[:, 0] - (vdata[:, 1] - vdata[:, 0]) / 2.0
+        # vdata_bins[:, 1:nv] = (vdata[:, :-1] + vdata[:, 1:nv]) / 2.0
+        #vdata_bins[:, nv] = (
+        #        vdata[:, nv - 1] + (vdata[:, nv - 1] - vdata[:, nv - 2]) / 2.0
+        #)
+        #print("Making 2d bin edge set")
+
+        # The sentinal value is there because arrays with nans will not compare equal
+        sentinel = 1e31
+        vdata_bins[0,:] = get_bin_boundaries(vdata[0,:])
+        pbins = vdata_bins[0,:]
+        bin_boundaries_set = set(pbins)
+        p = np.copy(vdata[0,:])
+        p[np.where(~np.isfinite(p))] = sentinel
+        for i in range(ntimes-1):
+            k = i+1
+            t=np.copy(vdata[k,:])
+            t[np.where(~np.isfinite(t))] = sentinel
+            diff=t-p
+            if np.any(diff):
+                # bin values have changed, recalculate boundaries and add to running set
+                #print(k)
+                u = get_bin_boundaries(vdata[k,:])
+                pbins = u
+                vdata_bins[k,:] = u
+                uset = set(u)
+                bin_boundaries_set = bin_boundaries_set | uset
+                p = t
+            else:
+                vdata_bins[k,:] = pbins
+
+
+
+    vdata_unsorted = np.array(list(bin_boundaries_set))
+
+    # Clean nans and sentinal values (e.g. may be present in FAST y bin values)
+    vdata_finite = [val for val in vdata_unsorted if np.isfinite(val)]
+
+    # Sort in ascending order
+    vdata_hi = np.sort(vdata_finite)
+
+    ny = len(vdata_hi)
+
+    # It is possible (e.g. ELFIN) that some bin boundaries are very close, but not equal
+    # (less than a pixel high).  We might want to thin out any bin boundaries within
+    # "epsilon" of the previous bin.
+
+    ymax=vdata_hi[ny-1]
+    ymin=vdata_hi[0]
+    yrange = ymax-ymin
+
+    # With the default min_ratio, epsilon is about one pixel in the y direction for a typical plot size and dpi
+    # If min_ratio is 0, the effect is that no bin boundaries will be discarded.
+
+    if ylog == "log":
+        epsilon = (np.log10(ymax)-np.log10(ymin)) * min_ratio
+    else:
+        epsilon = (ymax-ymin)*min_ratio
+    diff=vdata_hi[1:]-vdata_hi[:ny-1]
+
+    # logging.info("Specplot 1-D y bins before thinning: array size %d, yrange %f, smallest difference %f, epsilon %f,  ratio %f",len(vdata_hi),yrange,np.min(diff),epsilon, (ymax-ymin)/np.min(diff))
+
+    last_val = vdata_hi[0]
+    thinned_list = [vdata_hi[0]]
+    for i in range(ny):
+        val = vdata_hi[i]
+        if ylog == "log":
+            diff = np.log10(val) - np.log10(last_val)
+        else:
+            diff = val-last_val
+        if abs(val-last_val) > epsilon:
+            thinned_list.append(val)
+            last_val = val
+
+    vdata_hi = np.array(thinned_list)
+
+    ny = len(vdata_hi)
+    # There could be NaNs (e.g. FAST)
+    ymax=vdata_hi[ny-1]
+    ymin=vdata_hi[0]
+    yrange = ymax-ymin
+    diff=vdata_hi[1:]-vdata_hi[:ny-1]
+
+    # logging.info("Specplot 1-D y bins after thinning: array size %d, yrange %f, smallest difference %f, epsilon %f,  ratio %f",len(vdata_hi),yrange,np.min(diff),epsilon, (ymax-ymin)/np.min(diff))
+
+    out_values = np.zeros((ntimes, ny), dtype=values.dtype)
+
+    # Note that vdata_hi is always monotonically increasing, but
+    # vdata_bins can be monotonically decreasing
+
+    for j in range(ntimes):
+        if len(vdata.shape) == 1:
+            vtmp = vdata_bins
+        else:
+            vtmp = vdata_bins[j, :]
+
+        #if j==66:
+        #    print("j==66")
+
+        bin_start_idx = 0  # Initialize bin start index
+        bin_end_idx = ny - 1
+        for i in range(nv):
+            # Find the start and end of the current bin in vdata_hi
+            # The assumption that vdata_hi is monotonically increasing
+            # or decreasing allows us to break early from the inner
+            # loop (k) as soon as we find that an element falls
+            # outside the current bin, knowing that no further
+            # elements can belong to this bin.
+
+            # print("vdata_hi:")
+            # print(vdata_hi)
+            # print("vtmp:")
+            # print(vtmp)
+
+            # TODO: vdata bins may contain nans. What if we're unlucky and the first two bin valuyes are nan?
+            # Also, can it ever happen that consecutive bin values are equal?  Then both branches of
+            # the test will be equal.
+
+            if vtmp[i] < vtmp[i + 1]:  # increasing bin values
+                for k in range(bin_start_idx, ny):
+                    # Check if within bin range, count until
+                    # either the next value is out of range or
+                    # there are no more values
+                    if vdata_hi[k] >= vtmp[i] and (
+                            k == ny - 1 or vdata_hi[k + 1] >= vtmp[i + 1]
+                    ):
+                        bin_end_idx = (
+                            k + 1 if k != ny - 1 else ny
+                        )  # Handle last element edge case
+
+                        #print("bin_start_idx, bin_end_idx, vdata_hi[start],vdata_hi[end], i, vtmp[i],vtmp[i+1]" )
+                        #print(bin_start_idx, bin_end_idx, vdata_hi[bin_start_idx],vdata_hi[bin_end_idx], i,vtmp[i], vtmp[i+1])
+                        #logging.info("k: %d i: %d vdata_hi[k]: %f vtmp[i]: %f vtmp[i+1]:%f values[%d,%d] = %f",k,i,vdata_hi[k],vtmp[i], vtmp[i+1],j,i,values[j,i])
+
+                        out_values[j, bin_start_idx:bin_end_idx+1] = values[j, i]
+                        bin_start_idx = bin_end_idx  # Update for next bin
+                        break
+            elif vtmp[i] > vtmp[i + 1]:  # For decreasing vtmp, similar logic in reverse
+                for k in range(bin_end_idx, 0, -1): # highest to lowest index in vdata_hi
+                    if vdata_hi[k] <= vtmp[i] and (
+                            k == 0 or vdata_hi[k - 1] <= vtmp[i + 1]
+                    ):
+                        bin_start_idx = k - 1 if k != 0 else 0
+                        #print("bin_start_idx, bin_end_idx, vdata_hi[start],vdata_hi[end], i, vtmp[i],vtmp[i+1]" )
+                        #print(bin_start_idx, bin_end_idx, vdata_hi[bin_start_idx],vdata_hi[bin_end_idx], i,vtmp[i], vtmp[i+1])
+                        #logging.info("k: %d i: %d vdata_hi[k]: %f vtmp[i]: %f vtmp[i+1]:%f values[%d,%d] = %f",k,i,vdata_hi[k],vtmp[i], vtmp[i+1],j,i,values[j,i])
+                        out_values[j, bin_start_idx+1:bin_end_idx+1] = values[j, i]
+                        bin_end_idx = bin_start_idx
+                        break
+        # print(out_values[j,:])
+        # It looks like the above loop has some sort of fencepost error, where not all elements of out_values are set.
+        # For THEMIS keograms, it's the highest index of out_values[j,:].  Maybe the lowest index is wrong if the
+        # bin values are decreasing?  Set both just to be safe.
+        # out_values[j, 0] = values[j, 0]
+        # out_values[j, ny - 1] = values[j, nv - 1]
+
+    return out_values, vdata_hi
 
 
 def specplot_resample(values, vdata, vdata_hi):
@@ -125,7 +356,7 @@ def specplot(
     fig: matplotlib.figure.Figure
         A matplotlib figure object to be used for this plot
     variable: str
-        The name of the tplot variable to be plotted (used for log messages)
+        The name of the tplxxot variable to be plotted (used for log messages)
     time_idxs: array of int
         The indices of the subset of times to use for this plot. Defaults to None (plot all timestamps).
     style
@@ -140,6 +371,7 @@ def specplot(
     ztitle = zaxis_options["axis_label"]
     zlog = zaxis_options["z_axis_type"]
     ylog = yaxis_options["y_axis_type"]
+    #logging.info("ylog is " + str(ylog))
 
     yrange = yaxis_options["y_range"]
     if not np.isfinite(yrange[0]):
@@ -269,6 +501,11 @@ def specplot(
         # time varying 'v', need to limit the values to those within the requested time range
         out_vdata = out_vdata[time_idxs, :]
 
+    # This call flattens any time-varying bin boundaries into a 1-d list (out_vdata)
+    # and regrids it to the new y bin count (out_values)
+
+    out_values, out_vdata = specplot_make_1d_ybins(out_values, out_vdata, ylog)
+
     # automatic interpolation options
     if yaxis_options.get("x_interp") is not None:
         x_interp = yaxis_options["x_interp"]
@@ -372,15 +609,15 @@ def specplot(
                 out_vdata = 10**out_vdata
 
     # Resample to a higher resolution y grid, similar to interp, but only if y_no_resample is not set
-    if (
-        yaxis_options.get("y_no_resample") is None
-        or yaxis_options.get("y_no_resample") == 0
+    if ( False and
+         ((yaxis_options.get("y_no_resample") is None
+        or yaxis_options.get("y_no_resample") == 0))
     ):
         if ylog == "log":
             # Account for negative or fill values that are not NaN
             vgt0 = np.where(out_vdata > 0)[0]
             if vgt0.size == 0:
-                print("ERROR in specplot.py: no nonzero V values")
+                logging.warning("ERROR in specplot.py: ylog scaling, but no nonnegative V values")
             vmin = np.min(out_vdata[vgt0])
 
             vlt0 = np.where(out_vdata <= 0)[0]
@@ -403,6 +640,9 @@ def specplot(
             + ycrange[0]
         )
         out_values1 = specplot_resample(out_values, vdata, vdata1)
+        #out_values2, vdata2 = specplot_make_1d_ybins(out_values, vdata)
+        #ny = len(vdata2)
+
         out_values = out_values1
         if ylog == "log":
             out_vdata = 10**vdata1
