6ef83623265808404999242d144f603fd42c2372
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-07-08 19:54:29 -0700

    Version bump to 2.2.31, to pickup latest pytplot fixes for specplot errors

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pywavelets",
         "astropy",
         "hapiclient>=0.2.2",
-        "pytplot-mpl-temp>=2.2.30",
+        "pytplot-mpl-temp>=2.2.31",
         "viresclient",
     ],
     python_requires=">=3.9",
