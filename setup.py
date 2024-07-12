f64f474b22db18cae033ad46ec7784ab496b27c8
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-07-12 12:52:38 -0700

    Version bump to pytplot-mpl-temp 2.2.33 for latest specplot changes

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pywavelets",
         "astropy",
         "hapiclient>=0.2.2",
-        "pytplot-mpl-temp>=2.2.32",
+        "pytplot-mpl-temp>=2.2.33",
         "viresclient",
     ],
     python_requires=">=3.9",
