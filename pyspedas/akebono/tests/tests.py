767380fe30a46fa8d2d2fa28036c10740953e209
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-07-24 17:35:05 -0700

    Added tests of prefix and suffix arguments for each data type: prefix=None, suffix=None, and non-empty prefix+suffix.

--- pyspedas/akebono/tests/tests.py
+++ pyspedas/akebono/tests/tests.py
@@ -10,17 +10,68 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('akb_pws_RX1'))
         self.assertTrue(data_exists('akb_pws_RX2'))
 
+    def test_load_pws_data_prefix_none(self):
+        out_vars = pyspedas.akebono.pws(prefix=None)
+        self.assertTrue(data_exists('akb_pws_RX1'))
+        self.assertTrue(data_exists('akb_pws_RX2'))
+
+    def test_load_pws_data_suffix_none(self):
+        out_vars = pyspedas.akebono.pws(suffix=None)
+        self.assertTrue(data_exists('akb_pws_RX1'))
+        self.assertTrue(data_exists('akb_pws_RX2'))
+
+    def test_load_pws_data_prefix_suffix(self):
+        out_vars = pyspedas.akebono.pws(prefix='pre_', suffix='_suf')
+        self.assertTrue(data_exists('pre_akb_pws_RX1_suf'))
+        self.assertTrue(data_exists('pre_akb_pws_RX2_suf'))
+
     def test_load_rdm_data(self):
         out_vars = pyspedas.akebono.rdm()
         self.assertTrue(data_exists('akb_L'))
         self.assertTrue(data_exists('akb_MLT'))
         self.assertTrue(data_exists('akb_rdm_FEIO'))
 
+    def test_load_rdm_data_prefix_none(self):
+        out_vars = pyspedas.akebono.rdm(prefix=None)
+        self.assertTrue(data_exists('akb_L'))
+        self.assertTrue(data_exists('akb_MLT'))
+        self.assertTrue(data_exists('akb_rdm_FEIO'))
+
+    def test_load_rdm_data_suffix_none(self):
+        out_vars = pyspedas.akebono.rdm(suffix=None)
+        self.assertTrue(data_exists('akb_L'))
+        self.assertTrue(data_exists('akb_MLT'))
+        self.assertTrue(data_exists('akb_rdm_FEIO'))
+
+    def test_load_rdm_data_prefix_suffix(self):
+        out_vars = pyspedas.akebono.rdm(prefix='pre_', suffix='_suf')
+        self.assertTrue(data_exists('pre_akb_L_suf'))
+        self.assertTrue(data_exists('pre_akb_MLT_suf'))
+        self.assertTrue(data_exists('pre_akb_rdm_FEIO_suf'))
+
     def test_load_orb_data(self):
         out_vars = pyspedas.akebono.orb()
         self.assertTrue(data_exists('akb_orb_geo'))
         self.assertTrue(data_exists('akb_orb_gdlat'))
         self.assertTrue(data_exists('akb_orb_gdlon'))
+
+    def test_load_orb_prefix_none(self):
+        out_vars = pyspedas.akebono.orb(prefix=None)
+        self.assertTrue(data_exists('akb_orb_geo'))
+        self.assertTrue(data_exists('akb_orb_gdlat'))
+        self.assertTrue(data_exists('akb_orb_gdlon'))
+
+    def test_load_orb_suffix_none(self):
+        out_vars = pyspedas.akebono.orb(suffix=None)
+        self.assertTrue(data_exists('akb_orb_geo'))
+        self.assertTrue(data_exists('akb_orb_gdlat'))
+        self.assertTrue(data_exists('akb_orb_gdlon'))
+
+    def test_load_orb_prefix_suffix(self):
+        out_vars = pyspedas.akebono.orb(prefix='pre_', suffix='_suf')
+        self.assertTrue(data_exists('pre_akb_orb_geo_suf'))
+        self.assertTrue(data_exists('pre_akb_orb_gdlat_suf'))
+        self.assertTrue(data_exists('pre_akb_orb_gdlon_suf'))
 
     def test_load_notplot(self):
         out_vars = pyspedas.akebono.pws(notplot=True)
