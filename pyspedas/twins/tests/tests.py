6f510786c51184bfe73243739d69a3787bec1398
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-07-26 11:19:32 -0700

    Added tests for prefix and suffix parameters

--- pyspedas/twins/tests/tests.py
+++ pyspedas/twins/tests/tests.py
@@ -20,6 +20,21 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('lad1_data'))
         self.assertTrue(data_exists('lad2_data'))
 
+    def test_load_lad_data_prefix_none(self):
+        lad_vars = pyspedas.twins.lad(prefix=None)
+        self.assertTrue(data_exists('lad1_data'))
+        self.assertTrue(data_exists('lad2_data'))
+
+    def test_load_lad_data_suffix_none(self):
+        lad_vars = pyspedas.twins.lad(suffix=None)
+        self.assertTrue(data_exists('lad1_data'))
+        self.assertTrue(data_exists('lad2_data'))
+
+    def test_load_lad_data_prefix_suffix(self):
+        lad_vars = pyspedas.twins.lad(prefix='pre_', suffix='_suf')
+        self.assertTrue(data_exists('pre_lad1_data_suf'))
+        self.assertTrue(data_exists('pre_lad2_data_suf'))
+
     def test_load_ephem_data(self):
         ephemeris_vars = pyspedas.twins.ephemeris()
         self.assertTrue(data_exists('FLTGEO'))
