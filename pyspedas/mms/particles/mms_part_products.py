dcc1adde1822e153eabe03f1295cdecd4da3146f
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-08-15 11:39:54 -0700

    Added prefix and suffix parameters
    If pitch angle or limits are specified, and 'energy' or 'moments' are requested, use field aligned coordinates, apply the limits, and add a '_fac_' infix to the output variable names.

--- pyspedas/mms/particles/mms_part_products.py
+++ pyspedas/mms/particles/mms_part_products.py
@@ -50,7 +50,9 @@ def mms_part_products(in_tvarname,
                       disable_photoelectron_corrections=False,
                       no_regrid=False,
                       regrid=[32, 16],
-                      vel_name=None):
+                      vel_name=None,
+                      prefix='',
+                      suffix=''):
     """
     Generate spectra and moments from 3D MMS particle data; note: this routine isn't
     meant to be called directly - see the wrapper mms_part_getspec instead.
@@ -84,13 +86,16 @@ def mms_part_products(in_tvarname,
             Spacecraft probe #
 
         output: str or list of str
-            Output variables; options: 
+            Output variables; options::
+
                 'energy': energy spectrograms
                 'theta': theta spectrograms
                 'phi': phi spectrograms
                 'pa': pitch-angle spectrograms
                 'gyro': gyro-phase spectrograms
                 'moments': plasma moments
+                'fac_energy': energy spectrogram in field-aligned coordinates
+                'fac_moments': moments in field-aligned coordinates
 
         energy: list of float
             Energy range [min, max], in eV
@@ -145,6 +150,12 @@ def mms_part_products(in_tvarname,
 
         vel_name: str
             Name of tplot variable containing bulk velocity data
+
+        prefix: str
+            Prefix for output tplot variables. Default: ''
+
+        suffix: str
+            Suffix for output tplot variables. Default: ''
             
     Returns
     ----------
@@ -164,6 +175,33 @@ def mms_part_products(in_tvarname,
 
     if isinstance(output, str):
         output = output.split(' ')
+
+    # Ensure prefix and suffix are not 'None'.  There are some internally-applied prefixes,
+    # so we'll rename the input to user_prefix.
+
+    if prefix is None:
+        user_prefix = ''
+    else:
+        user_prefix = prefix
+
+    if suffix is None:
+        suffix = ''
+
+    # If pitch angle or gyro angle limits are specified, replace 'energy' and 'moments'
+    # with 'fac_energy' and 'fac_moments'
+
+    output_lc = np.array([item.lower() for item in output])
+    output_sub = []
+    if pitch is not None or gyro is not None:
+        for item in output_lc:
+            if item == 'energy' and 'fac_energy' not in output_lc:
+                output_sub.append('fac_energy')
+            elif item == 'moments' and 'fac_moments' not in output_lc:
+                output_sub.append('fac_moments')
+            else:
+                output_sub.append(item)
+        output_lc = output_sub
+    output = output_lc
 
     if instrument == 'fpi':
         dist_in = mms_get_fpi_dist(in_tvarname, species=species, probe=probe, data_rate=data_rate)
@@ -202,6 +240,8 @@ def mms_part_products(in_tvarname,
         out_pad_y = np.zeros((ntimes, dist_in[0]['n_theta']))
         out_gyro = np.zeros((ntimes, dist_in[0]['n_phi']))
         out_gyro_y = np.zeros((ntimes, dist_in[0]['n_phi']))
+        out_fac_energy = np.zeros((ntimes, dist_in[0]['n_energy']))
+        out_fac_energy_y = np.zeros((ntimes, dist_in[0]['n_energy']))
 
     # moments
     if 'moments' in output:
@@ -214,10 +254,20 @@ def mms_part_products(in_tvarname,
         out_ptens = np.zeros([ntimes, 6])
         #out_ttens = np.zeros([dist_in['n_times'], 3, 3])
 
+    if 'fac_moments' in output:
+        out_fac_density = np.zeros(ntimes)
+        out_fac_avgtemp = np.zeros(ntimes)
+        out_fac_vthermal = np.zeros(ntimes)
+        out_fac_flux = np.zeros([ntimes, 3])
+        out_fac_velocity = np.zeros([ntimes, 3])
+        out_fac_mftens = np.zeros([ntimes, 6])
+        out_fac_ptens = np.zeros([ntimes, 6])
+        #out_fac_ttens = np.zeros([dist_in['n_times'], 3, 3])
+
     out_vars = []
     last_update_time = None
 
-    if 'moments' in output or correct_photoelectrons or internal_photoelectron_corrections:
+    if 'moments' in output or 'fac_moments' in output or correct_photoelectrons or internal_photoelectron_corrections:
         support_data = mms_pgs_clean_support(data_times, mag_name=mag_name, vel_name=vel_name, sc_pot_name=sc_pot_name)
         mag_data = support_data[0]
         scpot_data = support_data[2]
@@ -227,7 +277,7 @@ def mms_part_products(in_tvarname,
         internal_photoelectron_corrections = False
 
     # Remind user that l2 moments should be used preferentially and FAC moments are experimental
-    if 'moments' in output or 'moments_fac' in output:
+    if 'moments' in output or 'fac_moments' in output:
         msg = """Moments generated with mms_part_products may be missing several important
 corrections, including photoelectron removal and spacecraft potential.
 The official moments released by the instrument teams include these and
@@ -317,8 +367,14 @@ are the scientific products that should be used for analysis."""
             clean_data = mms_pgs_split_hpca(clean_data)
 
         # Apply phi, theta, & energy limits
-        if energy is not None or theta is not None or phi is not None:
-            clean_data = spd_pgs_limit_range(clean_data, energy=energy, theta=theta, phi=phi)
+        if not fac_requested:
+            if energy is not None or theta is not None or phi is not None:
+                clean_data = spd_pgs_limit_range(clean_data, energy=energy, theta=theta, phi=phi)
+        else:
+            # Use pitch and gyro for theta and phi
+            if energy is not None or pitch is not None or gyro is not None:
+                clean_data = spd_pgs_limit_range(clean_data, energy=energy, theta=pitch, phi=gyro)
+
 
         # Build energy spectrogram
         if 'energy' in output:
@@ -364,6 +420,23 @@ are the scientific products that should be used for analysis."""
         if 'gyro' in output:
             out_gyro_y[i, :], out_gyro[i, :] = mms_pgs_make_phi_spec(fac_data, resolution=dist_in['n_phi'])
 
+        if 'fac_energy' in output:
+            out_fac_energy_y[i, :], out_fac_energy[i, :] = mms_pgs_make_e_spec(clean_data)
+
+        if 'fac_moments' in output:
+            if scpot_data is not None:
+                scpot_val = scpot_data[i]
+            else:
+                scpot_val = 0.0
+
+            fac_moments = spd_pgs_moments(clean_data, sc_pot=scpot_val)
+            out_fac_density[i] = fac_moments['density']
+            out_fac_avgtemp[i] = fac_moments['avgtemp']
+            out_fac_vthermal[i] = fac_moments['vthermal']
+            out_fac_flux[i, :] = fac_moments['flux']
+            out_fac_velocity[i, :] = fac_moments['velocity']
+            out_fac_mftens[i, :] = fac_moments['mftens']
+            out_fac_ptens[i, :] = fac_moments['ptens']
 
     if 'moments' in output:
         # put all of the moments arrays into a hash table prior to passing to the tplot routine
@@ -374,27 +447,43 @@ are the scientific products that should be used for analysis."""
               'ptens': out_ptens,
               'vthermal': out_vthermal,
               'avgtemp': out_avgtemp}
-        moments_vars = spd_pgs_moments_tplot(moments, x=data_times, prefix=in_tvarname)
+        moments_vars = spd_pgs_moments_tplot(moments, x=data_times, prefix=user_prefix + in_tvarname, suffix=suffix)
         out_vars.extend(moments_vars)
 
+    if 'fac_moments' in output:
+        # put all of the moments arrays into a hash table prior to passing to the tplot routine
+        fac_moments = {'density': out_fac_density,
+              'flux': out_fac_flux,
+              'mftens': out_fac_mftens,
+              'velocity': out_fac_velocity,
+              'ptens': out_fac_ptens,
+              'vthermal': out_fac_vthermal,
+              'avgtemp': out_fac_avgtemp}
+        fac_moments_vars = spd_pgs_moments_tplot(fac_moments, x=data_times, prefix=user_prefix + in_tvarname+'_fac', suffix=suffix)
+        out_vars.extend(fac_moments_vars)
+
     if 'energy' in output:
-        spd_pgs_make_tplot(in_tvarname+'_energy', x=data_times, y=out_energy_y, z=out_energy, units=units, ylog=True, ytitle=dist_in['data_name'], ysubtitle='energy (eV)')
-        out_vars.append(in_tvarname+'_energy')
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_energy' + suffix, x=data_times, y=out_energy_y, z=out_energy, units=units, ylog=True, ytitle=dist_in['data_name'], ysubtitle='energy (eV)')
+        out_vars.append(user_prefix + in_tvarname+'_energy' + suffix)
+
+    if 'fac_energy' in output:
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_fac_energy' + suffix, x=data_times, y=out_fac_energy_y, z=out_fac_energy, units=units, ylog=True, ytitle=dist_in['data_name'], ysubtitle='energy (eV)')
+        out_vars.append(user_prefix + in_tvarname+'_fac_energy' + suffix)
 
     if 'theta' in output:
-        spd_pgs_make_tplot(in_tvarname+'_theta', x=data_times, y=out_theta_y, z=out_theta, units=units, ytitle=dist_in['data_name'], ysubtitle='theta (deg)')
-        out_vars.append(in_tvarname+'_theta')
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_theta' + suffix, x=data_times, y=out_theta_y, z=out_theta, units=units, ytitle=dist_in['data_name'], ysubtitle='theta (deg)')
+        out_vars.append(user_prefix + in_tvarname+'_theta' + suffix)
 
     if 'phi' in output:
-        spd_pgs_make_tplot(in_tvarname+'_phi', x=data_times, y=out_phi_y, z=out_phi, units=units, ytitle=dist_in['data_name'], ysubtitle='phi (deg)')
-        out_vars.append(in_tvarname+'_phi')
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_phi' + suffix, x=data_times, y=out_phi_y, z=out_phi, units=units, ytitle=dist_in['data_name'], ysubtitle='phi (deg)')
+        out_vars.append(user_prefix + in_tvarname+'_phi' + suffix)
 
     if 'pa' in output:
-        spd_pgs_make_tplot(in_tvarname+'_pa', x=data_times, y=out_pad_y, z=out_pad, units=units, ytitle=dist_in['data_name'], ysubtitle='PA (deg)')
-        out_vars.append(in_tvarname+'_pa')
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_pa' + suffix, x=data_times, y=out_pad_y, z=out_pad, units=units, ytitle=dist_in['data_name'], ysubtitle='PA (deg)')
+        out_vars.append(user_prefix + in_tvarname+'_pa' + suffix)
 
     if 'gyro' in output:
-        spd_pgs_make_tplot(in_tvarname+'_gyro', x=data_times, y=out_gyro_y, z=out_gyro, units=units, ytitle=dist_in['data_name'], ysubtitle='gyro (deg)')
-        out_vars.append(in_tvarname+'_gyro')
+        spd_pgs_make_tplot(user_prefix + in_tvarname+'_gyro' + suffix, x=data_times, y=out_gyro_y, z=out_gyro, units=units, ytitle=dist_in['data_name'], ysubtitle='gyro (deg)')
+        out_vars.append(user_prefix + in_tvarname+'_gyro' + suffix)
 
     return out_vars
