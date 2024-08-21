103419fd731a6481e607b9b218ea1407162681f7
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-08-21 10:54:02 -0700

    Added del_data calls to avoid interference between tests
    Added tests for prefix and suffix handling

--- pyspedas/rbsp/tests/tests.py
+++ pyspedas/rbsp/tests/tests.py
@@ -23,6 +23,7 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue('y' in data['density'].keys())
 
     def test_load_emfisis_data(self):
+        del_data('*')
         emfisis_vars = pyspedas.rbsp.emfisis(trange=['2018-11-5', '2018-11-6'], datatype='magnetometer', level='l3', time_clip=True)
         self.assertTrue(data_exists('Mag'))
         emfisis_vars = pyspedas.rbsp.emfisis(trange=['2018-11-5', '2018-11-6'], datatype='magnetometer', level='l2')
@@ -44,25 +45,45 @@ class LoadTestCases(unittest.TestCase):
 
     def test_load_hfr_spectra(self):
         from pytplot import tplot
+        del_data('*')
         pyspedas.rbsp.emfisis(trange=['2013-01-17', '2013-01-18'], datatype='hfr', level='l2', wavetype='spectra')
         self.assertTrue(data_exists('HFR_Spectra'))
         tplot(['HFR_Spectra'], display=global_display, save_png='emphisis_hfr.png')
 
     def test_load_efw_data(self):
+        del_data('*')
         efw_vars = pyspedas.rbsp.efw(trange=['2015-11-5', '2015-11-6'], level='l2')
         self.assertTrue(data_exists('spec64_e12ac'))
         efw_vars = pyspedas.rbsp.efw(trange=['2015-11-5', '2015-11-6'], level='l3')
         self.assertTrue(data_exists('density'))
 
     def test_load_rbspice_download(self):
+        del_data('*')
         files = pyspedas.rbsp.rbspice(downloadonly=True, trange=['2018-11-5', '2018-11-6'], datatype='tofxeh', level='l3')
         self.assertTrue(isinstance(files, list))
 
     def test_load_rbspice_esrhelt(self):
+        del_data('*')
         rbspice_vars = pyspedas.rbsp.rbspice(trange=['2013-11-5', '2013-11-6'], datatype='ESRHELT', level='l3')
         self.assertTrue(data_exists('rbspa_rbspice_l3_ESRHELT_FEDU'))
 
+    def test_load_rbspice_esrhelt_prefix_none(self):
+        del_data('*')
+        rbspice_vars = pyspedas.rbsp.rbspice(trange=['2013-11-5', '2013-11-6'], datatype='ESRHELT', level='l3', prefix=None)
+        self.assertTrue(data_exists('rbspa_rbspice_l3_ESRHELT_FEDU'))
+
+    def test_load_rbspice_esrhelt_suffix_none(self):
+        del_data('*')
+        rbspice_vars = pyspedas.rbsp.rbspice(trange=['2013-11-5', '2013-11-6'], datatype='ESRHELT', level='l3', suffix=None)
+        self.assertTrue(data_exists('rbspa_rbspice_l3_ESRHELT_FEDU'))
+
+    def test_load_rbspice_esrhelt_prefix_suffix(self):
+        del_data('*')
+        rbspice_vars = pyspedas.rbsp.rbspice(trange=['2013-11-5', '2013-11-6'], datatype='ESRHELT', level='l3', prefix='pre_', suffix='_suf')
+        self.assertTrue(data_exists('pre_rbspa_rbspice_l3_ESRHELT_FEDU_suf'))
+
     def test_load_rbspice_data(self):
+        del_data('*')
         data = pyspedas.rbsp.rbspice(trange=['2018-11-5', '2018-11-6'], datatype='TOFxEH', level='l3')
         self.assertTrue(data_exists('rbspa_rbspice_l3_TOFxEH_proton_omni_spin'))
         self.assertTrue(data_exists('rbspa_rbspice_l3_TOFxEH_proton_omni'))
@@ -83,10 +104,12 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('rbspa_rbspice_l3_TOFxEH_proton_omni_0-1000.0keV_pad_spin'))
 
     def test_load_mageis_data(self):
+        del_data('*')
         mageis_vars = pyspedas.rbsp.mageis(trange=['2018-11-5', '2018-11-6'], level='l3', rel='rel04')
         self.assertTrue(data_exists('I'))
 
     def test_load_hope_data(self):
+        del_data('*')
         hope_vars = pyspedas.rbsp.hope(trange=['2018-11-5', '2018-11-6'], datatype='moments', level='l3', rel='rel04')
         self.assertTrue(data_exists('Ion_density'))
         hope_vars = pyspedas.rbsp.hope(trange=['2018-11-5', '2018-11-6'], datatype='pitchangle', level='l3')
@@ -95,6 +118,7 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('I_Ele'))
 
     def test_load_rep_data(self):
+        del_data('*')
         rept_vars = pyspedas.rbsp.rept(trange=['2018-11-4', '2018-11-5'], level='l2', rel='rel03')
         self.assertTrue(data_exists('FESA'))
         self.assertTrue(data_exists('FEDU'))
@@ -102,12 +126,14 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('FPDU'))
 
     def test_load_rps1min_data(self):
+        del_data('*')
         rps_vars = pyspedas.rbsp.rps()
         self.assertTrue(data_exists('DOSE2_RATE'))
         rps_vars = pyspedas.rbsp.rps(datatype='rps')
         self.assertTrue(data_exists('FPDU_Energy'))
 
     def test_load_magephem_data(self):
+        del_data('*')
         magephem_vars = pyspedas.rbsp.magephem()
         print(magephem_vars)
         self.assertTrue(data_exists('Rgse'))
