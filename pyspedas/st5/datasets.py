2e67e6b172060b573a6ba7fbe46ac545f8c8d18e
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-08-22 12:13:36 -0700

    Added missing imports for find_datasets

--- pyspedas/st5/datasets.py
+++ pyspedas/st5/datasets.py
@@ -1,4 +1,5 @@
 from .load import load
+from pyspedas.utilities.datasets import find_datasets
 
 # This routine was originally in st5/__init__.py, until being moved to its own file.
 # Please refer to __init__.py if you need to see the revision history before it was moved.
