bec8aaa6fb7ebe4e1273db073f32f4fb62273765
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-09-09 16:45:20 -0700

    Bump pyspedas version to 1.6.1 to pick up pytplot bug fixes, akebono and goes mission updates, additional tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from codecs import open
 
 setup(
     name="pyspedas",
-    version="1.6.0",
+    version="1.6.1",
     description="Python Space Physics Environment Data Analysis Software (pySPEDAS)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
