27e15095b24a172c04fd08b6ec59f95a4fba28ca
jameswilburlewis <jwl@ssl.berkeley.edu>
2024-10-24 11:22:48 -0700

    Added note that pyspedas.avg_data() is almost always a better choice, unless the input is known to be evenly gridded, and the res parameter evenly divides the sample count,.

--- pytplot/tplot_math/avg_res_data.py
+++ pytplot/tplot_math/avg_res_data.py
@@ -22,6 +22,13 @@ def avg_res_data(tvar,res,newname=None,new_tvar=None):
     -------
         None
 
+    Notes
+    -----
+
+    This routine uses the xarray coarsen() method to reduce the time resolution.  It will only work if the
+    data is evenly gridded, and the res parameter evenly divides the number of samples.
+    For most purposes, it is more appropriate to use pyspedas.avg_data() instead.
+
     Examples
     --------
 
