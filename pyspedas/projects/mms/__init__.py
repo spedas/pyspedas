b3763f37871705a24e6cabc1fdcae2d21c9ef2be
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-02-03 09:43:51 -0800

    Add some missing imports to top level MMS namespace

--- pyspedas/projects/mms/__init__.py
+++ pyspedas/projects/mms/__init__.py
@@ -10,6 +10,7 @@ from .mec_ascii.tetrahedron_qf import mms_load_tetrahedron_qf
 from .fgm_tools.fgm import mms_load_fgm
 from .hpca_tools.hpca import mms_load_hpca
 from .fpi_tools.fpi import mms_load_fpi
+from .fpi_tools.mms_fpi_ang_ang import mms_fpi_ang_ang
 from .scm_tools.scm import mms_load_scm
 from .mec_tools.mec import mms_load_mec
 from .feeps_tools.feeps import mms_load_feeps
@@ -27,6 +28,11 @@ from .cotrans.mms_cotrans_qrotate import mms_cotrans_qrotate
 from .cotrans.mms_cotrans_qtransformer import mms_cotrans_qtransformer
 from .cotrans.mms_qcotrans import mms_qcotrans
 from .particles import mms_part_getspec, mms_part_slice2d
+from .mms_orbit_plot import mms_orbit_plot
+from .mms_load_brst_segments import mms_load_brst_segments
+from .mms_load_fast_segments import mms_load_fast_segments
+from .mms_load_sroi_segments import mms_load_sroi_segments
+from .mms_events import mms_brst_events
 
 '''
     the following wrappers allow users to import the load routines using 
