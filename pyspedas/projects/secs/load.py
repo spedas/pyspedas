778af17dd5246071115170e3d2aecb8a0d693845
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-03-03 12:34:11 -0800

    Fixed formatting of example code

--- pyspedas/projects/secs/load.py
+++ pyspedas/projects/secs/load.py
@@ -73,10 +73,10 @@ def load(
 
     Examples
     --------
-    >> import pyspedas
-    >> secs_vars = pyspedas.projects.secs.data(dtype='EICS', trange=['2018-02-01', '2018-02-02'], downloadonly=True)
-    >> print(secs_vars)
-    ['/Users/user/data/secs/EICS/2018/02/EICS20180201.zip']
+    >>> import pyspedas
+    >>> secs_vars = pyspedas.projects.secs.data(dtype='EICS', trange=['2018-02-01', '2018-02-02'], downloadonly=True)
+    >>> print(secs_vars)
+    >>> ['/Users/user/data/secs/EICS/2018/02/EICS20180201.zip']
     """
 
     if dtype is None:
