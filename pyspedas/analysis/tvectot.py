16a936c2aece11d0d6ef6bd2a23aa3dfe34faed7
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-03-07 11:52:37 -0800

    join_component defaults to True to match IDL behavior Manipulate data arrays directly, to avoid clobbering tplot variables with split_vec/join_vec Don't clobber input suffix

--- pyspedas/analysis/tvectot.py
+++ pyspedas/analysis/tvectot.py
@@ -1,34 +1,46 @@
-from numpy import linalg
-from pytplot import split_vec, join_vec, get_data, store_data, options, tplot_wildcard_expand
+import numpy as np
+from copy import deepcopy
+from pytplot import get_data, store_data, options, tplot_wildcard_expand
 from typing import Union, List
 import logging
 
 def _tvectot(tvar: str, new_name: str, join_component: bool):
     data = get_data(tvar)
     md = get_data(tvar,metadata=True)
-    new_data = linalg.norm(data.y, axis=1)
-    store_data(new_name, data={'x':data.times,'y':new_data},attr_dict=md)
-    
+    new_data = np.linalg.norm(data.y, axis=1)
+
     if join_component:
         # Caution: this can clobber existing variable names!  Maybe better to manipulate the
         # data directly instead of calling split_vec/join_vec
-        join_vec(split_vec(tvar)+[new_name], newname=new_name)
+        #join_vec(split_vec(tvar)+[new_name], newname=new_name)
+        joined_data = np.append(data.y, new_data[:,np.newaxis],1)
+        store_data(new_name, data={'x':data.times,'y':joined_data}, attr_dict=deepcopy(md))
         options(new_name, 'legend_names', ['x', 'y', 'z', 'Magnitude'])
     else:
+        store_data(new_name, data={'x': data.times, 'y': new_data}, attr_dict=md)
         options(new_name, 'legend_names', 'Magnitude')
     return new_name
 
-def tvectot(tvars: Union[str, List[str]], newname=None, newnames: Union[str, List[str]] = None, suffix="_mag", join_component=False) -> Union[str , List[str]]:
+def tvectot(tvars: Union[str, List[str]],
+            newname=None,
+            newnames: Union[str, List[str]] = None,
+            suffix=None,
+            join_component=True) -> Union[str , List[str]]:
     """
     Computes the magnitude of a vector time series.
 
     Parameters
     ----------
-    - tvars : Names of the tplot variables.
-    - newnames: (Deprecated) Names for the resultant magnitude tplot variables. If not provided, it appends the suffix to `tvars`.
-    - newname: Names for the resultant magnitude tplot variables. If not provided, it appends the suffix to `tvars`.
-    - suffix: The suffix to append to tensor_names to form new_names if new_names is not provided.
-    - join_component: If True, the magnitude tplot variable is joined with the component tplot variables.
+    tvars : str or list[str]
+        Names of the tplot variables.
+    newnames: str or list[str]
+        (Deprecated) Names for the resultant magnitude tplot variables. If not provided, it appends the suffix to `tvars`.
+    newname: str or list[str]
+        Names for the resultant magnitude tplot variables. If not provided, it appends the suffix to `tvars`.
+    suffix: str
+        The suffix to append to tvars to form newnames if newnames is not provided.
+    join_component: bool
+        If True, the magnitude tplot variable is joined with the component tplot variables.
 
     Returns
     -------
@@ -47,8 +59,12 @@ def tvectot(tvars: Union[str, List[str]], newname=None, newnames: Union[str, Lis
     tvars_type = type(tvars)
     if tvars_type == str:
         tvars = [tvars]
-    if join_component:
-        suffix = "_tot"
+
+    if suffix is None:
+        if join_component:
+            suffix = "_tot"
+        else:
+            suffix='_mag'
 
     if newname is None:
         newname = [tvar + suffix for tvar in tvars]
