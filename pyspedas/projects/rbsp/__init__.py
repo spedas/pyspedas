a27d2fa966474196853b645fa9346af242fdbd03
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-03-09 11:02:47 -0700

    Added RBSP particle routines to top-level RBSP module

--- pyspedas/projects/rbsp/__init__.py
+++ pyspedas/projects/rbsp/__init__.py
@@ -7,5 +7,6 @@ from .magephem import magephem
 from .rbspice import rbspice
 from .rept import rept
 from .rps import rps
-
+from .rbspice_lib.rbsp_rbspice_omni import rbsp_rbspice_omni
+from .rbspice_lib.rbsp_rbspice_pad import rbsp_rbspice_pad
 
