23cefa69ba93134cad766c225cc2eb7226e6a477
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-03-18 17:35:32 -0700

    Bumping pyspedas version to 1.7.13 to pick up plotting improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2018-2025, Regents of the University of California, unless otherwis
 author = 'The PySPEDAS Community'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.12'
+release = '1.7.13'
 
 
 # -- General configuration ---------------------------------------------------
