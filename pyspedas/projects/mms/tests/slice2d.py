cdd4bad92298be3394c044d4e9d268cb3cfe952d
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-03-19 00:16:09 -0700

    Added global_display flag Added test for calling mms_part_slice2d with a numeric trange

--- pyspedas/projects/mms/tests/slice2d.py
+++ pyspedas/projects/mms/tests/slice2d.py
@@ -2,7 +2,7 @@ import numpy as np
 import unittest
 from pyspedas.projects.mms.particles.mms_part_slice2d import mms_part_slice2d
 from pyspedas.particles.spd_units_string import spd_units_string
-
+global_display = False
 
 class SliceTests(unittest.TestCase):
     def test_notime(self):
@@ -10,18 +10,18 @@ class SliceTests(unittest.TestCase):
         self.assertTrue(not nothing)
 
     def test_fpi_nospecies(self):
-        mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', rotation='xy', display=False)
-        mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', instrument='hpca', rotation='xy', display=False)
+        mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', rotation='xy', display=global_display)
+        mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', instrument='hpca', rotation='xy', display=global_display)
 
     def test_fpi_return_slice(self):
-        the_slice = mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', rotation='xy', display=False, return_slice=True)
+        the_slice = mms_part_slice2d(time='2015-10-16/13:06:30', probe='1', rotation='xy', display=global_display, return_slice=True)
         self.assertTrue(isinstance(the_slice, dict))
 
     def test_fpi_brst_slice_x(self):
-        mms_part_slice2d(time='2015-10-16/13:06:30', slice_x=np.array([1, 0, 0]), probe='1', species='i', data_rate='brst', rotation='bv', save_png='test_fpi_brst_i_bv_slice_x', display=False)
+        mms_part_slice2d(time='2015-10-16/13:06:30', slice_x=np.array([1, 0, 0]), probe='1', species='i', data_rate='brst', rotation='bv', save_png='test_fpi_brst_i_bv_slice_x', display=global_display)
 
     def test_fpi_brst_slice_norm(self):
-        mms_part_slice2d(time='2015-10-16/13:06:30', slice_norm=np.array([0, 0, 1]), probe='1', species='i', data_rate='brst', rotation='bv', save_png='test_fpi_brst_i_bv_slice_norm', display=False)
+        mms_part_slice2d(time='2015-10-16/13:06:30', slice_norm=np.array([0, 0, 1]), probe='1', species='i', data_rate='brst', rotation='bv', save_png='test_fpi_brst_i_bv_slice_norm', display=global_display)
 
     def test_fpi_brst_rotations(self):
         time = '2015-10-16/13:06:30'
@@ -30,49 +30,49 @@ class SliceTests(unittest.TestCase):
         species = ['i']
         for spc in species:
             for rotation in rotations:
-                mms_part_slice2d(time=time, probe='1', species=spc, data_rate='brst', rotation=rotation, save_png='test_fpi_brst_' + spc + '_' + rotation, display=False)
+                mms_part_slice2d(time=time, probe='1', species=spc, data_rate='brst', rotation=rotation, save_png='test_fpi_brst_' + spc + '_' + rotation, display=global_display)
 
     def test_fpi_subtract_bulk(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='2d', save_png='test_fpi_brst_subtract_bulk',
-                         subtract_bulk=True, display=False)
+                         subtract_bulk=True, display=global_display)
 
     def test_fpi_avg_angle(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_avg_angle',
-                         average_angle=[-45, 45], display=False)
+                         average_angle=[-45, 45], display=global_display)
 
     def test_fpi_sum_angle(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_sum_angle',
-                         sum_angle=[-45, 45], display=False)
+                         sum_angle=[-45, 45], display=global_display)
 
     def test_fpi_energy(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_energy',
-                         energy=True, display=False)
+                         energy=True, display=global_display)
 
     def test_fpi_samples(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_samples',
-                         samples=3, display=False)
+                         samples=3, display=global_display)
 
     def test_fpi_window(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_window',
-                         window=3, display=False)
+                         window=3, display=global_display)
 
     def test_fpi_window_center(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_window_center',
-                         window=3, center_time=True, display=False)
+                         window=3, center_time=True, display=global_display)
 
     def test_fpi_custom_rotation(self):
         rot = np.zeros((3, 3))
@@ -83,38 +83,47 @@ class SliceTests(unittest.TestCase):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
                          interpolation='geometric', save_png='test_fpi_brst_custom_rotation',
-                         custom_rotation=rot, display=False)
+                         custom_rotation=rot, display=global_display)
 
     def test_fpi_2d_interp(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
-                         interpolation='2d', save_png='test_fpi_brst_2d_interp', display=False)
+                         interpolation='2d', save_png='test_fpi_brst_2d_interp', display=global_display)
 
     def test_fpi_2d_interp_zdirrange(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy',
-                         interpolation='2d', save_png='test_fpi_brst_zdirrange', display=False,
+                         interpolation='2d', save_png='test_fpi_brst_zdirrange', display=global_display,
                          zdirrange=[0, 2500])
 
     def test_fpi_limits(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='i', data_rate='brst', rotation='xy', erange=[0, 10000],
-                         save_png='test_fpi_brst_erange', display=False)
+                         save_png='test_fpi_brst_erange', display=global_display)
 
     def test_fpi_electrons(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='e', data_rate='brst', rotation='xy',
-                         save_png='test_fpi_brst_electrons', display=False)
+                         save_png='test_fpi_brst_electrons', display=global_display)
 
     def test_hpca(self):
         time = '2015-10-16/13:06:30'
         mms_part_slice2d(time=time, probe='1', species='hplus', instrument='hpca', data_rate='brst', rotation='xy',
-                         save_png='test_hpca_brst', display=False)
+                         save_png='test_hpca_brst', display=global_display)
 
     def test_hpca_trange(self):
         trange = ['2015-10-16/13:06:20', '2015-10-16/13:06:40']
         mms_part_slice2d(trange=trange, probe='1', species='hplus', instrument='hpca', data_rate='brst', rotation='xy',
-                         save_png='test_hpca_brst_trange', display=False)
+                         save_png='test_hpca_brst_trange', display=global_display)
+
+    def test_numeric_trange(self):
+        from pyspedas.projects.mms.particles.mms_part_slice2d import mms_part_slice2d
+        from pytplot import time_double, time_string
+        time = '2017-09-10/09:32:20'
+        trange = [time_double(time) - 60, time_double(time) + 60]
+        # trange = ['2017-09-10/09:31:20', '2017-09-10/09:33:20']
+        mms_part_slice2d(trange=trange, instrument='fpi', species='i', rotation='bv', erange=[0, 300], cmap="turbo", display=global_display, save_png='test_str_trange_turbo.png')
+
 
     def test_units_string(self):
         self.assertTrue(spd_units_string('counts') == 'Counts')
