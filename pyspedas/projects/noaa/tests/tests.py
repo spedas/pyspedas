d543b6a2d4c64e793a233bcf14547ced642b6eff
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-04-28 17:16:59 -0700

    Re-enabled test_load_kp_data_2014, after updating location of NOAA KP_AP index data Removed gfz_ftp test, FTP option no longer supported

--- pyspedas/projects/noaa/tests/tests.py
+++ pyspedas/projects/noaa/tests/tests.py
@@ -26,7 +26,6 @@ class LoadTestCases(unittest.TestCase):
         self.assertTrue(data_exists('Cp'))
         self.assertTrue(data_exists('C9'))
 
-    @unittest.skip('KP_AP data set no longer exists on NOAA FTP server')
     def test_load_kp_data_2014(self):
         # Load data from noaa
         del_data('*')
@@ -58,27 +57,6 @@ class LoadTestCases(unittest.TestCase):
         # final
         del_data('*')
         kp_vars = pyspedas.projects.noaa.noaa_load_kp(trange=['2014-01-01', '2014-01-02'], gfz=True)
-        self.assertTrue('Kp' in kp_vars)
-        self.assertTrue('ap' in kp_vars)
-        self.assertTrue('Sol_Rot_Num' in kp_vars)
-        self.assertTrue('Sol_Rot_Day' in kp_vars)
-        self.assertTrue('Kp_Sum' in kp_vars)
-        self.assertTrue('ap_Mean' in kp_vars)
-        self.assertTrue('Cp' in kp_vars)
-        self.assertTrue('C9' in kp_vars)
-        self.assertTrue(data_exists('Kp'))
-        self.assertTrue(data_exists('ap'))
-        self.assertTrue(data_exists('Sol_Rot_Num'))
-        self.assertTrue(data_exists('Sol_Rot_Day'))
-        self.assertTrue(data_exists('Kp_Sum'))
-        self.assertTrue(data_exists('ap_Mean'))
-        self.assertTrue(data_exists('Cp'))
-        self.assertTrue(data_exists('C9'))
-
-    def test_load_kp_data_2014_gfz_ftp(self):
-        # final
-        del_data('*')
-        kp_vars = pyspedas.projects.noaa.noaa_load_kp(trange=['2014-01-01', '2014-01-02'], gfz=True,gfz_ftp=True)
         self.assertTrue('Kp' in kp_vars)
         self.assertTrue('ap' in kp_vars)
         self.assertTrue('Sol_Rot_Num' in kp_vars)
