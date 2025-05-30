5e819b76a1f9238fb20981b4edd5882a8f16d261
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-05-29 22:27:30 -0700

    Version bump for MMS FPI partial moment support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2018-2025, Regents of the University of California, unless otherwis
 author = 'The PySPEDAS Community'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.21'
+release = '1.7.22'
 
 
 # -- General configuration ---------------------------------------------------
