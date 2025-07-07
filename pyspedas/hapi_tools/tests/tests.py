b3a9af28204812ea2fdb931a1488be8019aa12a8
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-07-07 12:49:12 -0700

    Removed unittest.skip() line

--- pyspedas/hapi_tools/tests/tests.py
+++ pyspedas/hapi_tools/tests/tests.py
@@ -63,7 +63,6 @@ class HAPITests(unittest.TestCase):
         self.assertTrue(data_exists('BY_GSE'))
         self.assertTrue(data_exists('BZ_GSE'))
 
-    #@unittest.skip("Skipping due to possible server-side change, unable to parse JSON response")
     def test_string_time(self):
         del_data()
         server = "https://supermag.jhuapl.edu/hapi"
