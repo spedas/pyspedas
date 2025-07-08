81a98475d80ebd53af130a2cdeb2886c4adc620c
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-07-07 18:31:32 -0700

    Add fake varnames to test_replace_fillvals tests

--- pyspedas/hapi_tools/tests/tests.py
+++ pyspedas/hapi_tools/tests/tests.py
@@ -10,19 +10,19 @@ class HAPITests(unittest.TestCase):
     def test_replace_fillvals(self):
         da1_dbl = np.array([1.0, 2.0, 3.0, 4.0])
         fv1_dbl = 3.0
-        replace_fillvals(da1_dbl, fv1_dbl, 'double')
+        replace_fillvals(da1_dbl, fv1_dbl, 'da1_dbl_scalarfill', 'double')
         assert_array_equal(da1_dbl, [1.0, 2.0, np.nan, 4.0])
         da2_dbl = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0],[7.0, 2.0]])
         fv2_dbl = [1.0, 2.0]
-        replace_fillvals(da2_dbl, fv2_dbl, 'double')
+        replace_fillvals(da2_dbl, fv2_dbl, 'da2_dbl_arrayfill', 'double')
         assert_array_equal(da2_dbl, np.array([[np.nan, np.nan], [3.0, 4.0], [5.0, 6.0],[7.0, np.nan]]))
         # Repeat after replacement, should be nothing to replace, and output array same as before
         fv2_dbl = [1.0, 2.0]
-        replace_fillvals(da2_dbl, fv2_dbl, 'double')
+        replace_fillvals(da2_dbl, fv2_dbl, 'da2_dbl_arrayfill','double')
         assert_array_equal(da2_dbl, np.array([[np.nan, np.nan], [3.0, 4.0], [5.0, 6.0],[7.0, np.nan]]))
         da2_int = np.array([[0,1], [2,3], [4,5], [6,2]])
         da2_fvint = 2
-        replace_fillvals(da2_int, da2_fvint, 'integer')
+        replace_fillvals(da2_int, da2_fvint, 'da2_int_scalarfill','integer')
         assert_array_equal(da2_int, np.array([[0,1],[0,3],[4,5],[6,0]]))
 
 
