64435232870a8403ffab19ac33faad1b0d1ab1c6
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-08-11 10:23:10 -0700

    Updated Supermag string time test with latest dataset ID

--- pyspedas/hapi_tools/tests/tests.py
+++ pyspedas/hapi_tools/tests/tests.py
@@ -63,11 +63,10 @@ class HAPITests(unittest.TestCase):
         self.assertTrue(data_exists('BY_GSE'))
         self.assertTrue(data_exists('BZ_GSE'))
 
-    @unittest.skip('problems with supermag hapi server?')
     def test_string_time(self):
         del_data()
         server = "https://supermag.jhuapl.edu/hapi"
-        dataset = "ttb/PT1M/baseline_all"
+        dataset = "ttb/baseline_all/PT1M/XYZ"
         start = "2020-05-10T00:00Z"
         stop = "2020-05-14T00:00Z"
         parameters = ''
