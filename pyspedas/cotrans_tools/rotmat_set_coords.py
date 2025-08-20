0507298d78553a0d3fe49f2a6af8400f5ff5272f
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-08-19 17:54:38 -0700

    Eliminate unnecessary store_data call that was generating a nuisance warning (but store_data needs a fix for this)

--- pyspedas/cotrans_tools/rotmat_set_coords.py
+++ pyspedas/cotrans_tools/rotmat_set_coords.py
@@ -20,5 +20,5 @@ def rotmat_set_coords(varname:str, in_coords:str, out_coords:str):
 
     md=pyspedas.get_data(varname, metadata=True)
     data_att = {'input_coord_sys':in_coords, 'output_coord_sys':out_coords}
+    # We have a pointer to the internal attribute dictionary, no need to call store_data here
     md['data_att'] = data_att
-    pyspedas.store_data(varname, attr_dict=md)
