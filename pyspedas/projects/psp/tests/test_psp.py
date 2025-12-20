70166624c17c91f4f91d62469e746566cc4073df
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-12-20 15:46:40 -0700

    Added test for L3 quality filtering

--- pyspedas/projects/psp/tests/test_psp.py
+++ pyspedas/projects/psp/tests/test_psp.py
@@ -4,6 +4,7 @@ import numpy as np
 from pyspedas.tplot_tools import data_exists
 import pyspedas
 
+global_display = False
 
 class LoadTestCases(unittest.TestCase):
     def test_unpublished_data(self):
@@ -230,6 +231,30 @@ class LoadTestCases(unittest.TestCase):
     def test_load_epi_data_suffix_none(self):
         epilo_vars = pyspedas.projects.psp.epi(suffix=None)
         self.assertTrue(data_exists('psp_isois_HET_A_Electrons_Rate_TS'))
+
+    def test_l3_filtering(self):
+        import pyspedas.projects.psp as psp
+        from pyspedas import tplot, options
+
+        # Load data during a rotation
+
+        trange = ['2020-08-05/12:00', '2020-08-05/20:00']
+        psp.fields(trange=trange, time_clip=True, datatype='rfs_hfr', level='l3')
+
+        # Set scale to show variation during rotation
+
+        options('psp_fld_l3_rfs_hfr_auto_averages_ch0_V1V2', 'zrange', [1e-17, 5e-17])
+
+        # Filter out data during rotation
+
+        psp.filter_fields('psp_fld_l3_rfs_hfr_auto_averages_ch0_V1V2', [8])
+
+        # Plot data, filtered data, and attitude data showing rotation
+
+        tplot(['psp_fld_l3_rfs_hfr_auto_averages_ch0_V1V2',
+               'psp_fld_l3_rfs_hfr_auto_averages_ch0_V1V2_008',
+               'psp_fld_l3_rfs_hfr_ch0_V1V2_J2000'],display=global_display, save_png='psp_l3filter.png')
+        self.assertTrue(data_exists('psp_fld_l3_rfs_hfr_auto_averages_ch0_V1V2_008'))
 
     def test_load_epi_data_prefix_suffix(self):
         epilo_vars = pyspedas.projects.psp.epi(prefix='pre_', suffix='_suf')
