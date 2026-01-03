cc65dde16163edb8a13a5bd8abb898aa89032deb
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-01-03 11:44:16 -0800

    Updated docstrings, fixed some linter nitpicks

--- pyspedas/tplot_tools/exporters/tplot_ascii.py
+++ pyspedas/tplot_tools/exporters/tplot_ascii.py
@@ -6,8 +6,36 @@
 import pyspedas
 
 def tplot_ascii(tvar, filename=None, extension='.csv'):
+    """
+    Save a single tplot variable in CSV format.
+
+    This routine converts the internal tplot data structure into a Pandas data frame, then uses
+    the Pandas to_csv() function to save it in CSV format.
+
+    If the tplot variable includes spectral bin metadata, it will be written to a separate file with
+    a "_v" inserted after the filename and before the ".csv" suffix.
+
+    Parameters
+    ----------
+        tvar: str
+            tplot variable name
+        filename: str
+            Base filename to use.  If spec_bin data is present, it will be written to a separate file
+            with "_v" appended to the base filename. The file extension suffix should not be included here (it will be added internally).
+        extension: str
+            File extension suffix to apply to the base filename.  Default: ".csv" (no other file formats
+            are supported).
+
+    Examples
+    --------
+
+    >>> import pyspedas
+    >>> pyspedas.projects.themis.state(probe='a', trange=['2007-03-23', '2007-03-24'])
+    >>> pyspedas.tplot_ascii('tha_pos',filename='themis_a_position')
+    """
+
     # grab data, prepend index column
-    if filename == None:
+    if filename is None:
         filename = tvar
     # save data
     pyspedas.tplot_tools.data_quants[tvar].to_pandas().to_csv(filename + extension)
