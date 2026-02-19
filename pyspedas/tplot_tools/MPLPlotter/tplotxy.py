0535b70dcde2d81c9af3e636915d3b45fb0fc89e
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-02-19 11:21:20 -0800

    Add support for more options, additional tests

--- pyspedas/tplot_tools/MPLPlotter/tplotxy.py
+++ pyspedas/tplot_tools/MPLPlotter/tplotxy.py
@@ -2,14 +2,25 @@ from pyspedas.tplot_tools import tplot_wildcard_expand, get_data, get_coords, ge
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Wedge
 
 def tplotxy(tvars,
             plane='xy',
-            colors=('k','b','g','r','c','m','y'),
             center_origin=True,
             reverse_x = False,
             reverse_y = False,
             plot_units='re',
+            colors=('k', 'b', 'g', 'r', 'c', 'm', 'y'),
+            linestyles=('solid'),
+            linewidths=(None),
+            markers=(None),
+            startmarkers=(None),
+            endmarkers=(None),
+            markevery=10,
+            markersize=5,
+            plotmask=(True),
+            show_centerbody=True,
+            centerbody_size_re=1.0,
             save_png='',
             save_eps='',
             save_jpeg='',
@@ -28,14 +39,37 @@ def tplotxy(tvars,
         or in the case of field line plots, ntimesxnpointsx3
     plane: string or list of strings
         Plane to project the 3d vectors onto. Valid options: 'xy', 'xz', 'yz'. Default: 'xy'
-    colors: list or tuple of strings
-        Colors to use if multiple variables are plotted.
     center_origin: bool
         If True, center the plot on the origin.
     reverse_x: bool
         If True, reverse the x-axis of the plot (more positive values to the left)
     reverse_y: bool
         If True, reverse the y-axis of the plot (more positive values to the bottom)
+    colors: list or tuple of strings
+        Colors to use if multiple variables are plotted. Default: ('k', 'b', 'g', 'r', 'c', 'm', 'y').
+        If number of variables exceeds number of colors, they will cycle.
+    linestyles: list or tuple of strings
+        Line styles to use for each variable. Default: ('solid').
+        If number of variables exceeds number of linestyles, they will cycle.
+    linewidths: list or tuple of strings
+        Line widths to use for each variable. If number of variables exceeds number of linewidths, they will cycle.
+        Default: (None) (use matplotlib default)
+    markers: list or tuple of str
+        marker style for the data points (default: (None))
+    startmarkers: list or tuple of str
+        marker style for the start of each trace (default: (None))
+    endmarkers: list or tuple of str
+        marker style for the end of each trace (default: (None))
+    markevery: int or sequence of int
+        plot a marker at every n-th data point (default: 10)
+    markersize: float
+        size of the marker in points (default: 5)
+    plotmask: list or tuple of bool
+        If False, skip this variable when making plots, calculating axis ranges, etc. Default: (True)
+    show_centerbody: bool
+        If True, draw the central body at the origin
+    centerbody_size_re: float
+        The size in Re of the center body. Default: 1.0
     save_png : str, optional
         A full file name and path.
         If this option is set, the plot will be automatically saved to the file name provided in PNG format.
@@ -85,6 +119,24 @@ def tplotxy(tvars,
     xsize=4.0
     ysize=4.0
 
+    if not isinstance(colors, (list, np.ndarray,tuple)):
+        colors = [colors]
+    if not isinstance(markers, (list, np.ndarray,tuple)):
+        markers = [markers]
+    if not isinstance(startmarkers, (list, np.ndarray,tuple)):
+        startmarkers = [startmarkers]
+    if not isinstance(endmarkers, (list, np.ndarray,tuple)):
+        endmarkers = [endmarkers]
+    if not isinstance(linestyles, (list, np.ndarray,tuple)):
+        linestyles = [linestyles]
+    if not isinstance(linewidths, (list, np.ndarray,tuple)):
+        linewidths = [linewidths]
+
+    if not isinstance(plotmask, (list, np.ndarray,tuple)):
+        plotmask = [plotmask]
+    if (len(plotmask) != 1) and (len(plotmask) != len(tvars)):
+        logging.warning(f"Mismatch between plotmask length {len(plotmask)} and variable list length {len(tvars)}.")
+
     if fig is None and axis is None:
         fig, axis = plt.subplots(sharey=True, sharex=True, figsize=(xsize, ysize), layout='constrained')
 
@@ -100,6 +152,11 @@ def tplotxy(tvars,
         unit_annotation=f' Units: {plot_units} '
 
     for index,tvar in enumerate(tvars):
+        n_plotmask = len(plotmask)
+        this_plotmask = plotmask[index % n_plotmask]
+        if not this_plotmask:
+            logging.info(f"Variable {tvar} is masked, skipping")
+            continue
         units=get_units(tvar)
         if isinstance(plot_units, (list, np.ndarray)):
             units=units[0]
@@ -138,30 +195,77 @@ def tplotxy(tvars,
         elif plane=='yz':
             proj_x = d.y[:,1] * unit_conv
             proj_y = d.y[:,2] * unit_conv
-            axis.set_xlabel('X' + unit_annotation)
+            axis.set_xlabel('Y' + unit_annotation)
             axis.set_ylabel('Z' + unit_annotation)
 
 
 
-        local_xmax = np.max(proj_x)
-        local_ymax = np.max(proj_y)
-        local_xmin = np.max(proj_x)
-        local_ymin = np.max(proj_y)
+        local_xmax = np.nanmax(proj_x)
+        local_ymax = np.nanmax(proj_y)
+        local_xmin = np.nanmin(proj_x)
+        local_ymin = np.nanmin(proj_y)
 
-        max_x = np.max([local_xmax, max_x])
-        max_y = np.max([local_ymax, max_y])
-        min_x = np.min([local_xmin, min_x])
-        min_y = np.min([local_ymin, min_y])
-        axis.plot(proj_x, proj_y, color=colors[index])
+        max_x = np.nanmax([local_xmax, max_x])
+        max_y = np.nanmax([local_ymax, max_y])
+        min_x = np.nanmin([local_xmin, min_x])
+        min_y = np.nanmin([local_ymin, min_y])
+
+        n_colors = len(colors)
+        thiscolor = colors[index % n_colors]
+        n_styles = len(linestyles)
+        thisstyle = linestyles[index % n_styles]
+        n_widths = len(linewidths)
+        thiswidth = linewidths[index % n_widths]
+        n_markers = len(markers)
+        thismarker = markers[index % n_markers]
+        n_startmarkers = len(startmarkers)
+        thisstartmarker = startmarkers[index % n_startmarkers]
+        n_endmarkers = len(endmarkers)
+        thisendmarker = endmarkers[index % n_endmarkers]
+
+        axis.plot(proj_x, proj_y, color=thiscolor, linestyle=thisstyle, linewidth=thiswidth, marker=thismarker, markersize=markersize, markevery=markevery)
+        if thisstartmarker is not None:
+            axis.plot(proj_x[0], proj_y[0], color=thiscolor, linestyle=thisstyle, marker=thisstartmarker, markersize=markersize)
+        if thisendmarker is not None:
+            axis.plot(proj_x[-1], proj_y[-1], color=thiscolor, linestyle=thisstyle, marker=thisendmarker, markersize=markersize)
 
     if center_origin:
-        x_halfwidth = np.max(np.abs([max_x, min_x]))
-        y_halfwidth = np.max(np.abs([max_y, min_y]))
-        all_halfwidth = np.max([x_halfwidth, y_halfwidth])
+        x_halfwidth = np.nanmax(np.abs([max_x, min_x]))
+        y_halfwidth = np.nanmax(np.abs([max_y, min_y]))
+        # Add 5% to each boundary to give a little margin
+        all_halfwidth = 1.05 * np.nanmax([x_halfwidth, y_halfwidth])
         axis.set_xlim((-all_halfwidth, all_halfwidth))
         axis.set_ylim((-all_halfwidth, all_halfwidth))
     else:
-        pass
+        # Adjust the X and Y ranges by equal increments to give a little margin
+        x_adjust = .05*(max_x - min_x)
+        y_adjust = .05*(max_y - min_y)
+        all_adjust = np.nanmax([x_adjust, y_adjust])
+        xr = (min_x-all_adjust, max_x+all_adjust)
+        yr = (min_y-all_adjust, max_y+all_adjust)
+        axis.set_xlim(xr)
+        axis.set_ylim(yr)
+
+    if show_centerbody:
+        if plot_units == 're':
+            cb_radius = centerbody_size_re
+        else:
+            cb_radius = centerbody_size_re * km_in_re
+
+        theta1, theta2 = -90, 90
+        if plane=='xy' or plane=='xz':
+            if reverse_x:
+                w1 = Wedge((0, 0),cb_radius, theta1, theta2,fc='white', edgecolor='black' )
+                w2 = Wedge((0, 0), cb_radius, theta2, theta1, fc='black', edgecolor='black')
+            else:
+                w1 = Wedge((0, 0),cb_radius, theta1, theta2,fc='black', edgecolor='black' )
+                w2 = Wedge((0, 0), cb_radius, theta2, theta1, fc='white', edgecolor='black')
+            axis.add_artist(w1)
+            axis.add_artist(w2)
+        else:
+            theta1,theta2 = 0,360
+            w1 = Wedge((0, 0), cb_radius, theta1, theta2, fc='white', edgecolor='black')
+            axis.add_artist(w1)
 
     axis.set_aspect('equal')
     if reverse_x:
--- pyspedas/utilities/tests/test_utilities_tplotxy.py
+++ pyspedas/utilities/tests/test_utilities_tplotxy.py
@@ -40,33 +40,94 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
 default_trange = ["2020-01-01", "2020-01-02"]
-global_display=False
+global_display=True
 
 
 class PlotTestCases(unittest.TestCase):
-    """Test plot functions."""
+    """Test XY plot functions."""
 
-    def test_themis_orbit1(self):
+    def test_themis_orbit_km(self):
         # Test a simple THEMIS orbit plot in the XY plane, plot units in re
         themis.state(trange=default_trange, probe=['a','b','c','d','e'])
         tplotxy('th?_pos_gse', reverse_x=True, reverse_y=True, plot_units='re', display=global_display, save_png='orbits_thm_art_re.png')
 
-    def test_themis_orbit2(self):
+    def test_themis_orbit_km_xz(self):
+        # Test a simple THEMIS orbit plot in the XY plane, plot units in re
+        themis.state(trange=default_trange, probe=['a','b','c','d','e'])
+        tplotxy('th?_pos_gse', reverse_x=True, reverse_y=False, plane='xz', plot_units='re', display=global_display, save_png='orbits_thm_art_xz_re.png')
+
+    def test_themis_orbit_km_yz(self):
+        # Test a simple THEMIS orbit plot in the XY plane, plot units in re
+        themis.state(trange=default_trange, probe=['a','b','c','d','e'])
+        tplotxy('th?_pos_gse', reverse_x=False, reverse_y=False, plane='yz', plot_units='re', display=global_display, save_png='orbits_thm_art_yz_re.png')
+
+    def test_themis_orbit_re(self):
         # Test a simple THEMIS orbit plot in the XY plane,plot units in km
         themis.state(trange=default_trange, probe=['a','b','c','d','e'])
         tplotxy('th?_pos_gse', reverse_x=True, reverse_y=True, plot_units='km', display=global_display, save_png='orbits_thm_art_km.png')
 
-    def test_themis_orbit3(self):
+    def test_themis_orbit_inner_mixedunits_km(self):
         # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units km, mixed input units
         themis.state(trange=default_trange, probe=['a','d', 'e'])
         tkm2re('the_pos_gse', km=False)
         tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse_re'], reverse_x=True, reverse_y=True, plot_units='km', display=global_display, save_png='orbits_thm_mixed_km.png')
 
-    def test_themis_orbit4(self):
+    def test_themis_orbit_inner_mixedunits_re(self):
         # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
         themis.state(trange=default_trange, probe=['a','d', 'e'])
         tkm2re('the_pos_gse', km=False)
         tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse_re'], reverse_x=True, reverse_y=True, plot_units='re', display=global_display, save_png='orbits_thm_mixed_re.png')
+
+    def test_themis_orbit_inner_linestyles(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', linestyles=['solid','dotted','dashdot'], display=global_display, save_png='orbits_thm_linestyles.png')
+
+    def test_themis_orbit_inner_markers(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', markers=['x','.','+'], markevery=60, display=global_display, save_png='orbits_thm_markers.png')
+
+    def test_themis_orbit_inner_startendmarkers(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', startmarkers=['x','.','+'], endmarkers=['x','.','+'], markers=None, display=global_display, save_png='orbits_thm_startendmarkers.png')
+
+    def test_themis_orbit_inner_startmarkers(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', startmarkers=['x','.','+'], markers=None, display=global_display, save_png='orbits_thm_startmarkers.png')
+
+    def test_themis_orbit_inner_plotmask(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', plotmask=[True,False,True], display=global_display, save_png='orbits_thm_plotmask.png')
+
+    def test_themis_orbit_inner_linewidths(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', linewidths=[1,2,3], display=global_display, save_png='orbits_thm_linewidths.png')
+
+    def test_themis_orbit_inner_showearth_xy(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], reverse_x=True, reverse_y=True, plot_units='re', show_centerbody=True, display=global_display, save_png='orbits_thm_showearth_xy.png')
+
+    def test_themis_orbit_inner_showearth_xz(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], plane='xz', reverse_x=True, reverse_y=False, plot_units='re', show_centerbody=True, display=global_display, save_png='orbits_thm_showearth_xz.png')
+
+
+    def test_themis_orbit_inner_showearth_yz(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], plane='yz', reverse_x=True, reverse_y=False, plot_units='re', show_centerbody=True, display=global_display, save_png='orbits_thm_showearth_yz.png')
+
+    def test_themis_orbit_inner_showearth_yz_nocenter(self):
+        # Test a simple THEMIS orbit plot in the XY plane, inner probes, plot units re, mixed input units
+        themis.state(trange=default_trange, probe=['a','d', 'e'])
+        tplotxy(['tha_pos_gse', 'thd_pos_gse', 'the_pos_gse'], plane='yz', center_origin = False, reverse_x=True, reverse_y=False, plot_units='re', show_centerbody=True, display=global_display, save_png='orbits_thm_showearth_yz_nocenter.png')
 
 if __name__ == "__main__":
     unittest.main()
