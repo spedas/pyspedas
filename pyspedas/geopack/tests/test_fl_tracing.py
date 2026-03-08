fc5751657d708a965e187b05d852bc5a8a0eec12
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-03-08 16:31:14 -0700

    Default to reduced comparison data set from github Check for locally generated full IDL trace data before plotting additional diagnostics

--- pyspedas/geopack/tests/test_fl_tracing.py
+++ pyspedas/geopack/tests/test_fl_tracing.py
@@ -40,7 +40,7 @@ if not os.path.exists(save_dir):
 validation_dir = TESTING_CONFIG["remote_validation_dir"]
 
 trange = ["2015-10-16", "2015-10-17"]
-global_display = True
+global_display = False
 
 
 def gen_circle():
@@ -92,17 +92,16 @@ class LoadTestCases(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Download tplot files
-        # filename = test_data_download_file(
-        #     validation_dir,
-        #     "analysis_tools",
-        #     "geopack_idl_validate_multi_gp14.tplot",
-        #     save_dir,
-        # )
-        filename = '/tmp/ttrace_validate_jumbo.tplot'
+        filename = test_data_download_file(
+            validation_dir,
+            "analysis_tools",
+            "ttrace_validate_reduced.tplot",
+            save_dir,
+        )
+        # Uncomment this if you want to use locally generated comparison data including the huge IDL trace variable.
+        # filename = '/tmp/ttrace_validate_jumbo.tplot'
         del_data("*")
         tplot_restore(filename)
-
-        pass
 
 
     def test_trace_iono_s_idl_all(self):
@@ -296,13 +295,11 @@ class LoadTestCases(unittest.TestCase):
     def test_t89_equ_idl_all(self):
         from pyspedas.geopack import ttrace2endpoint
         d1=get_data('tha_state_pos')
-        d2=get_data('tha_eq_t89_trace')
         #tplotxy3(['tha_iono_t89_trace'])
         d3=get_data('eq_foot_t89')
         ttrace2endpoint('tha_state_pos',"t89", "equator", 'py_eq_foot', 'py_eq_trace', iopt=3, km=True)
         py_trace_data = get_data('py_eq_trace')
         py_foot_data = get_data('py_eq_foot')
-        idl_trace_data = d2.y
         idl_foot_data = d3.y
 
         set_coords('py_eq_foot','GSM')
@@ -316,22 +313,14 @@ class LoadTestCases(unittest.TestCase):
         foot_point = py_foot_data.y[0,:]
 
         n_py_trace = len(trace_points)
-        n_idl_trace = d2.y.shape[1]
         py_tp_array = np.zeros((n_py_trace,3))
         py_tp_array[:,:] = trace_points
         py_time_array = np.zeros(n_py_trace)
         py_time_array[:] = t
-        idl_time_array = np.zeros(n_idl_trace)
-        idl_time_array[:] = t
-        idl_tp_array = np.zeros((n_idl_trace,3))
-        idl_tp_array[:,:]= d2.y[0,:,:]
 
         store_data('py_eq_trace1',data={'x':py_time_array, 'y':trace_points})
         pyspedas.set_coords('py_eq_trace1','gsm')
         pyspedas.set_units('py_eq_trace1','km')
-        store_data('idl_eq_trace1',data={'x':idl_time_array, 'y':idl_tp_array})
-        pyspedas.set_coords('idl_eq_trace1','gsm')
-        pyspedas.set_units('idl_eq_trace1','km')
 
         max_trace_dist = 0
         max_foot_dist = 0
@@ -364,25 +353,47 @@ class LoadTestCases(unittest.TestCase):
         # affected, but the next ones out seem to work more-or-less OK.
 
         n=max_foot_dist_idx
-        idl_single_trace = idl_trace_data[n-3:n+4,:,:]
         py_single_trace = py_trace_data.y[n-3:n+4,:,:]
         orbit_single_trace = d1.y[n-3:n+4,:]
         store_data('py_eq_trace1',data={'x':d1.times[n-3:n+4], 'y':py_single_trace})
         pyspedas.set_coords('py_eq_trace1','gsm')
         pyspedas.set_units('py_eq_trace1','km')
-        store_data('idl_eq_trace1',data={'x':d1.times[n-3:n+4], 'y':idl_single_trace})
-        pyspedas.set_coords('idl_eq_trace1','gsm')
-        pyspedas.set_units('idl_eq_trace1','km')
-        store_data('orbit_trace1',data={'x':d1.times[n-3:n+4], 'y':orbit_single_trace})
+        # The full trace data for this example is huge (350 MB), too big to store in github.  You can generate it locally with IDL, and use the 'jumbo' version of
+        # the output file.
+        if data_exists('tha_eq_t89_trace'):
+            d2=get_data('tha_eq_t89_trace')
+            idl_trace_data = d2.y
+            n_idl_trace = d2.y.shape[1]
+            idl_time_array = np.zeros(n_idl_trace)
+            idl_time_array[:] = t
+            idl_tp_array = np.zeros((n_idl_trace, 3))
+            idl_tp_array[:, :] = d2.y[0, :, :]
+            store_data('idl_eq_trace1',data={'x':idl_time_array, 'y':idl_tp_array})
+            pyspedas.set_coords('idl_eq_trace1','gsm')
+            pyspedas.set_units('idl_eq_trace1', 'km')
+            idl_tp_array = np.zeros((n_idl_trace, 3))
+            idl_tp_array[:, :] = d2.y[0, :, :]
+            idl_single_trace = idl_trace_data[n - 3:n + 4, :, :]
+            store_data('idl_eq_trace1', data={'x': d1.times[n-3:n+4], 'y':idl_single_trace})
+            store_data('orbit_trace1', data={'x': d1.times[n - 3:n + 4], 'y': orbit_single_trace})
+            pyspedas.set_coords('idl_eq_trace1','gsm')
+            pyspedas.set_units('idl_eq_trace1','km')
+            tplotxy3(['py_eq_trace1', 'idl_eq_trace1', 'orbit_trace1'], legend_names=['py', 'idl', 'orbit'],
+                     colors=['black', 'red', 'blue'], markers=['+', '<', None], markevery=1, linewidths=[2, 1, 1],
+                     reverse_x=True, plot_units='km', show_centerbody=False, display=global_display,
+                     save_png='tha_equ_singletrace.png')
+            tplotxy3(['py_eq_trace', 'tha_eq_t89_trace', 'tha_state_pos'], legend_names=['py', 'idl', 'orbit'],
+                     colors=['black', 'red', 'blue'], markers=[None, None, '+'], reverse_x=True, plot_units='km',
+                     show_centerbody=False, display=global_display, save_png='tha_equ_all.png')
 
-        #print(f"Max directed trace distance: {max_trace_dist}")
+        # print(f"Max directed trace distance: {max_trace_dist}")
         print(f"Max foot distance: {max_foot_dist} at index {max_foot_dist_idx}")
         print(f"Max idl foot radius: {max_idl_foot_radius}")
         print(f"Min idl foot radius: {min_idl_foot_radius}")
         print(f"Max py foot radius: {max_py_foot_radius}")
         print(f"Min py foot radius: {min_idl_foot_radius}")
         print(f"Median foot point distance: {foot_dist_median}")
-        print(f"Python trace points at max foot distance: {len(py_trace_data.y[max_foot_dist_idx,:,:])}")
+        print(f"Python trace points at max foot distance: {len(py_trace_data.y[max_foot_dist_idx, :, :])}")
         print(f"Start point at max foot distance:{d1.y[max_foot_dist_idx,:]}")
 
         print(f"Foot points at max idx: py {py_foot_data.y[max_foot_dist_idx,:]} idl {idl_foot_data[max_foot_dist_idx,:]}")
@@ -392,8 +403,6 @@ class LoadTestCases(unittest.TestCase):
         print(f"py foot point {foot_point}, idl foot point {d3.y[0,:]}")
         print(f"foot point distance: {np.linalg.norm(foot_point-d3.y[0,:])}")
         print(f"foot point r: python {np.linalg.norm(foot_point)} idl: {np.linalg.norm(d3.y[0,:])}")
-        tplotxy3(['py_eq_trace1', 'idl_eq_trace1', 'orbit_trace1'], legend_names=['py','idl','orbit'], colors=['black', 'red', 'blue'], markers = ['+', '<', None], markevery=1,linewidths=[2,1,1], reverse_x=True,plot_units='km',show_centerbody=False, display=global_display, save_png='tha_equ_singletrace.png')
-        tplotxy3(['py_eq_trace', 'tha_eq_t89_trace', 'tha_state_pos'], legend_names=['py','idl','orbit'], colors=['black', 'red','blue'], markers=[None, None, '+'], reverse_x=True,plot_units='km',show_centerbody=False, display=global_display, save_png='tha_equ_all.png')
         tplotxy3(['py_eq_foot', 'eq_foot_t89'], legend_names=['py','idl'], colors=['green','red'],markers=['+',None], reverse_x=True, plot_units = 'km',show_centerbody=True, display=global_display, save_png='tha_equ_foot.png')
 
     def test_calculate_lshell(self):
