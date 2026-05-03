34b3dffc28dcf6221fe1c5ea456c45fe6651b9ed
Samuel Badman <samuel.badman@cfa.harvard.edu>
2026-05-03 12:51:02 -0400

    Change SWEAP pre-public URL from http to https

--- pyspedas/projects/psp/config.py
+++ pyspedas/projects/psp/config.py
@@ -3,7 +3,7 @@ import os
 CONFIG = {
     'local_data_dir': 'psp_data/',
     'remote_data_dir': 'https://spdf.gsfc.nasa.gov/pub/data/psp/',
-    'sweap_remote_data_dir': 'http://sweap.cfa.harvard.edu/data/sci/',
+    'sweap_remote_data_dir': 'https://sweap.cfa.harvard.edu/data/sci/',
     'fields_remote_data_dir': 'https://sprg.ssl.berkeley.edu/data/spp/data/sci/'
 }
 
