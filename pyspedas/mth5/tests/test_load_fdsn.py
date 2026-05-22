831d947f5b1131554cdbd449e26fcded3243c1ab
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-05-21 23:06:48 -0700

    Bump mth5 version requirement to >= 0.6.7, which includes obspy as an optional dependency (which pyspedas needs) Adjust FDSN imports to version 0.6.7 style. Delete obsolete/skipped tests

--- pyspedas/mth5/tests/test_load_fdsn.py
+++ pyspedas/mth5/tests/test_load_fdsn.py
@@ -4,9 +4,7 @@ import urllib.error
 from urllib.error import URLError, HTTPError
 import socket
 
-# PyCharm doesn't like this import of FDSN when mth5 version 0.6.7 is installed, but
-# everything still works. so there must be some runtime magic in play...
-from mth5.clients.make_mth5 import FDSN
+from mth5.clients.fdsn import FDSN
 from pyspedas.mth5.load_fdsn import load_fdsn
 from pyspedas.mth5.utilities import _list_of_fdsn_channels
 from pyspedas.mth5.utilities import datasets
@@ -360,61 +358,6 @@ class TestMTH5LoadFDSN(unittest.TestCase):
         # Verify the exception type and message for URLError
         self.assertTrue(isinstance(cm.exception, FDSNNoServiceException) or isinstance(cm.exception, URLError))
         # self.assertTrue(isinstance(cm.exception, URLError))
-
-    # This test seems to be obsolete
-    @unittest.skipIf(H5OPEN, "Open h5 files detected. Close all the h5 references before runing this test")
-    @unittest.skip
-    def test99_load_fdsn_h5_file_error(self):
-        # Testing if the file can be created when error occurs
-
-        request_df = pd.DataFrame(
-            {
-                "network": ["4P"],
-                "station": ["ALW48"],
-                "location": ["--"],
-                "channel": ["*F*"],
-                "start": ["2015-06-22T00:00:00"],
-                "end": ["2015-06-24T00:00:00:00"]  # intentional error
-            }
-        )
-
-        fdsn_object = FDSN(mth5_version='0.2.0', client="EARTHSCOPE")
-        try:
-            # This will raise an error because the date format is wrong. But the h5 files will be created and open
-            fdsn_object.make_mth5_from_fdsn_client(request_df, interact=False, path=CONFIG['local_data_dir'])
-        except Exception as e:
-            # Check if the type error is raised
-            self.assertIsInstance(e, TypeError)
-            pass
-
-        # test is load_fdsn closes the file when error is raised and message is logged
-        with self.assertLogs(logger=pyspedas.logger, level="INFO") as cm:
-            try:
-                load_fdsn(network="4P", station="ALW48", trange=['2015-06-22', '2015-06-24'])
-            except Exception as e:
-                pass
-            # test if we have ERROR and INFO in the log messages
-            self.assertTrue(any(map(lambda x: x.find("ERROR") == 0, cm.output)))
-            self.assertTrue(any(map(lambda x: x.find("INFO") == 0, cm.output)))
-
-        # Empty lists/dicts evaluate to False
-        self.assertFalse(h5py.h5f.get_obj_ids(types=h5py.h5f.OBJ_FILE))
-
-        # import h5py
-        # print(h5py.h5f.get_obj_ids(types=h5py.h5f.OBJ_FILE))
-        #
-        # with self.assertLogs(level="ERROR") as cm:
-        #     try:
-        #         load_fdsn(network="4P", station="ALW48", trange=['2015-06-22', '2015-06-24'])
-        #     except:
-        #         pass
-        # print(cm)
-
-        pass
-
-    @unittest.skip
-    def test99_load_fdsn_no_data(self):
-        pass
 
     def tearDown(self):
         pass
