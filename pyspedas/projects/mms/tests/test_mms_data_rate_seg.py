0bd2961ce721bf785f9e78fbeab83d13e4a6394c
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-07-04 16:45:56 -0700

    Add tests for new generic unix2tai and tai2unix converters. Also added some tests for cdflib tt2000 conversions, with failing assertions commented out.  This will be useful for figuring out if the cdflib bugs are truly fixed.

--- pyspedas/projects/mms/tests/test_mms_data_rate_seg.py
+++ pyspedas/projects/mms/tests/test_mms_data_rate_seg.py
@@ -1,7 +1,7 @@
 import unittest
 import logging
 import numpy as np
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 from pyspedas.projects.mms.spd_mms_load_bss import spd_mms_load_bss
 from pyspedas.projects.mms.mms_load_sroi_segments import mms_load_sroi_segments, get_mms_srois
 from pyspedas.projects.mms.deprecated.mms_load_fast_segments import mms_load_fast_segments
@@ -12,6 +12,83 @@ from pyspedas.projects.mms.mms_tai2unix import mms_tai2unix, mms_unix2tai
 
 
 class SegmentTestCases(unittest.TestCase):
+
+    def test_cdfepoch_leap(self):
+        from cdflib import cdfepoch
+        t1=np.int64(536500867184000000) # 23:59:59
+        t2=t1+1000000000 # Leap second: 23:59:60
+        t3=t2+1000000000 # First second of 2017, 00:00:00
+        t4=t3+1000000000 # 00:00:01
+        t5=t3+60*60*1000000000 # One hour after midnight, 2017-01-01/01:00:00
+        b3=cdfepoch.breakdown_tt2000(t3) # t3 converts correctly to 2017-01-01/00:00:00
+        assert_array_equal(b3, [2017, 1, 1, 0, 0, 0, 0, 0, 0])
+        print("t3 ",b3)
+
+        b23=cdfepoch.breakdown_tt2000([t2,t3]) # wrong: t3 comes out with min=60
+        # Assertion disabled pending bug fix in cdflib
+        #assert_array_equal(b23[1,:], [2017, 1, 1, 0, 0, 0, 0, 0, 0])
+        print("t2 t3",b23)
+
+        b13=cdfepoch.breakdown_tt2000([t1,t3]) # min = 60
+        #assert_array_equal(b13[1::], [2017, 1, 1, 0, 0, 0, 0, 0, 0])
+        print("t1 t3", b13)
+
+        b14=cdfepoch.breakdown_tt2000([t1,t4]) # correct
+        assert_array_equal(b14[1,:], [2017, 1, 1, 0, 0, 1, 0, 0, 0])
+        print("t1 t4",b14)
+
+        b15=cdfepoch.breakdown_tt2000([t1,t5]) # wrong: comes out as 00:60 rather than 01:00
+        #assert_array_equal(b15[1,:], [2017, 1, 1, 1, 0, 0, 0, 0, 0])
+        print("t1 t5",b15)
+
+
+    def test_tai2unix_scalar(self):
+        from pyspedas import unix2tai, tai2unix
+        start_date='2016-12-31/23:59:00'
+        start_unix = time_double(start_date)
+        unix=np.zeros(70,np.float64)
+        rt_unix=np.zeros(70,np.float64)
+
+        for i in range(70):
+            unix[i] = start_unix + i
+            unix_str = time_string(unix[i])
+            tai = unix2tai(unix[i])
+            rt_unix[i] = tai2unix(tai)
+            tai2unix_str = time_string(rt_unix[i])
+            print(unix[i],unix_str,tai,rt_unix[i],tai2unix_str)
+        assert_allclose(unix,rt_unix)
+
+    def test_tai2unix_array(self):
+        from pyspedas import unix2tai, tai2unix
+        start_date='2016-12-31/23:59:00'
+        start_unix = time_double(start_date)
+        unix=np.array([start_unix + i for i in range(70)])
+
+        unix_str = time_string(unix)
+        tai = unix2tai(unix)
+        # Why does this fail when the scalar version does not?
+        # It's a cdflib bug that's overeager about marking leap second transitions.
+        # We can avoid this by iterating over each input value. rather than all in one shot,
+        # so we can catch cdflib exceptions and work around the bog.   Once cdflib is fixed,
+        # we should be able to go back to a completely vectorized version of tai2unix.
+        rt_unix = tai2unix(tai)
+        assert_allclose(unix,rt_unix)
+        t1 = unix2tai(time_double('2016-12-31/23:59:59')) # just before 2017 leap second
+        t2 = t1 + 1.0 # the leap second itself...last second of 2016, 23:59:60
+        t3 = t2 + 1.0 # first second of 2017, 00:00:0
+        t4 = t3 + 1.0 # 00:00:01
+        t5 = t3 + 60*60 # 01:00:00, one hour after leap sec boundary
+
+        # Here we're testing that no uncaught exceptions are raised.  A few of these will
+        # trigger cdflib bugs, but tai2unix should catch them
+        t1_str = time_string(tai2unix(t1))
+        t2_str = time_string(tai2unix(t2))
+        t3_str = time_string(tai2unix(t3))
+        t12_str = time_string(tai2unix([t1,t2]))
+        t13_str = time_string(tai2unix([t1,t3]))
+        t134_str = time_string(tai2unix([t1,t3, t4]))
+        t15_str = time_string(tai2unix([t1,t5]))
+
 
     def test_sroi(self):
         del_data("*")
