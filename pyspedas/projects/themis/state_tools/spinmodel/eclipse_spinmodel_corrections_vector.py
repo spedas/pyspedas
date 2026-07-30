1eefb182579c450c3caf3c5512010ef8d084fa89
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-07-30 12:36:14 -0700

    Check number of dimensions for input variable

--- pyspedas/projects/themis/state_tools/spinmodel/eclipse_spinmodel_corrections_vector.py
+++ pyspedas/projects/themis/state_tools/spinmodel/eclipse_spinmodel_corrections_vector.py
@@ -44,7 +44,7 @@ def eclipse_spinmodel_corrections_vector(tvars:str,
         # The correction must be done in DSL coordinates
         d=get_data(v)
         t=d.times
-        if d.y.shape[1] != 3:
+        if (len(d.y.shape) <= 1) or d.y.shape[1] != 3:
             raise ValueError(f"Data array for variable {v} with shape {d.y.shape} is not a 3-element vector")
         elif d.y.shape[0] != len(t):
             raise ValueError(f"Mismatched time ({t.shape}) and data {d.y.shape} arrays for variable {v}")
