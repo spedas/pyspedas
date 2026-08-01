17f205acc9d961772e3f67859f454109a3f16f8a
jameswilburlewis <jwl@ssl.berkeley.edu>
2026-08-01 16:00:11 -0700

    Replaced three separate make_databar routines with one parameterized routine Replaced internal logic of spd_mms_load_bss with calls to mms_load_burst_intervals and mms_load_fast_intervals

--- pyspedas/projects/mms/make_databar_tvar.py
+++ pyspedas/projects/mms/make_databar_tvar.py
@@ -2,18 +2,22 @@ from pyspedas import store_data, options
 import logging
 import numpy as np
 
-def make_databar_burst_tvar(unix_starts,unix_ends, suffix:str = ''):
+def make_databar_tvar(basename='',suffix='',label='Fast',unix_starts=None, unix_ends=None):
     """
     Make a tplot variable like mms_bss_burst from lists of burst start and end times
 
     Parameters
     ----------
+    basename: str
+        Base name for tplot variable
+    suffix: str
+        Suffix to add to tplot variable name
+    label: str
+        String to use for legend_names plot option
     unix_starts:
         Start times of burst intervals
     unix_ends:
         End times of burst intervals
-    suffix:
-        Suffix to add to tplot variable name
 
     Returns
     -------
@@ -28,11 +32,11 @@ def make_databar_burst_tvar(unix_starts,unix_ends, suffix:str = ''):
         bar_x.extend([start_time, start_time, end_time, end_time])
         bar_y.extend([np.nan, 0., 0., np.nan])
 
-    varname = 'mms_bss_burst' + suffix
+    varname = basename + suffix
     vars_created = store_data(varname, data={'x': bar_x, 'y': bar_y})
 
     if not vars_created:
-        logging.error(f'Error creating BSS segment intervals tplot variable {varname}')
+        logging.error(f'Error creating databar tplot variable {varname}')
         return
 
     options(varname, 'panel_size', 0.09)
@@ -40,7 +44,7 @@ def make_databar_burst_tvar(unix_starts,unix_ends, suffix:str = ''):
     options(varname, 'Color', 'green')
     options(varname, 'border', False)
     options(varname, 'yrange', [-0.001, 0.001])
-    options(varname, 'legend_names', ['Burst'])
+    options(varname, 'legend_names', [label])
     options(varname, 'ytitle', '')
 
     return varname
--- pyspedas/projects/mms/spd_mms_load_bss.py
+++ pyspedas/projects/mms/spd_mms_load_bss.py
@@ -1,18 +1,17 @@
 import logging
-from pyspedas.tplot_tools import time_double
-from pyspedas.projects.mms.mms_load_sroi_segments import mms_load_sroi_segments
-from pyspedas.projects.mms.mms_load_brst_segments import mms_load_brst_segments
-from pyspedas.projects.mms.mms_update_fast_intervals import mms_update_fast_intervals
-from .make_bss_tplot_var import make_bss_tplot_var
+from pyspedas.projects.mms.mms_load_fast_intervals import mms_load_fast_intervals
+from pyspedas.projects.mms.mms_load_burst_intervals import mms_load_burst_intervals
+from .make_databar_tvar import make_databar_tvar
 
 
 def spd_mms_load_bss(trange=['2015-10-16', '2015-10-17'],
                      datatype=['fast', 'burst'],
-                     probe='1',suffix='',
+                     probe='1',
+                     suffix='',
                      no_download=False):
     """
     Creates tplot variables which allow you to display horizontal color bars 
-    indicating burst data availability.
+    indicating fast or burst data availability.
 
     Parameters
     -----------
@@ -25,46 +24,42 @@ def spd_mms_load_bss(trange=['2015-10-16', '2015-10-17'],
         probe: str or int
             S/C probe # for SRoI bars (used as fast survey segments after 6Nov15; default: 1)
 
+        suffix: str
+            Suffix to add to tplot variable names. Default: ''
+
         no_download: bool
             If True, use cached files rather than downloading from the MMS SDC
+            Default: False
+
+    Returns
+    -------
+    list[str]
+        List of tplot variables created
 
     """
 
     if not isinstance(datatype, list):
         datatype = [datatype]
 
+    tvars_created = []
+    
     for dtype in datatype:
         if dtype == 'fast':
-            abs_sroi_cutover = time_double('2015-11-06')
-            if time_double(trange[0]) <= abs_sroi_cutover and time_double(trange[1]) <= abs_sroi_cutover:
-                # use the old fast segments code for dates before 6Nov15
-                logging.info("Loading early mission fast survey segments from abs_selections datasets")
-                out = mms_update_fast_intervals(trange=trange,suffix=suffix,no_download=no_download)
-
-            elif time_double(trange[0]) <= abs_sroi_cutover and time_double(trange[1]) > abs_sroi_cutover:
-                # Requested range spans cutover date, get ABS before and SROI after, then combine
-                out1 = mms_update_fast_intervals(trange=[trange[0],abs_sroi_cutover], make_tplot_var=False, no_download=no_download)
-                out2 = mms_load_sroi_segments(trange=trange, probe=probe, make_tplot_var=False,no_download=no_download)
-                comb_starts = []
-                comb_ends = []
-                if out1 is not None:
-                    comb_starts.extend(out1[0])
-                    comb_ends.extend(out1[1])
-                if out2 is not None:
-                    comb_starts.extend(out2[0])
-                    comb_ends.extend(out2[1])
-                # make the tplot variable
-                out = comb_starts, comb_ends
-                make_bss_tplot_var(out[0],out[1],suffix=suffix)
-
+            out = mms_load_fast_intervals(trange=trange, probe=probe, no_download=no_download)
+            if (out is None) or (len(out[0]) == 0):
+                logging.error("Problem loading fast survey intervals")
             else:
-                # use SRoI code for dates on and after 6Nov15
-                out = mms_load_sroi_segments(trange=trange, probe=probe, suffix=suffix)
+                tvar = make_databar_tvar(basename='mms_bss_fast',label='Fast',suffix=suffix,unix_starts=out[0],unix_ends=out[1])
+                tvars_created.append(tvar)
         elif dtype == 'burst':
-            out = mms_load_brst_segments(trange=trange, suffix=suffix, no_download=no_download)
+            out = mms_load_burst_intervals(trange=trange, no_download=no_download)
+            if (out is None) or (len(out[0]) == 0):
+                logging.error("Problem loading burst intervals")
+            else:
+                tvar = make_databar_tvar(basename="mms_bss_burst", suffix=suffix, label='Burst',unix_starts=out[0], unix_ends=out[1])
+                tvars_created.append(tvar)
         else:
             logging.error('Unsupported datatype: ' + dtype + '; valid options: "fast" and "burst"')
             continue
 
-        if out is None:
-            logging.error('Problem loading segments for ' + dtype)
+    return tvars_created
